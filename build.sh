cb5b63c5dfa7e8ca502a7b0e5cdcf8caa84a8f48
linzy_hccl <linzhiyang5@huawei.com>
2025-12-10 12:51:13 +0800

    Add smoke-test

--- build.sh
+++ build.sh
@@ -30,6 +30,7 @@ ENABLE_UT="off"
 ENABLE_ST="off"
 CMAKE_BUILD_TYPE="Debug"
 ASCEND_3RD_LIB_PATH="${CURRENT_DIR}/output/third_party"
+BUILD_CB_TEST="false"
 
 # 自定义算子工程
 ENABLE_CUSTOM="off"
@@ -87,6 +88,11 @@ function build_package(){
     cmake_config
     log "Info: build_package"
     build package
+}
+
+function build_cb_test_verify(){
+    cd ${CURRENT_DIR}/examples/
+    bash build.sh
 }
 
 function build_test() {
@@ -386,6 +392,10 @@ while [[ $# -gt 0 ]]; do
     --sign-script=*)
         shift
         ;;
+    --cb_test_verify)
+        BUILD_CB_TEST="true"
+        shift
+        ;;
     --enable-sign)
         ENABLE_SIGN="true"
         shift
@@ -483,6 +493,15 @@ elif [ "${KERNEL}" == "true" ]; then
     build_kernel
 elif [ "${ENABLE_CUSTOM}" == "on" ]; then
     build_custom
+elif [ "${BUILD_CB_TEST}" == "true" ]; then
+    log "Info: Building cb_test_verify"
+    build_cb_test_verify
+    if grep -q "Make Failure" ${BUILD_DIR}/build.log || grep -q "Make test Failure" ${BUILD_DIR}/build.log; then
+        log "Info: Building cb_test_verify failed"
+        exit 1
+    else
+        log "Info: Building cb_test_verify success"
+    fi
 elif [ "${FULL_MODE}" == "true" ]; then
     cd ..
     mkdir -p ${BUILD_DEVICE_DIR}
