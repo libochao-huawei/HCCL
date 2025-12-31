8358f5bb9e15b88ff6a5b39a3973b7cc891d632f
zeng_hao <zenghao56@h-partners.com>
2025-12-31 17:31:09 +0800

    scatter_aicpumode dfx is host

--- src/ops/scatter/scatter_op.cc
+++ src/ops/scatter/scatter_op.cc
@@ -590,7 +590,7 @@ HcclResult SelectAlg(HcclComm comm, OpParam &param, TopoInfo* topoInfo, AlgType&
 
     // 在algTag中追加编排模式
     bool isAicpuMode = (param.engine == CommEngine::COMM_ENGINE_AICPU) ||
- 	                   (param.engine == CommEngine::COMM_ENGINE_AICPU_TS)
+ 	                   (param.engine == CommEngine::COMM_ENGINE_AICPU_TS);
  	const char* launchMode = (isAicpuMode ? "_aicpu" : "_host");
     int ret = strcat_s(param.algTag, sizeof(param.algTag), launchMode);
     if (ret != 0) {
