038335fa0da0200712081bada10d2c309a93dfd7
kingkong000 <leikaiyi@huawei.com>
2026-01-29 17:27:46 +0800

    scatter notify

--- src/ops/scatter/scatter_op.h
+++ src/ops/scatter/scatter_op.h
@@ -40,12 +40,12 @@ HcclResult CalcBaseTopoInfo(HcclComm comm, OpParam &param, TopoInfo** topoInfo);
 HcclResult SelectAlg(HcclComm comm, OpParam &param, TopoInfo* topoInfo, AlgType& algType, std::string &algName);
 
 HcclResult GetAlgRes(HcclComm comm, OpParam &param, std::unique_ptr<ExecutorBase> &executor,
-    TopoInfo* topoInfo, AlgType& algType, AlgResourceCtx** resCtx, aclrtNotify* notifies);
+    TopoInfo* topoInfo, AlgType& algType, AlgResourceCtx** resCtx);
 
 HcclResult GetAlgType(TopoInfo* topoInfo, HcclCMDType opType, AlgType& algType);
 
 HcclResult AllocAlgResource(HcclComm comm, const OpParam& param, AlgResourceRequest &resRequest,
-    AlgResourceCtx* resCtxHost, aclrtNotify* notifies);
+    AlgResourceCtx* resCtxHost);
 
 HcclResult SetAlgoLevel0(TopoInfo* topoInfo, HcclAlgoType algoConfig, AlgTypeLevel0 &algType);
 
