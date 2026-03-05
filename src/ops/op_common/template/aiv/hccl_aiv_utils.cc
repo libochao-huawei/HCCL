a8b39a363ee7a882be1d7f0ad0bd7febe1791d43
zhouqi148 <zhouqi148@huawei.com>
2026-03-05 14:40:31 +0800

    【Feature】ccu_aclgraph添加捕获从流的功能

--- src/ops/op_common/template/aiv/hccl_aiv_utils.cc
+++ src/ops/op_common/template/aiv/hccl_aiv_utils.cc
@@ -205,7 +205,9 @@ HcclResult GetAivCountTag(const std::string &commTag, u32 rank, s32 &aivCountTag
 // 满足isAivClearEnable条件时，在KernelLaunch前调用，清零标记区
 HcclResult ClearAivSyncBuf(const OpParam &param, AlgResourceCtxSerializable& resCtx)
 {
-    ACLCHECK(aclrtMemcpy(static_cast<u8*>(resCtx.aivCommInfoPtr) + AIV_FLAG_ADDR_OFFSET, AIV_FLAG_AREA_SIZE,
+    // param 暂时未使用
+    static_cast<void>(param);
+    CHK_RET(haclrtMemcpy(static_cast<u8*>(resCtx.aivCommInfoPtr) + AIV_FLAG_ADDR_OFFSET, AIV_FLAG_AREA_SIZE,
         static_cast<u8*>(resCtx.aivCommInfoPtr) + AIV_FLAG_CLEAR_OFFSET, AIV_FLAG_AREA_SIZE, ACL_MEMCPY_DEVICE_TO_DEVICE));
     HCCL_INFO("[ClearAivSyncBuf] clearaiv done.");
     return HCCL_SUCCESS;
