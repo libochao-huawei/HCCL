5fbeb09741092d68808d1c5045a999ab49af8cd4
chenliang267 <chenliang318@huawei.com>
2026-03-05 21:47:42 +0800

    ubx allreduce bugfix

--- src/ops/all_reduce/template/ccu/ccu_temp_all_reduce_nhr_mem2mem_1D_multi_jetty.h
+++ src/ops/all_reduce/template/ccu/ccu_temp_all_reduce_nhr_mem2mem_1D_multi_jetty.h
@@ -32,6 +32,7 @@ public:
 
     HcclResult CalcRes(HcclComm comm, const OpParam& param, const TopoInfoWithNetLayerDetails* topoInfo,
                        AlgResourceRequest& resourceRequest) override;
+    HcclResult GetRes(AlgResourceRequest& resourceRequest) override;
 
     HcclResult KernelRun(const OpParam& param,
                          const TemplateDataParams& templateDataParams,
--- src/ops/all_reduce/template/ccu/kernel/ccu_kernel_all_reduce_nhr_mem2mem_1D_multi_jetty.cc
+++ src/ops/all_reduce/template/ccu/kernel/ccu_kernel_all_reduce_nhr_mem2mem_1D_multi_jetty.cc
@@ -466,7 +466,7 @@ HcclResult CcuKernelAllReduceNhr1DMem2MemMultiJetty::DoSendRecvSlice(const u32 t
 
 HcclResult CcuKernelAllReduceNhr1DMem2MemMultiJetty::Algorithm()
 {
-    HCCL_INFO("[CcuKernelAllReduceNhr1DMem2MemMultiJetty] ReduceScatterMesh1DMem2Mem run");
+    HCCL_INFO("[CcuKernelAllReduceNhr1DMem2MemMultiJetty] CcuKernelAllReduceNhr1DMem2MemMultiJetty run");
 
     CHK_RET(InitResource());
     CHK_RET(LoadArgs());
@@ -475,7 +475,7 @@ HcclResult CcuKernelAllReduceNhr1DMem2MemMultiJetty::Algorithm()
     CHK_RET(DoReduceScatterNHR());
     CHK_RET(DoAllGatherNHR());
 
-    HCCL_INFO("[CcuKernelAllReduceNhr1DMem2MemMultiJetty] ReduceScatterMesh1DMem2Mem end");
+    HCCL_INFO("[CcuKernelAllReduceNhr1DMem2MemMultiJetty] CcuKernelAllReduceNhr1DMem2MemMultiJetty end");
     
     return HcclResult::HCCL_SUCCESS;
 }
