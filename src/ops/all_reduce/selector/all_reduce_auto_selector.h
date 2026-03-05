853512f9bd7a79202172cca92d32e88831d795b9
m0_50464975 <zhangdiyan@huawei.com>
2026-03-05 21:53:00 +0800

    clean code all-reduce aicpu

--- src/ops/all_reduce/selector/all_reduce_auto_selector.h
+++ src/ops/all_reduce/selector/all_reduce_auto_selector.h
@@ -30,7 +30,7 @@ private:
                                   std::string &selectAlgName) const;
     SelectorStatus SelectAivAlgo(TopoInfoWithNetLayerDetails* topoInfo, OpParam &opParam, const std::map<HcclCMDType, 
                                  std::vector<HcclAlgoType>> &configAlgMap, std::string &selectAlgName) const override;
-    SelectorStatus SelectMeshAlgoAicpu(const TopoInfoWithNetLayerDetails* topoInfo, OpParam &opParam,
+    SelectorStatus SelectMeshAlgoAicpu(const TopoInfoWithNetLayerDetails* topoInfo, const OpParam &opParam,
                                        std::string &selectAlgName) const;
 };
 
