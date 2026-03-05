9ab5cd8d8f8bd428de4c7aaf85e95eff5945544f
weixin_43960572 <yuantangzhi@huawei.com>
2026-03-05 20:29:51 +0800

    Hybrid comm and selector conflict  fix

--- src/ops/reduce/selector/reduce_auto_selector.h
+++ src/ops/reduce/selector/reduce_auto_selector.h
@@ -25,9 +25,15 @@ private:
     SelectorStatus SelectAicpuAlgo(const TopoInfoWithNetLayerDetails *topoInfo, const OpParam &opParam,
         const std::map<HcclCMDType, std::vector<HcclAlgoType>> &configAlgMap,
         std::string &selectAlgName) const override;
+<<<<<<< HEAD
     SelectorStatus SelectMeshAlgoCcums(const TopoInfoWithNetLayerDetails *topoInfo, const OpParam &opParam, std::string &selectAlgName) const;
     SelectorStatus SelectMeshAlgoCcuSchedule(const TopoInfoWithNetLayerDetails *topoInfo, const OpParam &opParam, std::string &selectAlgName) const;
     SelectorStatus SelectAivAlgo(const TopoInfoWithNetLayerDetails *topoInfo, const OpParam &opParam,
+=======
+    SelectorStatus SelectMeshAlgoCcums(TopoInfoWithNetLayerDetails *topoInfo, OpParam &opParam, std::string &selectAlgName) const;
+    SelectorStatus SelectMeshAlgoCcuSchedule(TopoInfoWithNetLayerDetails *topoInfo, OpParam &opParam, std::string &selectAlgName) const;
+    SelectorStatus SelectAivAlgo(TopoInfoWithNetLayerDetails *topoInfo, OpParam &opParam,
+>>>>>>> 789a2ad... Hybrid comm and selector conflict  fix
         const std::map<HcclCMDType, std::vector<HcclAlgoType>> &configAlgMap,
         std::string &selectAlgName) const override;
     SelectorStatus SelectMeshAlgoAicpu(const TopoInfoWithNetLayerDetails *topoInfo,
