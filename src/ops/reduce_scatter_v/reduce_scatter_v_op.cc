7d90a75521e76d9d196c8e0b3bf5ab6571eef863
weixin_43960572 <yuantangzhi@huawei.com>
2026-03-05 20:29:51 +0800

    Hybrid comm and selector fix

--- src/ops/reduce_scatter_v/reduce_scatter_v_op.cc
+++ src/ops/reduce_scatter_v/reduce_scatter_v_op.cc
@@ -182,7 +182,7 @@ if (!paramMem) {
     std::string algName;
     std::unique_ptr<TopoInfoWithNetLayerDetails> topoInfo = std::make_unique<TopoInfoWithNetLayerDetails>();
     CHK_RET(Selector(comm, param, topoInfo, algName));
-    if (param.opExecuteConfig != OpExecuteConfig::AICPU_TS && param.opExecuteConfig != OpExecuteConfig::HOSTCPU) {
+    if (param.opExecuteConfig != OpExecuteConfig::AICPU_TS && opExecuteConfig != OpExecuteConfig::HOSTCPU) {
         return HcclReduceScatterVInner(sendBuf, sendCounts, sendDispls, recvBuf, recvCount, dataType, op, comm, stream);
     }
     CHK_RET(HcclExecOp(comm, param, topoInfo, algName));
