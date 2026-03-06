d3238e26c83142bd76375e96e617ef048b230e22
rockethcgs <luobinyan1@huawei.com>
2026-03-06 11:18:53 +0800

    fix param check & delete mc2 testcase for checker

--- src/common/param_check.h
+++ src/common/param_check.h
@@ -12,15 +12,28 @@
 #define OPS_HCCL_SRC_COMMON_PARAM_CHECK
 
 #include <hccl/hccl_types.h>
+#include <hccl/hccl_res.h>
 #include "hccl/base.h"
 
 namespace ops_hccl {
+HcclResult HcomCheckGroupName(const char *group);
+
 HcclResult HcomCheckOpParam(const char *tag, const u64 count, const HcclDataType dataType, const char *group,
     const void *stream);
 
 HcclResult HcomCheckOpParam(const char *tag, const u64 count, const HcclDataType dataType, const void *stream);
 
 HcclResult HcomCheckOpParam(const char *tag, const u64 count, const HcclDataType dataType);
+
+HcclResult HcomCheckTag(const char *tag);
+
+HcclResult HcomCheckCount(const u64 count);
+
+HcclResult HcomCheckDataType(const HcclDataType dataType);
+
+HcclResult HcomCheckReductionOp(const HcclReduceOp op);
+
+HcclResult HcomCheckCommEngine(const CommEngine commEngine);
 
 HcclResult HcomCheckUserRank(const u32 totalRanks, const u32 userRank);
 }
