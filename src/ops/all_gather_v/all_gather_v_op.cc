7aa42b860812ea28ec5ae1e99c5686d8d29854dd
weixin_43960572 <yuantangzhi@huawei.com>
2026-03-06 14:05:55 +0800

    opExen fix 2

--- src/ops/all_gather_v/all_gather_v_op.cc
+++ src/ops/all_gather_v/all_gather_v_op.cc
@@ -141,7 +141,7 @@ HcclResult AllGatherVOutPlace(void *sendBuf, void *recvBuf, uint64_t sendCount,c
     std::string algName;
     std::unique_ptr<TopoInfoWithNetLayerDetails> topoInfo = std::make_unique<TopoInfoWithNetLayerDetails>();
     CCHK_RET(Selector(comm, param, topoInfo, algName));
-    if (param.opExecuteConfig != OpExecuteConfig::AICPU_TS) {
+    if (param.opExecuteConfig != OpExecuteConfig::AICPU_TS && param.opExecuteConfig != OpExecuteConfig::HOSTCPU) {
         return HcclAllGatherVInner(sendBuf, sendCount, recvBuf, recvCounts, recvDispls, dataType, comm, stream);
     }
     CHK_RET(HcclExecOp(comm, param, topoInfo, algName));
