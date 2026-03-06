17232c55f5f131ed796179afe9d5033ef519aaf3
weixin_43960572 <yuantangzhi@huawei.com>
2026-03-06 14:54:08 +0800

    fix scatter selector

--- src/ops/scatter/selector/scatter_auto_selector.h
+++ src/ops/scatter/selector/scatter_auto_selector.h
@@ -27,8 +27,9 @@ private:
     SelectorStatus SelectAicpuAlgo(const TopoInfoWithNetLayerDetails *topoInfo, const OpParam &opParam,
                                    const std::map<HcclCMDType, std::vector<HcclAlgoType>> &configAlgMap,
                                    std::string                                 &selectAlgName) const override;
-    SelectorStatus SelectAivAlgo(const TopoInfoWithNetLayerDetails *topoInfo, const OpParam &opParam, const std::map<HcclCMDType, 
-                                 const std::vector<HcclAlgoType>> &configAlgMap, std::string &selectAlgName) const override;                     
+    SelectorStatus SelectAivAlgo(const TopoInfoWithNetLayerDetails *topoInfo, const OpParam &opParam,
+                                 const std::map<HcclCMDType, std::vector<HcclAlgoType>> &configAlgMap,
+                                 std::string &selectAlgName) const override;                     
 };
 
 } // namespace ops_hccl
