302f5cd51450852dcb155e3203780f95f38bbbec
fuyunde <fuyunde@h-partners.com>
2026-03-07 11:54:17 +0800

    fix(): CleanCode

--- src/ops/scatter/executor/ins_v2_scatter_sole_executor.h
+++ src/ops/scatter/executor/ins_v2_scatter_sole_executor.h
@@ -24,8 +24,6 @@
 #include "topo_match_1d.h"
 #include "topo_match_multilevel.h"
 
-
-
 namespace ops_hccl {
 template <typename AlgTopoMatch, typename InsAlgTemplate> class InsV2ScatterSoleExecutor : public InsCollAlgBase {
 public:
