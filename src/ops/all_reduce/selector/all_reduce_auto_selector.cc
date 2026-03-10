2ddf99d694463028b433c2af90dd1e453d2167ec
weixin_43960572 <yuantangzhi@huawei.com>
2026-03-10 09:53:11 +0800

    fix compile bug

--- src/ops/all_reduce/selector/all_reduce_auto_selector.cc
+++ src/ops/all_reduce/selector/all_reduce_auto_selector.cc
@@ -213,7 +213,7 @@ SelectorStatus AllReduceAutoSelector::SelectCcuScheduleLevel0UBXAlgo(TopoInfoWit
     return SelectorStatus::MATCH;
 }
 
-SelectorStatus AllReduceAutoSelector::SelectCcuScheduleLevel0Algo(TopoInfoWithNetLayerDetails* topoInfo, 
+SelectorStatus AllReduceAutoSelector::SelectCcuScheduleLevel0Algo(TopoInfoWithNetLayerDetails* topoInfo, const OpParam &opParam,
                                  std::string &selectAlgName, u64 dataSize) const
 {
     if (topoInfo->level0Topo == Level0Shape::MESH_1D) {
