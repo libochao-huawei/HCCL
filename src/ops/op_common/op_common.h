8090a6b5daeb61dc3e4fd65e3e925f920c1e42bf
weixin_43960572 <yuantangzhi@huawei.com>
2026-03-10 11:10:22 +0800

    fix bug

--- src/ops/op_common/op_common.h
+++ src/ops/op_common/op_common.h
@@ -114,9 +114,9 @@ HcclResult HcclAivKernelEntranceLaunch(HcclComm comm, OpParam &param, std::uniqu
 
 HcclResult HcclGetOpExpansionMode(HcclComm comm, OpParam &param);
 
-HcclResult DecideHcclOpExpansionMode(HcclComm comm, OpParam &param);
+HcclResult DecideHcclOpExpansionMode(HcclComm comm, HcclOpExpansionMode &finalMod);
 
-HcclResult ApplyOpExpansionMode(HcclComm comm, OpParam &param);
+HcclResult ApplyOpExpansionMode(OpParam &param, HcclOpExpansionMode finalMode);
 
 bool HcclCheckAicpuEnableOpen();
 
