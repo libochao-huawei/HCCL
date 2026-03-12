f66169e64eac40eaf40cb63ef5651061d3779e6c
chenliang267 <chenliang318@huawei.com>
2026-03-12 17:36:24 +0800

    fix compile error

--- src/ops/all_reduce/executor/ins_v2_all_reduce_concurrent_executor.cc
+++ src/ops/all_reduce/executor/ins_v2_all_reduce_concurrent_executor.cc
@@ -28,15 +28,16 @@ InsV2AllReduceConcurrentExecutor<AlgTopoMatch, InsAlgTemplate0, InsAlgTemplate1>
 template <typename AlgTopoMatch, typename InsAlgTemplate0, typename InsAlgTemplate1>
 HcclResult InsV2AllReduceConcurrentExecutor<AlgTopoMatch, InsAlgTemplate0, InsAlgTemplate1>::InitCommInfo(
     const OpParam &param, const TopoInfoWithNetLayerDetails *topoInfo, const AlgHierarchyInfoForAllLevel &algHierarchyInfo)
-    : myRank_(topoInfo->userRank),
-      rankSize_(topoInfo->userRankSize),
-      devType_(topoInfo->deviceType),
-      reduceOp_(param.reduceType),
-      dataType_(param.DataDes.dataType),
-      dataCount_(param.DataDes.count),
-      dataTypeSize_(SIZE_TABLE[param.DataDes.dataType]),
-      algHierarchyInfo_(algHierarchyInfo)
 {
+    myRank_ = topoInfo->userRank;
+    rankSize_ = topoInfo->userRankSize;
+    devType_ = topoInfo->deviceType;
+    reduceOp_ = param.reduceType;
+    dataType_ = param.DataDes.dataType;
+    dataCount_ = param.DataDes.count;
+    dataTypeSize_ = SIZE_TABLE[param.DataDes.dataType];
+    algHierarchyInfo_ = algHierarchyInfo;
+    
     HCCL_INFO("[%s][InitCommInfo] myRank [%u], rankSize [%u], devType [%u], redOp [%u], "
               "dataType [%u] dataTypeSize [%u]",
               __func__, myRank_, rankSize_, devType_, reduceOp_, dataType_, dataTypeSize_);
