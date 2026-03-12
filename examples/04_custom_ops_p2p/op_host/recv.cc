f5ea58cd9eb00c59183810120c10653a4e831d7f
jiyuanhao <jiyuanhao1@huawei.com>
2026-03-12 21:48:04 +0800

    fix example

--- examples/04_custom_ops_p2p/op_host/recv.cc
+++ examples/04_custom_ops_p2p/op_host/recv.cc
@@ -8,8 +8,8 @@
  * See LICENSE in the root of the software repository for the full text of the License.
  */
 
-#include "hccl/hccl_types.h"
-#include "hccl/hcomm_primitives.h"
+#include <hccl/hccl_types.h>
+#include <hccl/hcomm_primitives.h>
 #include "log.h"
 #include "common.h"
 #include "hccl_custom_p2p.h"
