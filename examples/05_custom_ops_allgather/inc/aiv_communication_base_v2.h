cc406b6b50d000eb9eb2a6807711e9d9674ba1b9
liuhaoyu35 <liuhaoyu35@huawei.com>
2026-03-12 14:43:41 +0800

    test

--- examples/05_custom_ops_allgather/inc/aiv_communication_base_v2.h
+++ examples/05_custom_ops_allgather/inc/aiv_communication_base_v2.h
@@ -120,13 +120,13 @@ constexpr uint64_t FLAG_BUF_NUM = 3;
 // 当前每个kernel最多使用4组同步标记，这里预留6组
 constexpr uint32_t MAX_FLAG_SIZE_PER_KERNEL = 6 * MAX_RANK_SIZE * FLAG_SIZE;
 
-enum AivReduceOp {
-    AIV_REDUCE_SUM = 0,    /**< sum */
-    AIV_REDUCE_PROD = 1,   /**< prod */
-    AIV_REDUCE_MAX = 2,    /**< max */
-    AIV_REDUCE_MIN = 3,    /**< min */
-    AIV_REDUCE_RESERVED = 255 /**< reserved */
-};
+// enum AivReduceOp {
+//     AIV_REDUCE_SUM = 0,    /**< sum */
+//     AIV_REDUCE_PROD = 1,   /**< prod */
+//     AIV_REDUCE_MAX = 2,    /**< max */
+//     AIV_REDUCE_MIN = 3,    /**< min */
+//     AIV_REDUCE_RESERVED = 255 /**< reserved */
+// };
 
 #define BASE_FLAG_OFFSET (MAX_FLAG_SIZE_PER_KERNEL)
 
@@ -350,11 +350,11 @@ template<typename T>
 __aicore__ inline void AivCommBase::SetAtomicOp(uint32_t atomicOp)
 {
     switch (atomicOp) {
-        case AivReduceOp::AIV_REDUCE_SUM:
+        case HcclReduceOp::AIV_REDUCE_SUM:
         SetAtomicAdd<T>(); break;
-        case AivReduceOp::AIV_REDUCE_MAX:
+        case HcclReduceOp::AIV_REDUCE_MAX:
         SetAtomicMax<T>(); break;
-        case AivReduceOp::AIV_REDUCE_MIN:
+        case HcclReduceOp::AIV_REDUCE_MIN:
         SetAtomicMin<T>(); break;
         default:
         SetAtomicNone(); break;
