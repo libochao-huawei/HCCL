fc98d4575b445aa28617e00e09ff4293525d5a78
littlemons <yaowenjie7@huawei.com>
2026-03-12 11:48:44 +0800

    [alltoall]cleancode修改

--- src/ops/all_to_all_v/executor/ins_v2_all_to_all_concurrent_executor.h
+++ src/ops/all_to_all_v/executor/ins_v2_all_to_all_concurrent_executor.h
@@ -22,7 +22,7 @@ template <typename AlgTopoMatch, typename InsAlgTemplate0, typename InsAlgTempla
 class InsV2AllToAllConcurrentExecutor : public InsCollAlgBase {
 public:
     explicit InsV2AllToAllConcurrentExecutor();
-    ~InsV2AllToAllConcurrentExecutor() = default;
+    ~InsV2AllToAllConcurrentExecutor() override = default;
 
     HcclResult Orchestrate(const OpParam &param, const AlgResourceCtxSerializable& resCtx) override;
 
@@ -53,18 +53,17 @@ private:
         std::vector<u64> rdispls;
     };
 
-    HcclResult SetTemplateDataParams(TemplateDataParams &tempAlgParams, SendRecvData &splitData,u32 loop,
+    HcclResult SetTemplateDataParams(TemplateDataParams &tempAlgParams, const SendRecvData &splitData,u32 loop,
         u64 currDataCount, u64 processedDataCount, u64 maxDataCountPerLoop);
     HcclResult FillTemplateResource(const OpParam &param, const AlgResourceCtxSerializable& resCtx,
         TemplateResource& templateAlgRes, uint32_t index);
     HcclResult InitTemplateDataParams(const OpParam &param, const AlgResourceCtxSerializable& resCtx,
         TemplateDataParams& tempAlgParams);
-    HcclResult RestoreSendRecvData(const OpParam &param, const AlgResourceCtxSerializable& resCtx);
-    HcclResult SplitSendRecvData(const OpParam &param, const AlgResourceCtxSerializable& resCtx,
-        std::vector<SendRecvData>& splitData);
+    HcclResult RestoreSendRecvData(const OpParam &param);
+    HcclResult SplitSendRecvData(std::vector<SendRecvData>& splitData);
     HcclResult GetMaxSendRecvDataCount(u64& maxSendRecvDataCount, const SendRecvData& splitData);
-    HcclResult CalcMaxDataCountPerLoop(const OpParam &param,TemplateDataParams &tempAlgParams,
-        const std::vector<u64> scratchMulti, std::vector<u64>& maxDataCountPerLoop);
+    HcclResult CalcMaxDataCountPerLoop(const OpParam &param, const std::vector<u64> scratchMulti,
+        std::vector<u64>& maxDataCountPerLoop);
 
     std::vector<u64> sendCounts_;
     std::vector<u64> recvCounts_;
