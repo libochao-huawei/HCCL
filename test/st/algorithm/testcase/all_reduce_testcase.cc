c0b582291cb71512daae87d799c282fe1cde754c
email <chengyueming@huawei.com>
2026-03-16 09:57:53 +0800

    upload dpu ops

--- test/st/algorithm/testcase/all_reduce_testcase.cc
+++ test/st/algorithm/testcase/all_reduce_testcase.cc
@@ -1,12 +1,12 @@
 /**
- * Copyright (c) 2025 Huawei Technologies Co., Ltd.
- * This program is free software, you can redistribute it and/or modify it under the terms and conditions of
- * CANN Open Software License Agreement Version 2.0 (the "License").
- * Please refer to the License for details. You may not use this file except in compliance with the License.
- * THIS SOFTWARE IS PROVIDED ON AN "AS IS" BASIS, WITHOUT WARRANTIES OF ANY KIND, EITHER EXPRESS OR IMPLIED,
- * INCLUDING BUT NOT LIMITED TO NON-INFRINGEMENT, MERCHANTABILITY, OR FITNESS FOR A PARTICULAR PURPOSE.
- * See LICENSE in the root of the software repository for the full text of the License.
- */
+ * Copyright (c) 2025 Huawei Technologies Co., Ltd.
+ * This program is free software, you can redistribute it and/or modify it under the terms and conditions of
+ * CANN Open Software License Agreement Version 2.0 (the "License").
+ * Please refer to the License for details. You may not use this file except in compliance with the License.
+ * THIS SOFTWARE IS PROVIDED ON AN "AS IS" BASIS, WITHOUT WARRANTIES OF ANY KIND, EITHER EXPRESS OR IMPLIED,
+ * INCLUDING BUT NOT LIMITED TO NON-INFRINGEMENT, MERCHANTABILITY, OR FITNESS FOR A PARTICULAR PURPOSE.
+ * See LICENSE in the root of the software repository for the full text of the License.
+ */
 
 #include <algorithm>
 #include <thread>
@@ -257,6 +257,16 @@ TEST_F(ST_ALL_REDUCE_TEST, st_all_reduce_meshchunk_odd_rank)
     u32 dataTypeSize = 4;
     u64 dataCount = AR_AICPU_1D_MAX_DATA_SIZE + 200 * 1024;
     HcclReduceOp reduceOp = HcclReduceOp::HCCL_REDUCE_SUM;
+    RunAllReduceCase(topoMeta, dataCount, dataType, dataTypeSize, reduceOp);
+}
+
+TEST_F(ST_ALL_REDUCE_TEST, st_all_reduce_meshchunk_ultra_big_data)
+{
+    TopoMeta topoMeta{{{0, 1, 2, 3, 4, 5, 6, 7}}};
+    HcclDataType dataType = HcclDataType::HCCL_DATA_TYPE_INT8;
+    u32 dataTypeSize = 1;
+    u64 dataCount = std::max(AR_AICPU_1D_MAX_DATA_SIZE + 1024, static_cast<u64>(4 * 1024 * 1024 * 1024ULL));
+    HcclReduceOp reduceOp = HcclReduceOp::HCCL_REDUCE_MIN;
     RunAllReduceCase(topoMeta, dataCount, dataType, dataTypeSize, reduceOp);
 }
 
