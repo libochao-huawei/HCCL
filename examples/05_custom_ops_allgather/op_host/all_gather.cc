7effa9846c4d4b3397d5281c25ac9ad39e83377c
liuhaoyu35 <liuhaoyu35@huawei.com>
2026-03-16 20:25:23 +0800

    test

--- examples/05_custom_ops_allgather/op_host/all_gather.cc
+++ examples/05_custom_ops_allgather/op_host/all_gather.cc
@@ -20,128 +20,108 @@ using namespace ops_hccl_allgather;
 
 constexpr uint32_t AIV_TAG_ADDR_OFFSET = 16 * 1024;
 
-HcclResult PrepareResources(HcclComm comm, OpParam& param, aclrtStream stream) {
-    std::string aivTagStr = std::string(param.tag) + "_AIV";
-    const char* aivTag = aivTagStr.c_str();
-    
-    void* aivCommInfoPtr = nullptr;
+static HcclResult InitAivBuffer(HcclComm comm, const char* aivTag, void*& aivCommInfoPtr, HcclMemHandle& memHandle) {
     uint64_t aivCommInfoSize = AIV_TAG_BUFF_LEN;
-    HcclMemHandle memHandle;
-    
     auto hcclRet = HcclEngineCtxGet(comm, aivTag, CommEngine::COMM_ENGINE_AIV, &aivCommInfoPtr, &aivCommInfoSize);
-    HCCL_INFO("[PrepareResources] HcclEngineCtxGet ret=%d ptr=%p", hcclRet, aivCommInfoPtr);
+    
+    if (hcclRet == HCCL_SUCCESS && aivCommInfoPtr != nullptr) {
+        return HCCL_SUCCESS; 
+    }
 
-    if (hcclRet != HCCL_SUCCESS || aivCommInfoPtr == nullptr) {
-        hcclRet = HcclEngineCtxCreate(comm, aivTag, CommEngine::COMM_ENGINE_AIV, AIV_TAG_BUFF_LEN, &aivCommInfoPtr);
-        if (hcclRet != HCCL_SUCCESS) {
-            HCCL_ERROR("[PrepareResources] Failed to create AIV buffer. ret=%d", hcclRet);
-            return hcclRet;
-        }
-        HCCL_INFO("[PrepareResources] Created AIV buffer %p", aivCommInfoPtr);
-        ACLCHECK(aclrtMemset(aivCommInfoPtr, AIV_TAG_BUFF_LEN, 0, AIV_TAG_BUFF_LEN));
-        
-        CommMem regMem{COMM_MEM_TYPE_DEVICE, aivCommInfoPtr, AIV_TAG_BUFF_LEN};
-        hcclRet = HcclCommMemReg(comm, aivTag, &regMem, &memHandle);
-        if (hcclRet != HCCL_SUCCESS) {
-             HCCL_ERROR("[PrepareResources] Failed to register memory. ret=%d", hcclRet);
-             return hcclRet;
-        }
-        HCCL_INFO("[PrepareResources] Registered AIV memory handle");
+    hcclRet = HcclEngineCtxCreate(comm, aivTag, CommEngine::COMM_ENGINE_AIV, AIV_TAG_BUFF_LEN, &aivCommInfoPtr);
+    if (hcclRet != HCCL_SUCCESS) {
+        HCCL_ERROR("[%s] Failed to create AIV buffer. ret=%d", __func__, hcclRet);
+        return hcclRet;
     }
     
-    uint32_t rank = 0;
-    uint32_t rankSize = 0;
+    ACLCHECK(aclrtMemset(aivCommInfoPtr, AIV_TAG_BUFF_LEN, 0, AIV_TAG_BUFF_LEN));
+    CommMem regMem{COMM_MEM_TYPE_DEVICE, aivCommInfoPtr, AIV_TAG_BUFF_LEN};
+    hcclRet = HcclCommMemReg(comm, aivTag, &regMem, &memHandle);
+    if (hcclRet != HCCL_SUCCESS) {
+        HCCL_ERROR("[%s] Failed to register memory. ret=%d", __func__, hcclRet);
+        return hcclRet;
+    }
+    
+    return HCCL_SUCCESS;
+}
+
+static HcclResult SetupRemoteChannels(HcclComm comm, uint32_t rank, uint32_t rankSize, HcclMemHandle& memHandle, void** buffersIn, void** buffersOut) {
+    std::vector<HcclChannelDesc> channelRequests;
+    for (size_t remoteRank = 0; remoteRank < rankSize; remoteRank++) {
+        if (remoteRank == rank) continue;
+        uint32_t netLayer = 0, listSize = 0;
+        CommLink *linkList = nullptr;
+        CHK_RET(HcclRankGraphGetLinks(comm, netLayer, rank, remoteRank, &linkList, &listSize));
+        for (uint32_t idx = 0; idx < listSize; idx++) {
+            HcclChannelDesc desc;
+            HcclChannelDescInit(&desc, 1);
+            desc.memHandles = &memHandle;
+            desc.memHandleNum = 1;
+            desc.remoteRank = remoteRank;
+            desc.localEndpoint.protocol = linkList[idx].srcEndpointDesc.protocol;
+            desc.localEndpoint.commAddr = linkList[idx].srcEndpointDesc.commAddr;
+            desc.localEndpoint.loc = linkList[idx].srcEndpointDesc.loc;
+            desc.remoteEndpoint.protocol = linkList[idx].dstEndpointDesc.protocol;
+            desc.remoteEndpoint.commAddr = linkList[idx].dstEndpointDesc.commAddr;
+            desc.remoteEndpoint.loc = linkList[idx].dstEndpointDesc.loc;
+            desc.channelProtocol = linkList[idx].linkAttr.linkProtocol;
+            desc.notifyNum = 3;
+            channelRequests.push_back(desc);
+        }
+    }
+
+    uint32_t validNum = channelRequests.size();
+    std::vector<ChannelHandle> levelNChannels(validNum);
+    if (validNum > 0) {
+        CHK_RET(HcclChannelAcquire(comm, CommEngine::COMM_ENGINE_AIV, channelRequests.data(), validNum, levelNChannels.data()));
+    }
+
+    for (uint32_t idx = 0; idx < validNum; idx++) {
+        uint32_t currentRank = channelRequests[idx].remoteRank;
+        void* remoteBufferAddr = nullptr;
+        uint64_t remoteBufferSize = 0;
+        CHK_RET(HcclChannelGetHcclBuffer(comm, levelNChannels[idx], &remoteBufferAddr, &remoteBufferSize));
+        buffersIn[currentRank] = remoteBufferAddr;
+
+        uint32_t memNum = 0;
+        CommMem* remoteMems = nullptr;
+        char** memTags = nullptr;
+        CHK_RET(HcclChannelGetRemoteMems(comm, levelNChannels[idx], &memNum, &remoteMems, &memTags));
+        CHK_PRT_RET(memNum != 1, HCCL_ERROR("[%s] HcclChannelGetRemoteMems memNum not 1", __func__), HCCL_E_PARA);
+        buffersOut[currentRank] = remoteMems[0].addr;
+    }
+    return HCCL_SUCCESS;
+}
+
+HcclResult PrepareResources(HcclComm comm, OpParam& param, aclrtStream stream) {
+    std::string aivTagStr = std::string(param.tag) + "_AIV";
+    
+    void* aivCommInfoPtr = nullptr;
+    HcclMemHandle memHandle;
+    CHK_RET(InitAivBuffer(comm, aivTagStr.c_str(), aivCommInfoPtr, memHandle));
+
+    uint32_t rank = 0, rankSize = 0;
     CHK_RET(HcclGetRankId(comm, &rank));
     CHK_RET(HcclGetRankSize(comm, &rankSize));
 
-    void* cclBufferAddr;
-    uint64_t cclBufferSize;
-    // 从通信域获取CCL buffer
+    void* cclBufferAddr = nullptr;
+    uint64_t cclBufferSize = 0;
     CHK_RET(HcclGetHcclBuffer(comm, &cclBufferAddr, &cclBufferSize));
-    HCCL_INFO("[%s]local cclBufferAddr[%p] cclBufferSize[%llu]", __func__, cclBufferAddr, cclBufferSize);
 
     void* buffersIn[MAX_RANK_SIZE] = {};
     void* buffersOut[MAX_RANK_SIZE] = {};
     buffersIn[rank] = cclBufferAddr;
     buffersOut[rank] = aivCommInfoPtr;
 
-    // 获取子通信域的建链请求
-
-    std::vector<HcclChannelDesc> level0ChannelRequest;
-    level0ChannelRequest.reserve(rankSize);
-
-    for (size_t remoteRank = 0; remoteRank < rankSize; remoteRank++)
-    {
-        if (remoteRank == rank) continue;
-
-        uint32_t netLayer = 0;
-        CommLink *linkList = nullptr;
-        uint32_t listSize;
-        CHK_RET(HcclRankGraphGetLinks(comm, netLayer, rank, remoteRank, &linkList, &listSize));
-        for (uint32_t idx = 0; idx < listSize; idx++) {
-            HcclChannelDesc channelDesc;
-            HcclChannelDescInit(&channelDesc, 1);
-            channelDesc.memHandles = &memHandle;
-            channelDesc.memHandleNum = 1;
-            channelDesc.remoteRank = remoteRank;
-            CommLink link = linkList[idx];
-            channelDesc.localEndpoint.protocol = link.srcEndpointDesc.protocol;
-            channelDesc.localEndpoint.commAddr = link.srcEndpointDesc.commAddr;
-            channelDesc.localEndpoint.loc = link.srcEndpointDesc.loc;
-            channelDesc.remoteEndpoint.protocol = link.dstEndpointDesc.protocol;
-            channelDesc.remoteEndpoint.commAddr = link.dstEndpointDesc.commAddr;
-            channelDesc.remoteEndpoint.loc = link.dstEndpointDesc.loc;
-            HCCL_DEBUG("[CalcChannelRequestMesh1D] local device phyId: %u, remote device phyId: %u.",
-                        channelDesc.localEndpoint.loc.device.devPhyId,
-                        channelDesc.remoteEndpoint.loc.device.devPhyId);
-            HCCL_INFO("[CalcChannelRequestMesh1D] Add channel request between %zu and %zu, netLayerIdx %u, "
-                        "linkListIdx %u, protocol %zu",
-                        rank, channelDesc.remoteRank, netLayer, idx, channelDesc.remoteEndpoint.protocol);
-            channelDesc.channelProtocol = link.linkAttr.linkProtocol;
-            constexpr uint32_t NORMAL_NOTIFY_NUM = 3;
-            channelDesc.notifyNum = NORMAL_NOTIFY_NUM;
-            level0ChannelRequest.push_back(channelDesc);
-        }
-    }
-
-    // 获取子通信域的建链数量
-    uint32_t validChannelNum = level0ChannelRequest.size();
-    std::vector<ChannelHandle> levelNChannels;
-    levelNChannels.resize(validChannelNum);
-
-    if (validChannelNum > 0) {
-        CHK_RET(HcclChannelAcquire(comm, CommEngine::COMM_ENGINE_AIV, level0ChannelRequest.data(),
-            validChannelNum, levelNChannels.data()));
-    }
-
-    for (uint32_t idx = 0; idx < validChannelNum; idx++) {
-        HcclChannelDesc &channelDesc = level0ChannelRequest[idx];
-        void* remoteBufferAddr;
-        uint64_t remoteBufferSize;
-        CHK_RET(HcclChannelGetHcclBuffer(comm, levelNChannels[idx], &remoteBufferAddr, &remoteBufferSize));
-        HCCL_INFO("[%s]remoteRank[%u] cclBufferAddr[%p] cclBufferSize[%llu]", __func__, channelDesc.remoteRank,
-            remoteBufferAddr, remoteBufferSize);
-        buffersIn[channelDesc.remoteRank] = remoteBufferAddr;
-
-        uint32_t memNum;
-        CommMem* remoteMems;
-        char** memTags;
-        CHK_RET(HcclChannelGetRemoteMems(comm, levelNChannels[idx], &memNum, &remoteMems, &memTags));
-        CHK_PRT_RET(memNum != 1,
-            HCCL_ERROR("[%s] HcclChannelGetRemoteMems memNum[%u] not equal to 1", __func__, memNum), HCCL_E_PARA);
-        HCCL_INFO("[%s]remoteRank[%u] memNum[%u] regMemAddr[%p] regMemSize[%llu] memTag[%s]", __func__,
-            channelDesc.remoteRank, memNum, remoteMems[0].addr, remoteMems[0].size, memTags[0]);
-        buffersOut[channelDesc.remoteRank] = remoteMems[0].addr;
-    }
+    CHK_RET(SetupRemoteChannels(comm, rank, rankSize, memHandle, buffersIn, buffersOut));
 
     param.buffIn = (uint64_t)aivCommInfoPtr;
-    ACLCHECK(aclrtMemcpy(aivCommInfoPtr, MAX_RANK_SIZE * sizeof(void*), buffersIn, MAX_RANK_SIZE * sizeof(void*),
-        ACL_MEMCPY_HOST_TO_DEVICE));
+    ACLCHECK(aclrtMemcpy(aivCommInfoPtr, MAX_RANK_SIZE * sizeof(void*), buffersIn, 
+                         MAX_RANK_SIZE * sizeof(void*), ACL_MEMCPY_HOST_TO_DEVICE));
     ACLCHECK(aclrtMemcpy(static_cast<uint8_t*>(aivCommInfoPtr) + AIV_TAG_ADDR_OFFSET, MAX_RANK_SIZE * sizeof(void*),
-        buffersOut, MAX_RANK_SIZE * sizeof(void*), ACL_MEMCPY_HOST_TO_DEVICE));
+                         buffersOut, MAX_RANK_SIZE * sizeof(void*), ACL_MEMCPY_HOST_TO_DEVICE));
 
     HCCL_INFO("[%s] Alloc res success.", __func__);
-
     return HCCL_SUCCESS;
 }
 
@@ -194,12 +174,9 @@ extern "C" HcclResult HcclAllGatherCustom(void *sendBuf, void *recvBuf, uint64_t
     param.counterMemSize = 0;
     param.isEnableCounter = false;
 
-
-    
     HCCL_INFO("[HcclAllGatherCustom] Launching kernel... rank=%u rankSize=%u", rank, rankSize);
     CHK_RET(LaunchKernel(param, stream));
     HCCL_INFO("[HcclAllGatherCustom] Launch success");
     
     return HCCL_SUCCESS;
 }
-
