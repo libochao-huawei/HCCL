2d68419f910fa192aac9ee85b15b8b17f1c6d376
yanyefeng <yanyefeng@huawei.com>
2026-03-17 18:29:50 +0800

    支持hccl前向兼容处理
    Signed-off-by: yanyefeng <yanyefeng@huawei.com>

--- src/common/hcomm_dlsym/hccl_comm_dl.h
+++ src/common/hcomm_dlsym/hccl_comm_dl.h
@@ -55,6 +55,8 @@ HcclResult HcclSnapshotSave(void* snapshotBuf, uint32_t size, uint32_t step);
 HcclResult HcclSnapshotGetBufSize(uint32_t step, uint32_t* size);
 HcclResult HcclSnapshotRecoverAllComms(const char* clusterInfo, const char* changedInfo,
                                        void* snapshotBuf, uint32_t snapshotBufSize);
+HcclResult HcclGetCommHandleByCtx(void *ctx, void **opHandle);
+
 // 查询函数声明
 bool HcommIsSupportHcclCommInitClusterInfo(void);
 bool HcommIsSupportHcclCommInitClusterInfoConfig(void);
@@ -94,6 +96,7 @@ bool HcommIsSupportHcclCommInitClusterInfoMemConfig(void);
 bool HcommIsSupportHcclSnapshotSave(void);
 bool HcommIsSupportHcclSnapshotGetBufSize(void);
 bool HcommIsSupportHcclSnapshotRecoverAllComms(void);
+bool HcommIsSupportHcclGetCommHandleByCtx(void);
 
 void HcclCommDlInit(void* libHcommHandle);        // 本模块独立初始化
 void HcclCommDlFini(void);                         // 本模块独立销毁
