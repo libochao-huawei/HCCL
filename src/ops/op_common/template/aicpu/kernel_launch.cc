f30874dcf70560fd57c2a1faeed77a19ad93aaa8
weixin_58244yso <suimingcong@huawei.com>
2026-03-17 19:17:08 +0800

    位置更改

--- src/ops/op_common/template/aicpu/kernel_launch.cc
+++ src/ops/op_common/template/aicpu/kernel_launch.cc
@@ -43,7 +43,10 @@ HcclResult __attribute__((weak)) HcommProfilingReportKernelEndTask(uint64_t thre
 extern "C" unsigned int HcclLaunchAicpuKernel(OpParam *param)
 {
     HCCL_INFO("Entry-%s, commName[%s], tag[%s], algTag[%s]", __func__, param->commName, param->tag, param->algTag);
-
+    if (HcommAcquireComm(param->commName) != HCCL_SUCCESS) {
+        HCCL_ERROR("%s HcommAcquireComm fail, commName[%s]", __func__, param->commName);
+        return 1;
+    }
     #ifdef MACRO_DEV_TYPE_NEW
     if (param->deviceType != DevType::DEV_TYPE_950) {
     #else
@@ -52,11 +55,6 @@ extern "C" unsigned int HcclLaunchAicpuKernel(OpParam *param)
         ScatterOpInfo opInfo;
         if (CreateScatter(param, &opInfo) != HCCL_SUCCESS) {
             HCCL_ERROR("%s CreateScatter fail", __func__);
-            return 1;
-        }
-
-        if (HcommAcquireComm(param->commName) != HCCL_SUCCESS) {
-            HCCL_ERROR("%s HcommAcquireComm fail, commName[%s]", __func__, param->commName);
             return 1;
         }
         
