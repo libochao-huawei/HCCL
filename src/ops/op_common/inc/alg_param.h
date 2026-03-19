80821556d891595881a3db8430376389685e79cc
zhongyu1 <zhongyu37@huawei.com>
2026-03-19 21:07:10 +0800

    compile error fix

--- src/ops/op_common/inc/alg_param.h
+++ src/ops/op_common/inc/alg_param.h
@@ -252,12 +252,12 @@ struct CcuKernelInfo {
 };
 
 // 算法sqe最大个数（alltoallv除外）
-#define CCU_MAX_TASK_ARG_NUM 30;
+#define CCU_MAX_TASK_ARG_NUM 30
 
 struct CcuKernelSubmmitInfo {
     CcuKernelHandle kernelHandle;
     uint64_t sqeArgs[CCU_MAX_TASK_ARG_NUM];
-}
+};
 
 // ccu快速下发上下文
 struct CcuFastRunCtx {
@@ -267,18 +267,17 @@ struct CcuFastRunCtx {
     // ThreadHandle threadHandles[];
     // CcuKernelSubmmitInfo ccuKernelSubmmitInfo[];
 
-    ThreadHandle *GetThreadHandlePtr()
+    ThreadHandle *GetThreadHandlePtr() const
     {
-        size_t offset = offsetof(CcuFastRunCtx, ccuKernelNum)
-        return reinterpret_cast<ThreadHandle*>(reinterpret_cast<char*>(this) + offset + sizeof(u32));
-    }
-    CcuKernelSubmmitInfo *GetCcuKernelSubmmitInfoPtr()
-    {
-        size_t offset = offsetof(CcuFastRunCtx, ccuKernelNum) + sizeof(u32) + sizeof(ThreadHandle) * threadNum;
+        size_t offset = offsetof(CcuFastRunCtx, ccuKernelNum) + sizeof(u32);
         return reinterpret_cast<ThreadHandle*>(reinterpret_cast<char*>(this) + offset);
     }
-
-}
+    CcuKernelSubmmitInfo *GetCcuKernelSubmmitInfoPtr() const
+    {
+        size_t offset = offsetof(CcuFastRunCtx, ccuKernelNum) + sizeof(u32) + sizeof(ThreadHandle) * threadNum;
+        return reinterpret_cast<CcuKernelSubmmitInfo*>(reinterpret_cast<char*>(this) + offset);
+    }
+};
 
 // A5用了cntNotify
 struct AlgResourceRequest {
