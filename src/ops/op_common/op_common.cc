b57355c9fee064742c9bebd11d1561351e57d927
hechangcheng2303 <hechangcheng2@h-partners.com>
2026-03-20 14:55:08 +0800

    test

--- src/ops/op_common/op_common.cc
+++ src/ops/op_common/op_common.cc
@@ -1173,12 +1173,12 @@ HcclResult ApplyOpExpansionMode(OpParam &param, HcclOpExpansionMode finalMode)
             HCCL_DEBUG("[ApplyOpExpansionMode] AICPU mode selected.");
             break;
         case HcclOpExpansionMode::HCCL_OP_EXPANSION_AIV:
-            param.opExecuteConfig = OpExecuteConfig::AIV;
-            param.engine = CommEngine::COMM_ENGINE_AIV;
-            CHK_RET(RegisterKernel(param.opType, g_aivKernelInfoMap[param.opType].first, g_aivKernelInfoMap[param.opType].second));
-            HCCL_DEBUG("[ApplyOpExpansionMode] AIV mode selected.");
-            break;
-        case HcclOpExpansionMode::HCCL_OP_EXPANSION_AIV_ONLY:
+        //     param.opExecuteConfig = OpExecuteConfig::AIV;
+        //     param.engine = CommEngine::COMM_ENGINE_AIV;
+        //     CHK_RET(RegisterKernel(param.opType, g_aivKernelInfoMap[param.opType].first, g_aivKernelInfoMap[param.opType].second));
+        //     HCCL_DEBUG("[ApplyOpExpansionMode] AIV mode selected.");
+        //     break;
+        // case HcclOpExpansionMode::HCCL_OP_EXPANSION_AIV_ONLY:
             param.opExecuteConfig = OpExecuteConfig::AIV_ONLY;
             param.engine = CommEngine::COMM_ENGINE_AIV;
             CHK_RET(RegisterKernel(param.opType, g_aivKernelInfoMap[param.opType].first, g_aivKernelInfoMap[param.opType].second));
