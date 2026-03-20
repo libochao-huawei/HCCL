393646e4fcdb2e877acd9154a0b06bc3ddca67ff
Archerls <wuhuiyang3@huawei.com>
2026-03-20 10:37:30 +0800

    merge bugfix

--- src/ops/all_gather/all_gather_op.h
+++ src/ops/all_gather/all_gather_op.h
@@ -39,9 +39,6 @@ HcclResult AllGatherOutPlaceCommon(void *sendBuf, void *recvBuf, uint64_t sendCo
                                    aclrtStream stream, const std::string &tag, OpMode opMode, const ResPackGraphMode &resPack);
 
 HcclResult CheckAllGatherInputPara(const HcclComm comm, const void* sendBuf, const void* recvBuf, const aclrtStream stream);
-HcclResult AllGatherOutPlaceGraphMode(void *sendBuf, void *recvBuf, uint64_t sendCount, HcclDataType dataType, HcclComm comm,
-                                      aclrtStream stream, const std::string &tag, const ResPackGraphMode &resPack);
-HcclResult AllGatherInitAndCheck(HcclComm comm, void *sendBuf, void *recvBuf, uint64_t sendCount, HcclDataType dataType, aclrtStream stream, std::string &opTag);
 
 HcclResult AllGatherInitAndCheck(HcclComm comm, void *sendBuf, void *recvBuf, uint64_t sendCount, HcclDataType dataType, aclrtStream stream, std::string &opTag);
 
