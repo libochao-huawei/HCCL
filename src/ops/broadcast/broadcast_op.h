a3415912851a4cffd2453e0ccfaac43a17f32340
ssstars <ssstars@example.com>
2026-03-21 18:58:34 +0800

    Update broadcast core files (rebased on master)
    Modified files:
    - broadcast_op.cc/h: Remove GraphMode functions and external dependencies
    - executor/ins_v2_broadcast_parallel_executor.cc: +1 line
    - executor/ins_v2_broadcast_sole_executor.cc: +1 line
    - template/aicpu/ins_temp_broadcast_mesh_1D_two_shot.cc/h: enableRemoteMemAccess support
    - template/aicpu/ins_temp_broadcast_nhr.cc: enableRemoteMemAccess support
    
    Other files (CMakeLists.txt, selector) use upstream/master version.

--- src/ops/broadcast/broadcast_op.h
+++ src/ops/broadcast/broadcast_op.h
@@ -32,8 +32,6 @@ HcclResult HcclBroadcast(void *buf, uint64_t count, HcclDataType dataType, uint3
 
 namespace ops_hccl {
 HcclResult BroadcastOutPlace(void *buf, uint64_t count, HcclDataType dataType, uint32_t root, HcclComm comm, aclrtStream stream, const std::string &tag);
-
-
 HcclResult CheckBroadcastInputPara(const HcclComm comm, const void *buf);
 
 }
