6c1c8504b83fad2ec35e65b995f325e3d75919c8
jiyuanhao <jiyuanhao1@huawei.com>
2026-03-21 15:11:09 +0800

    fix

--- examples/04_custom_ops_p2p/op_host/send.cc
+++ examples/04_custom_ops_p2p/op_host/send.cc
@@ -55,9 +55,11 @@ HcclResult HcclSendCustom(
     uint64_t size = sizeof(AlgResourceCtx);
     if (HcclEngineCtxGet(comm, param.tag, engine, &ctx, &size) == HCCL_SUCCESS) {
         // device资源已经存在
+        HCCL_INFO("[HcclSendCustom] Engine context already exists");
         param.resCtx = static_cast<AlgResourceCtx *>(ctx);
     } else {
         // 不存在，新创建Context
+        HCCL_INFO("[HcclSendCustom] Creating engine context");
         CHK_RET(HcclEngineCtxCreate(comm, param.tag, engine, size, &ctx));
         param.resCtx = static_cast<AlgResourceCtx *>(ctx);
 
