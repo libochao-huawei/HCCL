55940ef062ca7d7168afcc7a90002f21d9fbb99e
Dive8 <wangchenmao1@huawei.com>
2026-03-23 20:21:30 +0800

    跨框非对称适配
    Signed-off-by: Dive8 <wangchenmao1@huawei.com>

--- src/ops/reduce_scatter/selector/reduce_scatter_auto_selector.cc
+++ src/ops/reduce_scatter/selector/reduce_scatter_auto_selector.cc
@@ -116,9 +116,6 @@ SelectorStatus ReduceScatterAutoSelector::SelectCcuScheduleAlgo(const TopoInfoWi
     if (topoInfo->topoLevelNums > 1) {
         // Level1Nhr 已在 CalcTopoShape 中设置（GCD==1 时为 true）
         if (topoInfo->Level1Nhr) {
-            CHK_PRT_RET(opParam.DataDes.dataType == HcclDataType::HCCL_DATA_TYPE_INT8,
-            HCCL_WARNING("[ReduceScatterAutoSelector] dataType[%d] is not supported yet for ccu schedule mode.",
-                opParam.DataDes.dataType), SelectorStatus::NOT_MATCH);
             selectAlgName = "CcuReduceScatterNHR1DMem2Mem";
             HCCL_INFO("[ReduceScatterAutoSelector] Level1Nhr=true, select [%s]", selectAlgName.c_str());
             return SelectorStatus::MATCH;
@@ -216,6 +213,7 @@ SelectorStatus ReduceScatterAutoSelector::SelectAicpuAlgo(const TopoInfoWithNetL
             selectAlgName = "InsReduceScatterAicpuReduceNHR";
         } else if (topoInfo->Level1Nhr) {
             selectAlgName = "InsReduceScatterNHR";
+            HCCL_INFO("[ReduceScatterAutoSelector] Level1Nhr=true, select [%s]", selectAlgName.c_str());
         } else if (topoInfo->Level0Nhr) {
             selectAlgName = "InsReduceScatterNHR"; // InsReduceScatterParallelNHRNHR备用
         } else if (topoInfo->netLayerDetails.localNetInsSizeOfLayer.at(0) > 1 && topoInfo->level0Topo == Level0Shape::MESH_1D) {
