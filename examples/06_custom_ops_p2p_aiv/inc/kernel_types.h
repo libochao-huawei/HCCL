f35317a8512fdead17142fb2885757ef4ea129c7
linzhenkang <linzhenkang@huawei.com>
2026-03-24 14:54:53 +0800

    p2p_aiv

---
+++ examples/06_custom_ops_p2p_aiv/inc/kernel_types.h
@@ -0,0 +1,48 @@
+﻿/**
+ * Copyright (c) 2025 Huawei Technologies Co., Ltd.
+ * This program is free software, you can redistribute it and/or modify it under the terms and conditions of
+ * CANN Open Software License Agreement Version 2.0 (the "License").
+ * Please refer to the License for details. You may not use this file except in compliance with the License.
+ * THIS SOFTWARE IS PROVIDED ON AN "AS IS" BASIS, WITHOUT WARRANTIES OF ANY KIND, EITHER EXPRESS OR IMPLIED,
+ * INCLUDING BUT NOT LIMITED TO NON-INFRINGEMENT, MERCHANTABILITY, OR FITNESS FOR A PARTICULAR PURPOSE.
+ * See LICENSE in the root of the software repository for the full text of the License.
+ */
+
+#ifndef OPS_HCCL_P2P_AIV_KERNEL_TYPES_H
+#define OPS_HCCL_P2P_AIV_KERNEL_TYPES_H
+
+#include <cstdint>
+
+namespace ops_hccl_p2p_aiv {
+
+constexpr uint32_t kP2pAivTaskSend = 0;
+constexpr uint32_t kP2pAivTaskRecv = 1;
+constexpr uint32_t kP2pAivTagValue = 1;
+constexpr uint32_t kP2pAivKernelLaunchTimeoutSec = 1800;
+constexpr uint32_t kP2pAivKernelBlockNum = 1;
+constexpr uint64_t kP2pAivCommInfoSize = 2 * 1024 * 1024;
+constexpr uint64_t kP2pAivReadyFlagOffset = 0;
+constexpr uint64_t kP2pAivDoneFlagOffset = 64;
+constexpr uint64_t kP2pAivFlagAreaSize = 128;
+constexpr uint32_t kP2pAivKernelCtxVersion = 1;
+
+struct P2pAivKernelParam {
+    uint32_t version = kP2pAivKernelCtxVersion;
+    uint32_t taskType = 0;
+    uint32_t dataType = 0;
+    uint32_t rank = 0;
+    uint32_t peerRank = 0;
+    uint32_t blockNum = kP2pAivKernelBlockNum;
+    uint32_t tag = kP2pAivTagValue;
+    uint64_t lenBytes = 0;
+    uint64_t inputAddr = 0;
+    uint64_t outputAddr = 0;
+    uint64_t localBufferAddr = 0;
+    uint64_t remoteBufferAddr = 0;
+    uint64_t localCommInfoAddr = 0;
+    uint64_t remoteCommInfoAddr = 0;
+};
+
+} // namespace ops_hccl_p2p_aiv
+
+#endif // OPS_HCCL_P2P_AIV_KERNEL_TYPES_H
---
+++ examples/06_custom_ops_p2p_aiv/op_kernel/p2p_send_recv_kernel.h
@@ -0,0 +1,43 @@
+﻿#ifndef OPS_HCCL_P2P_AIV_SEND_RECV_KERNEL_H
+#define OPS_HCCL_P2P_AIV_SEND_RECV_KERNEL_H
+
+#include "p2p_aiv_kernel_base.h"
+
+namespace ops_hccl_p2p_aiv {
+
+template <typename T>
+class P2pSendRecvKernel : public P2pAivKernelBase {
+public:
+    __aicore__ inline void Run(const P2pAivKernelParam &param)
+    {
+        Init(param);
+        if (param.taskType == kP2pAivTaskSend) {
+            RunSend();
+            return;
+        }
+        if (param.taskType == kP2pAivTaskRecv) {
+            RunRecv();
+        }
+    }
+
+private:
+    __aicore__ inline void RunSend()
+    {
+        const uint64_t count = param_.lenBytes / sizeof(T);
+        CopyGmToGm(reinterpret_cast<__gm__ T *>(param_.localBufferAddr), reinterpret_cast<__gm__ T *>(param_.inputAddr), count);
+        RecordFlag(kP2pAivReadyFlagOffset);
+        WaitFlag(kP2pAivDoneFlagOffset);
+    }
+
+    __aicore__ inline void RunRecv()
+    {
+        WaitFlag(kP2pAivReadyFlagOffset);
+        const uint64_t count = param_.lenBytes / sizeof(T);
+        CopyGmToGm(reinterpret_cast<__gm__ T *>(param_.outputAddr), reinterpret_cast<__gm__ T *>(param_.remoteBufferAddr), count);
+        RecordFlag(kP2pAivDoneFlagOffset);
+    }
+};
+
+} // namespace ops_hccl_p2p_aiv
+
+#endif // OPS_HCCL_P2P_AIV_SEND_RECV_KERNEL_H
