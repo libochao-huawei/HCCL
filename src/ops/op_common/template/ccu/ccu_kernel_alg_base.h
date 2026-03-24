e496ed560252d23270979de66671b4eb7d1213fe
a1847373419 <zhengyuntao2@huawei.com>
2026-03-24 10:32:13 +0800

    A6 algbase

--- src/ops/op_common/template/ccu/ccu_kernel_alg_base.h
+++ src/ops/op_common/template/ccu/ccu_kernel_alg_base.h
@@ -17,11 +17,21 @@
 namespace ops_hccl {
 using namespace hcomm;
 
+constexpr uint64_t CCU_LOOP_NUM_V2 = 8;
+constexpr uint64_t SINGLE_SLICE_MS_NUM = 1;
+constexpr uint64_t CCU_LOOP_LEN_V2 = CcuRep::CCU_MS_SIZE * SINGLE_SLICE_MS_NUM;
+constexpr uint64_t CCU_MAX_RANKSIZE_V2 = 8;
+constexpr uint64_t CCU_MS_INTERLEAVE_V2 = CCU_MAX_RANKSIZE_V2 * SINGLE_SLICE_MS_NUM;
+constexpr uint64_t CCU_LOOP_CKE_NUM_BCAST_V2 = 2; 
+constexpr uint64_t CCU_LOOP_CKE_NUM_REDUCE_V2 = 3;
+
 /* hccl仓CcuKernel基类，提供group高阶操作接口 */
 class CcuKernelAlgBase : public CcuKernel {
 public:
     // 继承所有构造函数
     using CcuKernel::CcuKernel;
+    uint64_t ccu_version;
+    enum CcuVersion{CCU_INVALID,CCU_V1,CCU_V2};
 
 protected:
     // 编程接口
@@ -77,8 +87,18 @@ protected:
 
     HcclResult GroupBroadcast(const std::vector<ChannelHandle>& channels, std::vector<CcuRep::RemoteAddr> dst,
                               CcuRep::LocalAddr src, GroupOpSize goSize);
+    HcclResult GroupBroadcastV1(const std::vector<ChannelHandle>& channels, std::vector<CcuRep::RemoteAddr> dst,
+                              CcuRep::LocalAddr src, GroupOpSize goSize);
+    HcclResult GroupBroadcastV2(const std::vector<ChannelHandle>& channels, std::vector<CcuRep::RemoteAddr> dst,
+                              CcuRep::LocalAddr src, GroupOpSize goSize);
 
     HcclResult GroupReduce(const std::vector<ChannelHandle>& channels, CcuRep::LocalAddr dst,
+                           std::vector<CcuRep::RemoteAddr> src, GroupOpSize goSize, HcclDataType dataType,
+                           HcclDataType outputDataType, HcclReduceOp opType);
+    HcclResult GroupReduceV1(const std::vector<ChannelHandle>& channels, CcuRep::LocalAddr dst,
+                           std::vector<CcuRep::RemoteAddr> src, GroupOpSize goSize, HcclDataType dataType,
+                           HcclDataType outputDataType, HcclReduceOp opType);
+    HcclResult GroupReduceV2(const std::vector<ChannelHandle>& channels, CcuRep::LocalAddr dst,
                            std::vector<CcuRep::RemoteAddr> src, GroupOpSize goSize, HcclDataType dataType,
                            HcclDataType outputDataType, HcclReduceOp opType);
 
@@ -86,8 +106,14 @@ protected:
 
 private:
     HcclResult CreateMultiOpCopy();
-    HcclResult CreateMultiOpBroadcast(const std::vector<ChannelHandle> &channels);
-    HcclResult CreateMultiOpReduce(const std::vector<ChannelHandle>& channels, HcclDataType dataType,
+
+    HcclResult CreateMultiOpBroadcastV1(const std::vector<ChannelHandle> &channels);
+    HcclResult CreateMultiOpBroadcastV2(const std::vector<ChannelHandle> &channels);
+
+
+    HcclResult CreateMultiOpReduceV1(const std::vector<ChannelHandle>& channels, HcclDataType dataType,
+                                   HcclDataType outputDataType, HcclReduceOp opType);
+    HcclResult CreateMultiOpReduceV2(const std::vector<ChannelHandle>& channels, HcclDataType dataType,
                                    HcclDataType outputDataType, HcclReduceOp opType);
 };
 
