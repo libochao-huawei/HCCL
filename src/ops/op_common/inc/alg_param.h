a17eebf7937ccdd9d002692699aaf4d423b9a4bc
zhongyu1 <zhongyu37@huawei.com>
2026-03-24 15:25:55 +0800

    reduce scatter parallel fast launch

--- src/ops/op_common/inc/alg_param.h
+++ src/ops/op_common/inc/alg_param.h
@@ -438,6 +438,7 @@ struct OpParam { // 不申请ctx，每个算子单独下发
     u64 inputSize = 0;
     void* outputPtr = nullptr;
     u64 outputSize = 0;
+    HcclMem hcclBuff;   // 当前仅快速下发时使用此处的地址
     HcclReduceOp reduceType = HcclReduceOp::HCCL_REDUCE_RESERVED;
     u32 root = INVALID_VALUE_RANKID;
     u32 sendRecvRemoteRank = INVALID_VALUE_RANKID;
