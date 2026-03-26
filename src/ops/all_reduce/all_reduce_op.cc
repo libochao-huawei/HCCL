4c24cd19af79181b80bf534e5992f5fe9e34dbfb
zuyu <zhangzuyu3@h-partners.com>
2026-03-26 20:36:51 +0800

    allreduce detour

--- src/ops/all_reduce/all_reduce_op.cc
+++ src/ops/all_reduce/all_reduce_op.cc
@@ -122,7 +122,7 @@ HcclResult AllReduceOutPlace(void *sendBuf, void *recvBuf, uint64_t count, HcclD
     param.DataDes.count = count;
     param.DataDes.dataType = dataType;
     param.opType = HcclCMDType::HCCL_CMD_ALLREDUCE;
-    param.enableDetour = false;
+    param.detourType = GetExternalInputHcclDetourType();
     param.deviceType = deviceType;
     
     std::string algName;
