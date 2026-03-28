db93bdce8288db733d5b3b66eae171d5ff596810
yanzhenghang <625612333@qq.com>
2026-03-28 11:15:51 +0800

    rm dpu etc.

--- src/ops/all_to_all_v/all_to_all_v_op.cc
+++ src/ops/all_to_all_v/all_to_all_v_op.cc
@@ -322,7 +322,11 @@ HcclResult AlltoAllVOutPlace(const void *sendBuf, const void *sendCounts, const 
 
     u32 userRankSize;
     CHK_RET(HcclGetRankSize(comm, &userRankSize));
+
     u64 varMemSize = ALL_TO_ALL_V_VECTOR_NUM * userRankSize * sizeof(u64);
+    void *paramMem = malloc(sizeof(OpParam) + varMemSize);
+    if (!paramMem) {
+        // 内存分配失败
         HCCL_ERROR("[AlltoAllVOutPlace] malloc OpParam failed!");
         return HCCL_E_INTERNAL;
     }
@@ -400,5 +404,13 @@ HcclResult AlltoAllVOutPlace(const void *sendBuf, const void *sendCounts, const 
     if (userRankSize == 1) {
         HCCL_WARNING("[%s] ranksize == 1, enter SingleRankProc", __func__);
         CHK_RET(SingleRankProc(param));
+        return HcclResult::HCCL_SUCCESS;
+    }
+    std::string algName;
+    std::unique_ptr<TopoInfoWithNetLayerDetails> topoInfo = std::make_unique<TopoInfoWithNetLayerDetails>();
+    CHK_RET(Selector(comm, param, topoInfo, algName));
+    CHK_RET(HcclExecOp(comm, param, topoInfo, algName));
+    HCCL_INFO("Execute AlltoAllVOutPlace success.");
+    return HCCL_SUCCESS;
 }
 }
