2ab8d803a36328b19ec7fe97fa4e5353f2af6e39
lijuntao26 <lijuntao17@h-partners.com>
2026-03-28 14:55:53 +0800

    fix hccl st build

--- test/st/algorithm/testcase/main.cc
+++ test/st/algorithm/testcase/main.cc
@@ -10,9 +10,11 @@
 
 #include <iostream>
 #include "gtest/gtest.h"
+#include "hccl_dl_stub.h"
 
 GTEST_API_ int main(int argc, char **argv)
 {
+    InitAllDlStubFunc();
     std::cout << "Start to run demo for hccl_checker_ops_stest." << std::endl;
     // testing::GTEST_FLAG(filter) = "ST_REDUCE_SCATTER_TEST.test_host_dpu_reducescatter_020";
     testing::InitGoogleTest(&argc, argv);
