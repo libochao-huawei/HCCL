c6d42309798bf9a14983dcfc72c1653924837da9
tangjingjuan <tangjingjuan@huawei.com>
2026-03-31 10:16:28 +0800

    测试Write模式第二种方案：统一rank写入顺序，保证所有节点reduce计算顺序一致

--- src/ops/op_common/template/ccu/ccu_kernel_alg_base.cc
+++ src/ops/op_common/template/ccu/ccu_kernel_alg_base.cc
@@ -36,6 +36,7 @@ void CcuKernelAlgBase::AllocGoResource(uint32_t parallelDim, uint32_t msPerLoop)
     moConfig.loopCount = parallelDim;
     // 算法配置的msPerLoop * CcuRep::CCU_MS_SIZE覆盖默认配置，msPerLoop默认为1
     moConfig.memSlice = msPerLoop * CcuRep::CCU_MS_SIZE;
+    moConfig.msInterleave = 9;
     HCCL_INFO("[AllocGoResource]moConfig: loopCount = %u, msInterleave = %u", moConfig.loopCount, moConfig.msInterleave);
 
     // 简单实现,只需要申请一次资源
@@ -868,7 +869,8 @@ HcclResult CcuKernelAlgBase::CreateMultiOpWrite(const std::vector<ChannelHandle>
 {
     uint32_t channelSize = channels.size();
     uint32_t size        = channelSize + 1; // N-1 peers + self
-    // moConfig.msInterleave = size + 1;
+    moConfig.msInterleave = size + 1;
+    HCCL_INFO("CreateMultiOpWrite, channelSize: %d, size: %d, msInterleave: %d", channelSize, size, moConfig.msInterleave);
     AllocGoResource();
 
     std::string loopType = GetReduceTypeStr(dataType, opType) + "_write";
@@ -888,42 +890,39 @@ HcclResult CcuKernelAlgBase::CreateMultiOpWrite(const std::vector<ChannelHandle>
         lb(src, dst, len, lenForExpansion);
 
         std::vector<CcuRep::CcuBuf> bufs = {moRes.ccuBuf.begin() + index * (moConfig.msInterleave),
-                                               moRes.ccuBuf.begin() + index * (moConfig.msInterleave) + usedBufNum};
+                                               moRes.ccuBuf.begin() + index * (moConfig.msInterleave) + usedBufNum+1};
         CcuRep::CompletedEvent &event = moRes.completedEvent[index];
 
         uint32_t ckeIdx = (index == 0) ? 2 : 3;
 
+        // 方案一：本端数据搬运到最后一个ms，往其他rank写数据时根据rankId写入对应ms。
+        // event.mask = 1;
+        // LocalCopyNb(bufs[bufs.size() - 1], src, len, event);
+        // WaitEvent(event);
+
+        // // Step 4: 发送 bufs[rankId] 到所有 peers（对称 MS 分配，远端也写入 bufs[rankId]）
+        // for (uint32_t i = 0; i < channels.size(); i++) {
+        //     if (i < rankId) {
+        //         CHK_RET(MsWriteNb(channels[i], bufs[bufs.size() - 1], bufs[rankId - 1], len, ckeIdx, WRITE_DONE_MASK));
+        //     } else {
+        //         CHK_RET(MsWriteNb(channels[i], bufs[bufs.size() - 1], bufs[rankId], len, ckeIdx, WRITE_DONE_MASK));
+        //     }
+        // }
+
+        // **********************************
+        // 方案二，保证各个端计算顺序一致
+        // Step 1: 本端 HBM → bufs[rankId]（每个 rank 固定占用自己的 MS slot）
         event.mask = 1;
         LocalCopyNb(bufs[bufs.size() - 1], src, len, event);
         WaitEvent(event);
 
-        // Step 4: 发送 bufs[rankId] 到所有 peers（对称 MS 分配，远端也写入 bufs[rankId]）
         for (uint32_t i = 0; i < channels.size(); i++) {
-            if (i < rankId) {
-                CHK_RET(MsWriteNb(channels[i], bufs[bufs.size() - 1], bufs[rankId - 1], len, ckeIdx, WRITE_DONE_MASK));
-            } else {
-                CHK_RET(MsWriteNb(channels[i], bufs[bufs.size() - 1], bufs[rankId], len, ckeIdx, WRITE_DONE_MASK));
-            }
+            CHK_RET(MsWriteNb(channels[i], bufs[bufs.size() - 1], bufs[rankId], len, ckeIdx, WRITE_DONE_MASK));
         }
 
-        // // Step 1: 本端 HBM → bufs[rankId]（每个 rank 固定占用自己的 MS slot）
-        // event.mask = 1;
-        // LocalCopyNb(bufs[rankId], src, len, event);
-        // WaitEvent(event);
-
-        // if (rankId == 0) {
-        //     event.mask = 1;
-        //     LocalCopyNb(bufs[bufs.size() - 1], src, len, event);
-        //     WaitEvent(event);
-        //     for (uint32_t i = 0; i < channels.size(); i++) {
-        //         CHK_RET(MsWriteNb(channels[i], bufs[bufs.size() - 1], bufs[rankId], len, ckeIdx, WRITE_DONE_MASK));
-        //     }
-        // } else {
-        //     // Step 4: 发送 bufs[rankId] 到所有 peers（对称 MS 分配，远端也写入 bufs[rankId]）
-        //     for (uint32_t i = 0; i < channels.size(); i++) {
-        //         CHK_RET(MsWriteNb(channels[i], bufs[rankId], bufs[rankId], len, ckeIdx, WRITE_DONE_MASK));
-        //     }
-        // }
+        event.mask = 1;
+        LocalCopyNb(bufs[rankId], src, len, event);
+        WaitEvent(event);
 
         // Step 5: 等待每个 peer 的写完成通知（WRITE_DONE，bit 2）
         for (uint32_t i = 0; i < channels.size(); i++) {
