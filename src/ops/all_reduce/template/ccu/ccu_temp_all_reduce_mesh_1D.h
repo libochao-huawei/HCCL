bdf8f77641e1784e88305f0cae3fc6e1e9d99cb2
zhangtao_1 <zhangtao210@huawei.com>
2026-04-01 16:54:41 +0800

    allreduce oneshot快速下发

--- src/ops/all_reduce/template/ccu/ccu_temp_all_reduce_mesh_1D.h
+++ src/ops/all_reduce/template/ccu/ccu_temp_all_reduce_mesh_1D.h
@@ -18,6 +18,7 @@ namespace ops_hccl {
 
 class CcuTempAllReduceMesh1D : public CcuAlgTemplateBase {
 public:
+    CcuTempAllReduceMesh1D() = default;
     explicit CcuTempAllReduceMesh1D(const OpParam& param, 
                                     const u32 rankId, // 传通信域的rankId，userRank
                                     const std::vector<std::vector<u32>> &subCommRanks);
@@ -34,6 +35,8 @@ public:
     HcclResult KernelRun(const OpParam& param,
                          const TemplateDataParams& templateDataParams,
                          TemplateResource& templateResource) override;
+    HcclResult FastLaunch(const OpParam& param, const TemplateFastLaunchCtx& tempFastLaunchCtx) override;
+
 private:
     HcclResult CalcSliceInfo(const u64 dataSize, RankSliceInfo &sliceInfoVec);
     HcclResult CheckCcuDataType() const;
