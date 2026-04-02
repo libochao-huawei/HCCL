048d20ba980373e278854f94ef37066487723bf5
zuyu <zhangzuyu3@h-partners.com>
2026-04-02 17:42:59 +0800

    allreduce fast launch parallel

--- src/ops/all_reduce/template/ccu/ccu_temp_all_reduce_mesh_1D_mem2mem.h
+++ src/ops/all_reduce/template/ccu/ccu_temp_all_reduce_mesh_1D_mem2mem.h
@@ -33,6 +33,7 @@ public:
     HcclResult KernelRun(const OpParam& param,
                          const TemplateDataParams& templateDataParams,
                          TemplateResource& templateResource) override;
+    HcclResult FastLaunch(const OpParam& param, const TemplateFastLaunchCtx& tempFastLaunchCtx) override;
     u64 CalcScratchMultiple(BufferType inBuffType, BufferType outBuffType) override;
     u64 GetThreadNum() const override;
     HcclResult GetRes(AlgResourceRequest& resourceRequest) const override;
