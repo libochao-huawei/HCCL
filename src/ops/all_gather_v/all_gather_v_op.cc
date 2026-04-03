be5c439b42d66939e30516e237a73e458c4e22ec
z00850967 <zhusijie5@huawei.com>
2026-04-03 16:40:03 +0800

    fix bug

--- src/ops/all_gather_v/all_gather_v_op.cc
+++ src/ops/all_gather_v/all_gather_v_op.cc
@@ -283,10 +283,8 @@ HcclResult AllGatherVOutPlaceGraphMode(void *sendBuf, void *recvBuf, uint64_t se
  	}
  	  	 
  	// 参数准备
- 	param.inputPtr = sendBuf, param.inputSize = inputSize, param.outputPtr = recvBuf, param.outputSize = outputSize, param.DataDes.count = sendCount, param.vDataDes.dataType = dataType;
-    // 带V算子的参数
     const void *temp = recvCounts;
-    param.varMemSize = varMemSize, param.vDataDes.counts = const_cast<void*>(temp);
+ 	param.inputPtr = sendBuf, param.inputSize = inputSize, param.outputPtr = recvBuf, param.outputSize = outputSize, param.DataDes.count = sendCount, param.vDataDes.dataType = dataType, param.varMemSize = varMemSize, param.vDataDes.counts = const_cast<void*>(temp);
     // 从源内存地址按字节直接拷贝数据到目标地址
     std::vector<u64> merged(userRankSize + userRankSize); 
     const uint64_t *countsPtr = reinterpret_cast<const uint64_t *>(recvCounts);
