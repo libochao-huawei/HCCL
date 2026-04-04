9812176ea5a65d8871283552952efd05a56ad394
SeasonChu <zhuxincheng2@huawei.com>
2026-04-04 18:20:55 +0800

    Add Alloc Com Resource By Tiling

---
+++ test/ut/hccl_mc2/dtype_common.h
@@ -0,0 +1,6 @@
+#ifndef HCCL_MC2_UT_DTYPE_COMMON_BRIDGE_H
+#define HCCL_MC2_UT_DTYPE_COMMON_BRIDGE_H
+
+#include <hccl/dtype_common.h>
+
+#endif
---
+++ test/ut/hccl_mc2/hccl_res.h
@@ -0,0 +1,6 @@
+#ifndef HCCL_MC2_UT_HCCL_RES_BRIDGE_H
+#define HCCL_MC2_UT_HCCL_RES_BRIDGE_H
+
+#include <hccl/hccl_res.h>
+
+#endif
