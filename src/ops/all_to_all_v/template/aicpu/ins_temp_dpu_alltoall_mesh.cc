b906abfe1b5af2740aee5a1e549af61529f93e79
shiqiwen1 <shiqiwen@huawei.com>
2026-04-07 11:46:19 +0800

    修改格式

--- src/ops/all_to_all_v/template/aicpu/ins_temp_dpu_alltoall_mesh.cc
+++ src/ops/all_to_all_v/template/aicpu/ins_temp_dpu_alltoall_mesh.cc
@@ -36,7 +36,9 @@ HcclResult InsTempDpuAlltoAllMesh::CalcRes(HcclComm comm, const OpParam &param, 
         CHK_RET(CalcChannelRequestMesh1D(comm, param, topoInfo, subCommRanks_, level0Channels));
 
     } else {
-        CHK_PRT_RET(subCommRanks_.size() != NET_NUM, HCCL_ERROR("[InsTempDpuAlltoAllMesh][CalcRes] subCommRankNum[%zu] is not [%u]", subCommRanks_.size(), NET_NUM), HCCL_E_PARA);
+        CHK_PRT_RET(subCommRanks_.size() != NET_NUM,
+                    HCCL_ERROR("[InsTempDpuAlltoAllMesh][CalcRes] subCommRankNum[%zu] is not [%u]",
+                    subCommRanks_.size(), NET_NUM), HCCL_E_PARA);
         u32 intraRankNum = subCommRanks_[0].size();
         threadNum = (intraRankNum > 1) ? (intraRankNum - 1) : 1;
         subCommRanks_ = {subCommRanks_[1]};
