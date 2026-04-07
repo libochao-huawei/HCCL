daefef07fc4df3a0bf0df7feabc9582c5bee5880
shiqiwen1 <shiqiwen@huawei.com>
2026-04-07 11:46:19 +0800

    修改格式

--- src/ops/all_to_all_v/executor/ins_v2_all_to_all_v_sole_executor.cc
+++ src/ops/all_to_all_v/executor/ins_v2_all_to_all_v_sole_executor.cc
@@ -50,12 +50,20 @@ HcclResult InsV2AlltoAllVSoleExecutor<AlgTopoMatch, InsAlgTemplate>::CalcRes(
 {
     std::vector<std::vector<u32>> tempAlgHierachyInfo;
     if (topoInfo->level0Topo == Level0Shape::MESH_1D_CLOS) {
-        CHK_PRT_RET(algHierarchyInfo.infos[0].size() != INST_NUM_NET, HCCL_ERROR("[InsV2AlltoAllVSoleExecutor][CalcRes] algHierarchyInfo.infos[0].size [%zu] with Level0Topo[%u] is not %u", algHierarchyInfo.infos[0].size(), topoInfo->level0Topo, INST_NUM_NET), HCCL_E_PARA);
-
+        CHK_PRT_RET(algHierarchyInfo.infos[0].size() != INST_NUM_NET,
+                    HCCL_ERROR("[InsV2AlltoAllVSoleExecutor][CalcRes] algHierarchyInfo.infos[0].size[%zu] "
+                        "with Level0Topo[%u] is not %u",
+                        algHierarchyInfo.infos[0].size(), topoInfo->level0Topo, INST_NUM_NET),
+                    HCCL_E_PARA);
         if (topoInfo->topoLevelNums == 1 || param.engine == CommEngine::COMM_ENGINE_AIV || param.engine == CommEngine::COMM_ENGINE_CCU){
             tempAlgHierachyInfo.push_back(algHierarchyInfo.infos[1][0]);
         } else {
-            CHK_PRT_RET(algHierarchyInfo.infos[0][1].size() >= algHierarchyInfo.infos[1][0].size(), HCCL_ERROR("[InsV2AlltoAllVSoleExecutor][CalcRes] ranknum [%zu] in Layer0 with Level0Topo[%u] is no greater than ranknum [%zu] in Layer1", algHierarchyInfo.infos[0][1].size(), topoInfo->level0Topo, algHierarchyInfo.infos[1][0].size()), HCCL_E_PARA);
+            CHK_PRT_RET(algHierarchyInfo.infos[0][1].size() >= algHierarchyInfo.infos[1][0].size(),
+                        HCCL_ERROR("[InsV2AlltoAllVSoleExecutor][CalcRes] ranknum [%zu] in Layer0 with Level0Topo[%u] "
+                                   "should be smaller than ranknum [%zu] in Layer1",
+                                   algHierarchyInfo.infos[0][1].size(), topoInfo->level0Topo,
+                                   algHierarchyInfo.infos[1][0].size()),
+                        HCCL_E_PARA);
             tempAlgHierachyInfo.push_back(algHierarchyInfo.infos[0][1]); // 跨框时，增加框内通信域，用于AICPU框内申请流资源
             tempAlgHierachyInfo.push_back(algHierarchyInfo.infos[1][0]);
         }
