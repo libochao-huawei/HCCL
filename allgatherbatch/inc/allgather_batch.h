3c14c37f103843ad6d933948ee60e4d7c86c1572
linzhenkang <linzhenkang@huawei.com>
2026-04-08 14:12:54 +0800

    add

--- allgatherbatch/inc/allgather_batch.h
+++ allgatherbatch/inc/allgather_batch.h
@@ -11,8 +11,6 @@
 extern "C" {
 #endif
 
-// 对外暴露的 item 描述符。
-// sendCount 的单位是 dataType 对应的元素个数，而不是字节数。
 typedef struct HcclAllGatherItemDef {
     void *sendBuf;
     void *recvBuf;
@@ -20,8 +18,6 @@ typedef struct HcclAllGatherItemDef {
     HcclDataType dataType;
 } HcclAllGatherItem;
 
-// 把多个 AllGather 负载合并成一次自定义算子调用。
-// 当前实现主要面向 A3 和同一 superpod 内的多 server 场景。
 HcclResult HcclAllGatherBatch(
     const HcclAllGatherItem *items, uint32_t itemCount, HcclComm comm, aclrtStream stream);
 
--- allgatherbatch/op_kernel_aicpu/exec_op.cc
+++ allgatherbatch/op_kernel_aicpu/exec_op.cc
@@ -1,4 +1,4 @@
-﻿#include "exec_op.h"
+#include "exec_op.h"
 
 #include "allgather_batch_small_count_executor.h"
 
@@ -7,7 +7,6 @@ namespace ops_hccl_allgatherbatch {
 HcclResult ExecOp(const OpParam &param, AlgResourceCtx *resCtx)
 {
     HCCL_CHK_PTR(resCtx);
-    // ExecOp 本身不承载算法细节，只负责做最薄的一层参数转发。
     AllGatherBatchSmallCountExecutor executor(param, *resCtx);
     return executor.Orchestrate();
 }
