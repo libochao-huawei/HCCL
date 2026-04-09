5a7b179342b4963ff811a75f00024e45b2ae50f1
ethWeek1 <penghaohang@huawei.com>
2026-04-09 15:55:18 +0800

    fix

--- src/ops/all_gather/template/ccu/ccu_temp_all_gather_mesh_1D_detour.cc
+++ src/ops/all_gather/template/ccu/ccu_temp_all_gather_mesh_1D_detour.cc
@@ -11,8 +11,8 @@
 #include "channel.h"
 #include "hccl_ccu_res.h"
 #include "ccu_assist_pub.h"
-#include "ccu_kernel_all_gather_mesh1d.h"
-#include "ccu_temp_all_gather_mesh_1D.h"
+#include "ccu_kernel_all_gather_mesh1d_detour.h"
+#include "ccu_temp_all_gather_mesh_1D_detour.h"
 
 namespace ops_hccl {
 
