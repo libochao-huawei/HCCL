8ab582fc20691947e29a467bf5400a8bd3d8d712
ethWeek1 <penghaohang@huawei.com>
2026-04-09 15:48:35 +0800

    fix

--- src/ops/all_gather/executor/ins_v2_all_gather_sole_executor.cc
+++ src/ops/all_gather/executor/ins_v2_all_gather_sole_executor.cc
@@ -19,7 +19,7 @@
 #include "ccu_temp_all_gather_2dies_mesh_1d_mem2mem.h"
 #include "ccu_temp_all_gather_2dies_mesh_1D.h"
 #include "ccu_temp_all_gather_nhr_1D_multi_jetty_mem2mem.h"
-#include "ccu_temp_all_gather_mesh_1D detour.h"
+#include "ccu_temp_all_gather_mesh_1D_detour.h"
 #endif
 #include "topo_match_ubx.h"
 namespace ops_hccl {
