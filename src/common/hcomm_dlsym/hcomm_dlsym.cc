c94283de4d98fed6663ab13319e9a5a8f6d48381
shabai <liyanze5@h-partners.com>
2026-04-09 17:19:32 +0800

    update

--- src/common/hcomm_dlsym/hcomm_dlsym.cc
+++ src/common/hcomm_dlsym/hcomm_dlsym.cc
@@ -15,7 +15,6 @@
 #include "hccl_inner_dl.h"
 #include "hcomm_host_profiling_dl.h"
 #include "hccl_host_comm_dl.h"
-#include "hccl_device_comm_dl.h"
 #include <pthread.h>
 #include <dlfcn.h>
 #include <stdio.h>
