d4b923361c01220b289937953d5882f0a33bacd2
shabai <liyanze5@h-partners.com>
2026-04-09 16:37:33 +0800

    update

--- src/ops/op_common/template/aicpu/kernel_launch.cc
+++ src/ops/op_common/template/aicpu/kernel_launch.cc
@@ -23,7 +23,7 @@
 #include <unordered_map>
 #include <shared_mutex>
 #include <atomic>
-#include "hccl/hccl_comm.h"
+#include <hccl/hccl_comm.h>
 #include "hccl_diag.h"
 #include "hccl_device_comm_dl.h"
 
