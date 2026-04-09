a0d8e7944457f7a5376f75467743b7b0f8662e63
shabai <liyanze5@h-partners.com>
2026-04-09 15:49:01 +0800

    update

--- src/ops/op_common/template/aicpu/kernel_launch.cc
+++ src/ops/op_common/template/aicpu/kernel_launch.cc
@@ -23,9 +23,8 @@
 #include <unordered_map>
 #include <shared_mutex>
 #include <atomic>
-#include "hccl/hccl_comm.h"
 #include "hccl_diag.h"
-
+#include "hccl_device_comm_dl.h"
 
 using namespace ops_hccl;
 namespace {
