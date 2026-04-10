4245895fb5a592b3c6cf3161931fe1d1dcb03d13
shabai <liyanze5@h-partners.com>
2026-04-10 09:29:25 +0800

    update

--- src/ops/op_common/template/aicpu/kernel_launch.cc
+++ src/ops/op_common/template/aicpu/kernel_launch.cc
@@ -265,7 +265,6 @@ extern "C" unsigned int HcclLaunchAicpuKernel(OpParam *param)
         HcommReleaseComm(param->commName);
         HcclCommStatus commStatus = HCCL_COMM_STATUS_INVALID;
         auto statusRet = HcclCommGetStatus(param->commName, &commStatus);
-        HCCL_ERROR("hccldeviceHcclCommGetStatus,commStatus = %d", static_cast<int>(commStatus));
         if (statusRet != HCCL_SUCCESS) {
             HCCL_ERROR("%s HcclCommGetStatus fail, commName[%s], ret = %d", __func__, param->commName, statusRet);
             return 1;
