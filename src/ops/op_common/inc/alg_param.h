56c7b43039f451eb2f03e937e7eacefd75974d05
x30067372 <xujiahui27@h-partners.com>
2026-04-14 14:51:02 +0800

    c接口适配

--- src/ops/op_common/inc/alg_param.h
+++ src/ops/op_common/inc/alg_param.h
@@ -16,6 +16,7 @@
 #include <map>
 #include <set>
 #include <unordered_set>
+#include <memory>
 #include <functional>
 #include <functional>
 #include <memory>
@@ -28,11 +29,7 @@
 #include "hccl_rank_graph_dl.h"
 #include "hccl_host_comm_dl.h"
 #include "binary_stream.h"
-#if CANN_VERSION_NUM >= 90000000
-#include "hccl_ccu_res.h"
-#else
-typedef void *CcuKernelHandle; // 8.5.0 下无 hccl_ccu_res.h，用 opaque 占位
-#endif
+#include "ccu_types.h"
 
 namespace ops_hccl {
 
@@ -71,7 +68,7 @@ constexpr u64 ALL_GATHER_V_VECTOR_NUM = 2;
 constexpr uint64_t GE_PARALLEL = 36;
 
 constexpr uint64_t AICPU_ALIGN_SIZE = 4096;
-// Z axis detour 需要
+constexpr uint64_t CCU_MAX_RANK_SIZE = 16;
 constexpr u32 MESH_CHANNELS_NUM = 1;
 
 enum class TopoType {
@@ -262,15 +259,31 @@ struct TopoInfoWithNetLayerDetails : public TopoInfo { // 通信域拓扑ctx
     }
 };
 
+struct CcuKernelArgBase {
+    // std::vector<ChannelHandle> channels;
+    ChannelHandle channels[CCU_MAX_RANK_SIZE];
+    uint32_t      channelCount;
+};
+
 // ccu kernel register所需信息
 struct CcuKernelInfo {
     // kernel资源组序号，group号不同时，资源复用
     u32 resGroup = 0;
-char *kernelFuncName;
-    CcuKernelFunc kernelFunc;
+char kernelFuncName[64];
+    void* kernelFunc;
     void *kernelArg;
     // kernel所需channel
     std::vector<HcclChannelDesc> channels;
+
+private:
+    std::shared_ptr<CcuKernelArgBase> kernelArgSmartPtr;
+
+public:
+    template<typename T>
+    void setKernelArg(std::shared_ptr<T> arg) {
+        kernelArgSmartPtr = std::static_pointer_cast<CcuKernelArgBase>(arg);
+        kernelArg = static_cast<void*>(arg.get());
+    }
 };
 
 // 算法taskArg入参最大个数，用于快速下发缓存
