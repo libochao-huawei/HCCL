dc1172eaec8e2429fa7eb494bd2340a31464c5d7
ethWeek1 <penghaohang@huawei.com>
2026-04-15 16:29:29 +0800

    fix

--- src/ops/broadcast/broadcast_op.cc
+++ src/ops/broadcast/broadcast_op.cc
@@ -220,7 +220,7 @@ HcclResult BroadcastOutPlace(OpParam &param, void *buf, uint64_t count, HcclData
     param.DataDes.dataType = dataType;
     param.root = root;
     param.opType = HcclCMDType::HCCL_CMD_BROADCAST;
-    param.enableDetour = false;
+    param.detourType = GetExternalInputHcclDetourType();
     param.deviceType = deviceType;
 
     CcuFastLaunchCtx *ccuFastLaunchCtx = nullptr;
