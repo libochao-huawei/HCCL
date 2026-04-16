f1677ce2e709d63c4cadd00bcc781a377759696b
shirokawa_ <jinliuyi@huawei.com>
2026-04-16 19:48:29 +0800

    add

--- src/ops/interface_graph_mode/calc_resource_graph_mode.cc
+++ src/ops/interface_graph_mode/calc_resource_graph_mode.cc
@@ -93,8 +93,7 @@ HcclResult HcclSetOpParamGraphModeHCCLBufferSize(OpParamGraphMode *opParam, cons
     memcpy_s(&paramPtr->hcclBufferSize, sizeof(paramPtr->hcclBufferSize), hcclBufferSize, sizeof(u64));
     return HCCL_SUCCESS;
 }
-HcclResult HcclSetAivSelectOpParamGraphMode(OpParamGraphMode *opParam, const char *group, u64 count, void *counts, 
- 	                                        HcclDataType dataType, HcclReduceOp op, HcclCMDType opTypeAiv, u32 aivCoreLimit, bool ifAiv)
+HcclResult HcclSetAivSelectOpParamGraphMode(OpParamGraphMode *opParam, u32 aivCoreLimit)
 {
     if (opParam == nullptr || group == nullptr) {
         return HCCL_E_PARA;
@@ -102,13 +101,7 @@ HcclResult HcclSetAivSelectOpParamGraphMode(OpParamGraphMode *opParam, const cha
     // 将void*转换为OpParamGraphMode*
     OpParamGraphMode *paramPtr = reinterpret_cast<OpParamGraphMode *>(opParam);
     strncpy_s(paramPtr->group, sizeof(paramPtr->group), group, sizeof(paramPtr->group) - 1);
-    paramPtr->count = count;
-    paramPtr->counts = counts;
-    paramPtr->dataType = dataType;
-    paramPtr->op = op;
-    paramPtr->opTypeAiv = opTypeAiv;
     paramPtr->aivCoreLimit = aivCoreLimit;
-    paramPtr->ifAiv = ifAiv;
     return HCCL_SUCCESS;
 }
 
