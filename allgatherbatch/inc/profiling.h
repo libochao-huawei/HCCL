cfb2050c5dfdf48eb7134e74d6a9ebafdcd69576
linzhenkang <linzhenkang@huawei.com>
2026-04-16 21:03:19 +0800

    add prof

--- allgatherbatch/inc/profiling.h
+++ allgatherbatch/inc/profiling.h
@@ -47,16 +47,50 @@ extern "C" HCCL_ALLGATHERBATCH_WEAK HcclResult HcommProfilingReportMainStreamAnd
 extern "C" HCCL_ALLGATHERBATCH_WEAK HcclResult HcommProfilingReportDeviceHcclOpInfo(HcomProInfoTmp profInfo);
 extern "C" HCCL_ALLGATHERBATCH_WEAK HcclResult HcommProfilingEnd(ThreadHandle *threads, uint32_t threadNum);
 
-inline bool IsProfilingEnabled()
+inline bool IsHostProfilingEnabled()
 {
     return (&HcommGetProfilingSysCycleTime != nullptr) &&
         (&HcommProfilingReportOp != nullptr) &&
-        (&HcommProfilingReportKernel != nullptr) &&
+        (&HcommProfilingReportKernel != nullptr);
+}
+
+inline bool IsDeviceProfilingEnabled()
+{
+    return (&HcommGetProfilingSysCycleTime != nullptr) &&
         (&HcommProfilingInit != nullptr) &&
         (&HcommProfilingReportMainStreamAndFirstTask != nullptr) &&
         (&HcommProfilingReportMainStreamAndLastTask != nullptr) &&
         (&HcommProfilingReportDeviceHcclOpInfo != nullptr) &&
         (&HcommProfilingEnd != nullptr);
+}
+
+inline bool IsProfilingEnabled()
+{
+    return IsHostProfilingEnabled() || IsDeviceProfilingEnabled();
+}
+
+inline uint32_t BuildProfilingThreadList(
+    const AlgResourceCtx &resCtx,
+    ThreadHandle *threads,
+    uint32_t maxThreadNum)
+{
+    if (threads == nullptr || maxThreadNum == 0 || resCtx.mainThreadHandle == 0) {
+        return 0;
+    }
+
+    uint32_t threadNum = 0;
+    threads[threadNum++] = resCtx.mainThreadHandle;
+
+    uint32_t workerCount = resCtx.lastTwoWorkerCount;
+    if (workerCount > SubThreadNum) {
+        workerCount = SubThreadNum;
+    }
+    for (uint32_t idx = 0; idx < workerCount && threadNum < maxThreadNum; ++idx) {
+        if (resCtx.subThreadHandles[idx] != 0) {
+            threads[threadNum++] = resCtx.subThreadHandles[idx];
+        }
+    }
+    return threadNum;
 }
 
 inline void CopyProfilingString(char *dst, size_t dstSize, const char *src)
