88d78e5560457002db0df52126eb779b6367f582
hechangcheng2303 <hechangcheng2@h-partners.com>
2026-04-16 19:15:51 +0800

    test

--- src/ops/all_to_all_v/template/aiv/kernel/aiv_communication_v2_alltoallv.h
+++ src/ops/all_to_all_v/template/aiv/kernel/aiv_communication_v2_alltoallv.h
@@ -24,7 +24,7 @@ EXPORT_AIV_META_INFO(aiv_alltoallv_##type)
 
 #define AIV_OMNI_KERNEL_BATCH_DEF(type) \
 extern "C" __global__ __aicore__ void omni_aiv_##type(EXTERN_KERNEL_ARGS_DEF_V2) { \
-    AivOmniV2Entry<type>(EXTERN_KERNEL_ARGS_CALL); \
+    return; \
 }                                               \
 EXPORT_AIV_META_INFO(omni_aiv_##type)
 
