d312753d90624ed632463468b84152aa997c15e6
yjzz1007 <zhaojiayu7@huawei.com>
2026-04-20 19:29:59 +0800

    ccu_customized AR&RS bugfix

--- src/ops/reduce_scatter/selector/reduce_scatter_auto_selector.cc
+++ src/ops/reduce_scatter/selector/reduce_scatter_auto_selector.cc
@@ -171,6 +171,8 @@ SelectorStatus ReduceScatterAutoSelector::SelectMeshAlgoCcuSchedule(const TopoIn
         }
     } else if (topoInfo->level0Topo == Level0Shape::MESH_1D_CLOS) {
         // UBX机型
+        CHK_PRT_RET(opParam.DataDes.dataType == HcclDataType::HCCL_DATA_TYPE_INT8, HCCL_WARNING("[ReduceScatterAutoSelector] dataType[%d] is "
+                "not supported yet for ccu_schedule mode with ms reduce.", opParam.DataDes.dataType), SelectorStatus::NOT_MATCH);
         bool isMeshNumEqualToClosNum = false;
         bool isClosNumMultipleOfMeshNum = false;
         CHK_PRT_RET(CheckMeshNumEqualToClosNum(topoInfo, isMeshNumEqualToClosNum) != HCCL_SUCCESS,
