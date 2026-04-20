cd54096a4a570d11cf2ce1f973bb09b224e2474f
yjzz1007 <zhaojiayu7@huawei.com>
2026-04-20 20:50:28 +0800

    bugfix

--- src/ops/all_reduce/selector/all_reduce_auto_selector.cc
+++ src/ops/all_reduce/selector/all_reduce_auto_selector.cc
@@ -164,9 +164,6 @@ SelectorStatus AllReduceAutoSelector::SelectCcuScheduleAlgo(const TopoInfoWithNe
             return SelectorStatus::NOT_MATCH;
         }
     } else {
-    CHK_PRT_RET(opParam.DataDes.dataType == HcclDataType::HCCL_DATA_TYPE_INT8,
-        HCCL_DEBUG("[AllReduceAutoSelector] dataType[%d] is not supported yet for ccu schedule mode "
-                    "with ms reduce.", opParam.DataDes.dataType), SelectorStatus::NOT_MATCH);
         return SelectCcuScheduleLevel0Algo(topoInfo, opParam, selectAlgName, dataSize);
     }
     HCCL_DEBUG("[AllReduceAutoSelector][%s] Algo match [%s]", __func__, selectAlgName.c_str());
@@ -236,6 +233,9 @@ SelectorStatus AllReduceAutoSelector::SelectCcuScheduleLevel0Algo(const TopoInfo
         }
         return SelectorStatus::MATCH;
     } else if (topoInfo->level0Topo == Level0Shape::MESH_1D_CLOS) {
+        CHK_PRT_RET(opParam.DataDes.dataType == HcclDataType::HCCL_DATA_TYPE_INT8,
+            HCCL_DEBUG("[AllReduceAutoSelector] dataType[%d] is not supported yet for ccu schedule mode "
+                    "with ms reduce.", opParam.DataDes.dataType), SelectorStatus::NOT_MATCH);
         return SelectCcuScheduleLevel0UBXAlgo(topoInfo, selectAlgName, dataSize);
     } else {
         HCCL_DEBUG("[AllReduceAutoSelector] level0Topo[%d] is not supported yet for ccu schedule mode.",
