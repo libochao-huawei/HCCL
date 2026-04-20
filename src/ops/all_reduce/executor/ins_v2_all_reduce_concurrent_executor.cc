780d6c0c245887a31289c3af27810438ae8ef446
yjzz1007 <zhaojiayu7@huawei.com>
2026-04-20 19:29:59 +0800

    ccu_customized AR&RS bugfix

--- src/ops/all_reduce/executor/ins_v2_all_reduce_concurrent_executor.cc
+++ src/ops/all_reduce/executor/ins_v2_all_reduce_concurrent_executor.cc
@@ -136,7 +136,7 @@ HcclResult InsV2AllReduceConcurrentExecutor<AlgTopoMatch, InsAlgTemplate0, InsAl
                                               resReq0.ccuKernelInfos.end());
         resourceRequest.ccuKernelInfos.insert(resourceRequest.ccuKernelInfos.end(), resReq1.ccuKernelInfos.begin(),
                                               resReq1.ccuKernelInfos.end());
-    } else if (param.engine == CommEngine::COMM_ENGINE_AICPU) {
+    } else if (param.engine == CommEngine::COMM_ENGINE_AICPU || param.engine == CommEngine::COMM_ENGINE_AICPU_TS) {
         // 都放在level0，前面放temp0的channels，后面放temp1的channels，两者数量应相等
         resourceRequest.channels.resize(1);
         resourceRequest.channels[0].insert(resourceRequest.channels[0].end(), channelDescs0.begin(),
--- src/ops/all_reduce/selector/all_reduce_auto_selector.cc
+++ src/ops/all_reduce/selector/all_reduce_auto_selector.cc
@@ -174,6 +174,9 @@ SelectorStatus AllReduceAutoSelector::SelectCcuScheduleLevel0UBXAlgo(const TopoI
     std::string &selectAlgName, const u64 dataSize) const
 {
     // UBX机型
+    CHK_PRT_RET(opParam.DataDes.dataType == HcclDataType::HCCL_DATA_TYPE_INT8,
+        HCCL_DEBUG("[AllReduceAutoSelector] dataType[%d] is not supported yet for ccu schedule mode "
+                    "with ms reduce.", opParam.DataDes.dataType), SelectorStatus::NOT_MATCH);
     bool isMeshNumEqualToClosNum = false;
     bool isClosNumMultipleOfMeshNum = false;
     CHK_PRT_RET(CheckMeshNumEqualToClosNum(topoInfo, isMeshNumEqualToClosNum) != HCCL_SUCCESS,
