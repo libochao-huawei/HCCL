1ec5bb7d236c620207a32ec5f1220254946a65ae
yjzz1007 <zhaojiayu7@huawei.com>
2026-04-21 17:29:00 +0800

    aicpu_ts add

--- src/ops/all_reduce/executor/ins_v2_all_reduce_concurrent_executor.cc
+++ src/ops/all_reduce/executor/ins_v2_all_reduce_concurrent_executor.cc
@@ -252,7 +252,7 @@ HcclResult InsV2AllReduceConcurrentExecutor<AlgTopoMatch, InsAlgTemplate0, InsAl
         // AIV模式
         tempAlgResource0.aivCommInfoPtr = resCtx.aivCommInfoPtr;
         tempAlgResource1.aivCommInfoPtr = resCtx.aivCommInfoPtr;
-    } else if (param.engine == CommEngine::COMM_ENGINE_AICPU) {
+    } else if (param.engine == CommEngine::COMM_ENGINE_AICPU || param.engine == CommEngine::COMM_ENGINE_AICPU_TS) {
         // AICPU模式
         const auto &channels = resCtx.channels[0];
         const size_t channelCount = channels.size();
