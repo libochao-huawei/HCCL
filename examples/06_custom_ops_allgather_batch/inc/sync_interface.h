230e703e954356625573d201504634eaa4fa5817
scarlett_Xie <xiehuijiao@huawei.com>
2026-04-21 11:21:25 +0800

    Make batch sync interface self-contained

--- examples/06_custom_ops_allgather_batch/inc/sync_interface.h
+++ examples/06_custom_ops_allgather_batch/inc/sync_interface.h
@@ -1,6 +1,10 @@
 #ifndef OPS_HCCL_CUSTOM_ALLGATHER_BATCH_SYNC_INTERFACE_H
 #define OPS_HCCL_CUSTOM_ALLGATHER_BATCH_SYNC_INTERFACE_H
 
-#include "../../05_custom_ops_allgather/inc/sync_interface.h"
+#include "kernel_operator.h"
+
+using namespace AscendC;
+
+constexpr uint64_t UB_FLAG_PAD_COUNT = 8;
 
 #endif
