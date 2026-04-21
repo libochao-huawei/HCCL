7708af0310479b7c6b17fb94397975086eb4f716
wlwy <wulinwenyuan@huawei.com>
2026-04-21 17:15:25 +0800

    tag12

--- src/ops/op_common/template/wrapper/dpu_alg_data_trans_wrapper.cc
+++ src/ops/op_common/template/wrapper/dpu_alg_data_trans_wrapper.cc
@@ -25,9 +25,9 @@ HcclResult SendRecvWrite(const SendRecvInfo &sendRecvInfo)
     // 向write rank发送tx同步，确保该rank的hcclBuffer可用
     // 这里只是在host上向device下任务，所以实际在host侧不会因为wait而阻塞
     CHK_RET(static_cast<HcclResult>(HcommChannelNotifyRecordOnThread(0, recvChannel.handle, NOTIFY_IDX_ACK,
-        sendRecvInfo.myRankId, sendRecvInfo.aicpuTaskId)));
+        sendRecvInfo.aicpuTaskId)));
     CHK_RET(static_cast<HcclResult>(HcommChannelNotifyWaitOnThread(0, sendChannel.handle, NOTIFY_IDX_ACK, DPU_TIMEOUT,
-        sendRecvInfo.myRankId, sendRecvInfo.aicpuTaskId)));
+        sendRecvInfo.aicpuTaskId)));
     for (int i = 0; i < repeatNum; i++) {
         // tx同步完成后准备将自己的userIn上的数据写到对方的hcclBuffer上
         const DataSlice srcSlice = srcSlices[i];
@@ -36,16 +36,16 @@ HcclResult SendRecvWrite(const SendRecvInfo &sendRecvInfo)
         void *src = static_cast<void *>(static_cast<s8 *>(srcSlice.addr_) + srcSlice.offset_);
         CHK_RET(static_cast<HcclResult>(
             HcommWriteWithNotifyNbiOnThread(0, sendChannel.handle, dst, src, srcSlice.size_, NOTIFY_IDX_DATA_SIGNAL,
-                sendRecvInfo.myRankId, sendRecvInfo.aicpuTaskId)));
+                sendRecvInfo.aicpuTaskId)));
         CHK_RET(static_cast<HcclResult>(
             HcommChannelNotifyWaitOnThread(0, recvChannel.handle, NOTIFY_IDX_DATA_SIGNAL, DPU_TIMEOUT,
-                sendRecvInfo.myRankId, sendRecvInfo.aicpuTaskId)));
+                sendRecvInfo.aicpuTaskId)));
     }
     // 写完之后做后同步告诉对面写完了
     CHK_RET(static_cast<HcclResult>(HcommChannelNotifyRecordOnThread(0, sendChannel.handle, NOTIFY_IDX_FIN_ACK,
-        sendRecvInfo.myRankId, sendRecvInfo.aicpuTaskId)));
+        sendRecvInfo.aicpuTaskId)));
     CHK_RET(static_cast<HcclResult>(HcommChannelNotifyWaitOnThread(0, recvChannel.handle, NOTIFY_IDX_FIN_ACK, DPU_TIMEOUT,
-        sendRecvInfo.myRankId, sendRecvInfo.aicpuTaskId)));
+        sendRecvInfo.aicpuTaskId)));
     CHK_RET(static_cast<HcclResult>(HcommChannelFenceOnThread(0, recvChannel.handle)));
     CHK_RET(static_cast<HcclResult>(HcommFenceOnThread(0)));
 #endif
