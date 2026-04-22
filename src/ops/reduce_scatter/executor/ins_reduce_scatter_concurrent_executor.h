73071023a985481b265a7fe937904bb430488903
yjzz1007 <zhaojiayu7@huawei.com>
2026-04-22 11:26:50 +0800

    fastlaunch

--- src/ops/reduce_scatter/executor/ins_reduce_scatter_concurrent_executor.h
+++ src/ops/reduce_scatter/executor/ins_reduce_scatter_concurrent_executor.h
@@ -32,10 +32,10 @@ public:
     HcclResult CalcAlgHierarchyInfo(HcclComm comm, TopoInfoWithNetLayerDetails* topoInfo,
                                     AlgHierarchyInfoForAllLevel& algHierarchyInfo) override;
 
-                                    #ifndef AICPU_COMPILE
+#ifndef AICPU_COMPILE
     HcclResult FastLaunch(const OpParam &param, const CcuFastLaunchCtx *resCtx) override;
-    HcclResult FastLaunchSaveCtx(const OpParam &param, const TemplateResource &templateAlgResIntra,
-                                 const TemplateResource &templateAlgResInter);
+    HcclResult FastLaunchSaveCtx(const OpParam &param, const TemplateResource &templateAlgRes0,
+                                 const TemplateResource &templateAlgRes1);
 #endif
 
 protected:
