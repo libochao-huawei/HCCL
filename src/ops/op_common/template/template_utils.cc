b57d7a652892ceed3c4ee04ba1cf55126ab95ad5
xiaocui235 <cuiruxing@huawei.com>
2026-04-22 11:20:43 +0800

    recover

--- src/ops/op_common/template/template_utils.cc
+++ src/ops/op_common/template/template_utils.cc
@@ -31,47 +31,47 @@ u32 GetNHRStepNum(u32 rankSize)
     return nSteps;
 }
 
-// HcclResult CalcDataSplitByPortGroup(const u64 totalDataCount, const u64 dataTypeSize,
-//                                     const std::vector<ChannelInfo> &channels,
-//                                     std::vector<u64> &elemCountOut, std::vector<u64> &sizeOut,
-//                                     std::vector<u64> &elemOffset)
-// {
-//     elemCountOut.clear();
-//     sizeOut.clear();
-//     elemOffset.clear();
+HcclResult CalcDataSplitByPortGroup(const u64 totalDataCount, const u64 dataTypeSize,
+                                    const std::vector<ChannelInfo> &channels,
+                                    std::vector<u64> &elemCountOut, std::vector<u64> &sizeOut,
+                                    std::vector<u64> &elemOffset)
+{
+    elemCountOut.clear();
+    sizeOut.clear();
+    elemOffset.clear();
 
-//     std::vector<u32> portGroups;
-//     u32 totalPorts = 0;
-//     for (const auto &ch : channels) {
-//         portGroups.push_back(ch.portGroupSize);
-//         totalPorts += ch.portGroupSize;
-//         HCCL_INFO("[CalcDataSplitByPortGroup] GenExtIns ch.portGroupSize[%u], totalPorts[%u]",
-//                   ch.portGroupSize, totalPorts);
-//     }
+    std::vector<u32> portGroups;
+    u32 totalPorts = 0;
+    for (const auto &ch : channels) {
+        portGroups.push_back(ch.portGroupSize);
+        totalPorts += ch.portGroupSize;
+        HCCL_INFO("[CalcDataSplitByPortGroup] GenExtIns ch.portGroupSize[%u], totalPorts[%u]",
+                  ch.portGroupSize, totalPorts);
+    }
 
-//     u32 channelsize = portGroups.size();
-//     u64 accumCount = 0;
-//     u64 offset = 0;
-//     for (u32 channelIdx = 0; channelIdx < channelsize; channelIdx++) {
-//         u64 elemCount = 0;
-//         u64 elemSize = 0;
-//         if (channelIdx == channelsize - 1) {
-//             elemCount = totalDataCount - accumCount;
-//         } else {
-//             CHK_PRT_RET(totalPorts == 0,
-//                         HCCL_ERROR("[CalcDataSplitByPortGroup] totalPorts [%u] is 0.", totalPorts),
-//                         HcclResult::HCCL_E_INTERNAL);
-//             elemCount = static_cast<u64>((totalDataCount * portGroups[channelIdx]) / totalPorts);
-//         }
-//         elemOffset.push_back(offset);
-//         elemCountOut.push_back(elemCount);
-//         elemSize = elemCount * dataTypeSize;
-//         sizeOut.push_back(elemSize);
-//         offset += elemSize;
-//         accumCount += elemCount;
-//     }
+    u32 channelsize = portGroups.size();
+    u64 accumCount = 0;
+    u64 offset = 0;
+    for (u32 channelIdx = 0; channelIdx < channelsize; channelIdx++) {
+        u64 elemCount = 0;
+        u64 elemSize = 0;
+        if (channelIdx == channelsize - 1) {
+            elemCount = totalDataCount - accumCount;
+        } else {
+            CHK_PRT_RET(totalPorts == 0,
+                        HCCL_ERROR("[CalcDataSplitByPortGroup] totalPorts [%u] is 0.", totalPorts),
+                        HcclResult::HCCL_E_INTERNAL);
+            elemCount = static_cast<u64>((totalDataCount * portGroups[channelIdx]) / totalPorts);
+        }
+        elemOffset.push_back(offset);
+        elemCountOut.push_back(elemCount);
+        elemSize = elemCount * dataTypeSize;
+        sizeOut.push_back(elemSize);
+        offset += elemSize;
+        accumCount += elemCount;
+    }
 
-//     return HcclResult::HCCL_SUCCESS;
-// }
+    return HcclResult::HCCL_SUCCESS;
+}
 
 }
