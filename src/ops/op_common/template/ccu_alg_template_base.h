81410aef79a9b5f9f0f3043fefcac90702ea99c7
xiaocui235 <cuiruxing@huawei.com>
2026-04-22 10:23:43 +0800

    remove some space

--- src/ops/op_common/template/ccu_alg_template_base.h
+++ src/ops/op_common/template/ccu_alg_template_base.h
@@ -36,7 +36,7 @@ public:
                                  
     virtual HcclResult GetRes(AlgResourceRequest& resourceRequest) const override;
     virtual u64 GetThreadNum() const override;
-    
+
     virtual u64 CalcScratchMultiple(BufferType inBuffType, BufferType outBuffType) override;
 
     uint64_t PointerToAddr(void* pointer) const;
