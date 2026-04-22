69c764b8b1945583c5d58df10cf6ea609113937e
ethWeek1 <penghaohang@huawei.com>
2026-04-22 11:49:15 +0800

    fix

--- src/ops/all_to_all_v/template/ccu/ccu_temp_all_to_all_mesh1d_2Die.cc
+++ src/ops/all_to_all_v/template/ccu/ccu_temp_all_to_all_mesh1d_2Die.cc
@@ -142,7 +142,7 @@ HcclResult CcuTempAllToAllMesh1D2Die::KernelRun(const OpParam &param, const Temp
 
     for (uint32_t dieId = 0; dieId < DIE_NUM; dieId++) {    // 2Die算法，需要执行两次
         std::unique_ptr<hcomm::CcuTaskArg> taskArg = std::make_unique<CcuTaskArgAllToAllMesh2Die>(
-            inputAddr, outputAddr, sliceSize, token, inputSliceStride, outputSliceStride, outBuffBaseOff);
+            inputAddr, outputAddr, token, sliceSize, inputSliceStride, outputSliceStride);
         void *taskArgPtr = static_cast<void *>(taskArg.get());
         CHK_RET(HcclCcuKernelLaunch(param.hcclComm, templateResource.threads[dieId], templateResource.ccuKernels[dieId],
             taskArgPtr));
