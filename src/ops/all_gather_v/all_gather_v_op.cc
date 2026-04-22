160d5a109178060ac44da8b5c2ef85ef5e75fda7
weixin_43960572 <yuantangzhi@huawei.com>
2026-04-22 14:25:03 +0800

    Timeout time is controlled by using environment variables (HCCL_EXEC_TIMEOUT).

--- src/ops/all_gather_v/all_gather_v_op.cc
+++ src/ops/all_gather_v/all_gather_v_op.cc
@@ -22,9 +22,6 @@ extern "C" unsigned int LaunchAicpuKernel(OpParam *param);
 HcclResult HcclAllGatherV(void *sendBuf, uint64_t sendCount, void *recvBuf, const void *recvCounts,
     const void *recvDispls, HcclDataType dataType, HcclComm comm, aclrtStream stream)
 {
-    if (!HcclCheckAicpuEnableOpen() && !HcclCheckCcuEnableOpen() && !HcclCheckAivEnableOpen()) {
-        return HcclAllGatherVInner(sendBuf, sendCount, recvBuf, recvCounts, recvDispls, dataType, comm, stream);
-    }
     HCCL_INFO("Start to run execute HcclAllGatherV");
     if (GetHcommVersion() < 90000000) { // compat handle
         return HcclAllGatherVInner(sendBuf, sendCount, recvBuf, recvCounts, recvDispls, dataType, comm, stream);
