d9c55bfbf869639d5b3a36f62b4524fe4509b6fa
renyumin <renyumin1@huawei.com>
2026-04-25 17:37:50 +0800

    dlsym

---
+++ src/common/hcomm_dlsym/hccl_res_exp_dl.h
@@ -0,0 +1,31 @@
+/**
+ * Copyright (c) 2026 Huawei Technologies Co., Ltd.
+ * This program is free software, you can redistribute it and/or modify it under the terms and conditions of
+ * CANN Open Software License Agreement Version 2.0 (the "License").
+ * Please refer to the License for details. You may not use this file except in compliance with the License.
+ * THIS SOFTWARE IS PROVIDED ON AN "AS IS" BASIS, WITHOUT WARRANTIES OF ANY KIND, EITHER EXPRESS OR IMPLIED,
+ * INCLUDING BUT NOT LIMITED TO NON-INFRINGEMENT, MERCHANTABILITY, OR FITNESS FOR A PARTICULAR PURPOSE.
+ * See LICENSE in the root of the software repository for the full text of the License.
+ */
+
+#ifndef HCCL_RES_EXP_DL_H
+#define HCCL_RES_EXP_DL_H
+
+#include "dlsym_common.h"
+#include "hccl_res_exp.h"
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+DECL_SUPPORT_FLAG(HcclCommAddExchangeInfo);
+DECL_SUPPORT_FLAG(HcclCommGetExchangeInfo);
+
+// 动态库管理接口（大驼峰命名）
+void HcclResExpDlInit(void *libHcommHandle);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif // HCCL_RES_EXP_DL_H
