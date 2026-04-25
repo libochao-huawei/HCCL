86e1e0490eaf6a7039c426837e2dda5e96e722a5
renyumin <renyumin1@huawei.com>
2026-04-25 17:37:50 +0800

    dlsym

---
+++ src/common/hcomm_dlsym/hccl_res_exp_dl.cc
@@ -0,0 +1,24 @@
+/**
+ * Copyright (c) 2026 Huawei Technologies Co., Ltd.
+ * This program is free software, you can redistribute it and/or modify it under the terms and conditions of
+ * CANN Open Software License Agreement Version 2.0 (the "License").
+ * Please refer to the License for details. You may not use this file except in compliance with the License.
+ * THIS SOFTWARE IS PROVIDED ON AN "AS IS" BASIS, WITHOUT WARRANTIES OF ANY KIND, EITHER EXPRESS OR IMPLIED,
+ * INCLUDING BUT NOT LIMITED TO NON-INFRINGEMENT, MERCHANTABILITY, OR FITNESS FOR A PARTICULAR PURPOSE.
+ * See LICENSE in the root of the software repository for the full text of the License.
+ */
+
+#include "hccl_res_exp_dl.h"
+#include <dlfcn.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+// ---------- 桩函数定义（签名与真实API完全一致）----------
+DEFINE_WEAK_FUNC(HcclResult, HcclCommAddExchangeInfo, HcclComm comm, void *data, uint32_t length);
+DEFINE_WEAK_FUNC(HcclResult, HcclCommGetExchangeInfo, HcclComm comm, uint32_t remoteRank, void *data, uint32_t &length);
+
+// 初始化
+void HcclResExpDlInit(void *libHcommHandle) {
+    INIT_SUPPORT_FLAG(libHcommHandle, HcclCommAddExchangeInfo);
+    INIT_SUPPORT_FLAG(libHcommHandle, HcclCommGetExchangeInfo);
+}
