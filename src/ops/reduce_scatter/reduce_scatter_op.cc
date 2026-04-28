59a699a7c6f8e5d48d7e6adaac72c0d992136497
weixin_43960572 <yuantangzhi@huawei.com>
2026-04-28 09:25:32 +0800

    decrease notify in AG/RS
    Co-authored-by: weixin_43960572<yuantangzhi@huawei.com>
    
    !686 merge repeat into master
    
    decrease notify in AG/RS
    
    Created-by: weixin_43960572
    Commit-by: weixin_43960572
    Merged-by: cann-robot
    Description: ## 描述
    <!--在这里详细描述你的改动，包括改动的原因和所采取的方法。-->
    decrease notify in AG/RS
    
    <!-- 如果这个PR是为了解决特定的Issue，请在这里提供Issue链接。例如：关联Issue #123-->
    <!-- 如果这个PR不涉及Issue，可填写"NA"。-->
    
    <!--描述进行了哪些测试来验证你的改动。包括但不限于构造对应xx测试用例、二级冒烟、算子泛化等。-->
    
    <!--如果这个PR包含文档的更新，请在这里指出。例如：更新了README.md文件。-->
    
    <!--  [x] 表示选中 -->
    - [ ] Bug修复
    - [x] 新特性
    - [ ] 性能优化
    - [ ] 文档更新
    - [ ] 其他，请描述：
    
    See merge request: cann/hccl!686

--- src/ops/reduce_scatter/reduce_scatter_op.cc
+++ src/ops/reduce_scatter/reduce_scatter_op.cc
@@ -246,4 +246,153 @@ HcclResult ReduceScatterOutPlaceGraphMode(void *sendBuf, void *recvBuf, uint64_t
     HCCL_INFO("Execute ReduceScatterOutPlaceGraphMode success.");
     return HCCL_SUCCESS;
 }
+
+TEST_F(HcommCcuControlApiTest, Ut_HcommCcuKernelRegister_When_AllFine_Expect_ReturnCcuSUCCESS)
+ 	 {
+ 	     // 整体打桩，处理ccu资源
+ 	     HcommResult hcclRet = 0;
+ 	     CcuResult ccuRet = CcuResult::CCU_E_RESERVED;
+ 	     constexpr uint32_t fakeDevId = MAX_MODULE_DEVICE_NUM - 2;
+ 	     MOCKER(HcclGetThreadDeviceId).stubs().will(returnValue(fakeDevId));
+ 	     int32_t fakeDeviceLogicId = static_cast<int32_t>(fakeDevId);
+ 	     MOCKER(hrtGetDevice).stubs()
+ 	         .with(outBoundP(&fakeDeviceLogicId))
+ 	         .will(returnValue(HcclResult::HCCL_SUCCESS));
+ 	     MOCKER(hrtGetDevicePhyIdByIndex).stubs()
+ 	         .with(any(), outBound(static_cast<uint32_t>(fakeDeviceLogicId)), any())
+ 	         .will(returnValue(HcclResult::HCCL_SUCCESS));
+ 	     constexpr hcomm::CcuVersion fakeCcuVersion = hcomm::CcuVersion::CCU_V1;
+ 	     MockCcuNetworkDeviceDefault(fakeDeviceLogicId); // 先处理网络设备，再初始化ccu
+ 	     EXPECT_EQ(MockCcuResourcesDefault(fakeDeviceLogicId, fakeCcuVersion), HcclResult::HCCL_SUCCESS);
+ 	     MockCcuChannelGetRes();
+ 	     MOCKER(hrtMemcpy).stubs().will(returnValue(HcclResult::HCCL_SUCCESS));
+ 	 
+ 	     // ccuInstance构建（正常在通信域创建中，本用例仅测试hcomm接口）
+ 	     constexpr auto MS_INS_TPYE = CcuInstanceType::CCU_MS;
+ 	     auto insType = MS_INS_TPYE;
+ 	     void *ccuResDesc = static_cast<void *>(&insType);
+ 	     CcuInsHandle insHandle{0};
+ 	     ccuRet = HcommCcuInsCreate(ccuResDesc, &insHandle);
+ 	     EXPECT_EQ(ccuRet, CcuResult::CCU_SUCCESS);
+ 	 
+ 	     // 建链流程打桩
+ 	     constexpr auto commEngine = CommEngine::COMM_ENGINE_CCU;
+ 	     constexpr uint32_t srcDevPhyId = fakeDevId;
+ 	     constexpr uint32_t dstDevPhyId = 1;
+ 	     constexpr uint32_t srcIp = 167772383; // 需要与RaGetDevEidInfoList接口打桩一致
+ 	     constexpr uint32_t dstIp = 0x87654321;
+ 	     const auto &handlePair = MockCcuChannelConnect(srcDevPhyId, dstDevPhyId, srcIp, dstIp, commEngine);
+ 	 
+ 	     // 构造CcuKernel实现
+ 	     CcuKernelFunc demoFunc = CcuEventKernel;
+ 	     CcuVarAddKernelArg demoArg{};
+ 	     demoArg.numA = 1;
+ 	     demoArg.numB = 2;
+ 	     demoArg.channelHandle = handlePair.second;
+ 	     auto kernelFunc = reinterpret_cast<void *>(demoFunc);
+ 	     auto kernelArg = static_cast<CcuKernelArg>(&demoArg);
+ 	 
+ 	     // 重置CCU资源
+ 	     ccuRet = HcommCcuKernelRegisterStart(insHandle);
+ 	     EXPECT_EQ(ccuRet, CcuResult::CCU_SUCCESS);
+ 	 
+ 	     // kernel注册
+ 	     char *kernelFuncName = "ccu_var_add_simple_demo";
+ 	     CcuKernelHandle kernelHandle{0};
+ 	     ccuRet = HcommCcuKernelRegister(insHandle, kernelFuncName,
+ 	         kernelFunc, kernelArg, &kernelHandle);
+ 	     EXPECT_EQ(ccuRet, CcuResult::CCU_SUCCESS);
+ 	 
+ 	     // kernel翻译
+ 	     ccuRet = HcommCcuKernelRegisterEnd(insHandle);
+ 	     EXPECT_EQ(ccuRet, CcuResult::CCU_SUCCESS);
+ 	     
+ 	     // 申请流，假定已经获取了threadHandle
+ 	     auto fakeThreadHandle = MockThreadAllocWithStream(commEngine);
+ 	 
+ 	     // kernel下发
+ 	     // todo: 当前未适配LoadArgs，暂时使用空args
+ 	     void *fakeTaskArgs = nullptr;
+ 	     uint32_t fakeArgSize = 0;
+ 	     EXPECT_EQ(HcommCcuKernelLaunch(fakeThreadHandle, kernelHandle,
+ 	         fakeTaskArgs, fakeArgSize), CcuResult::CCU_SUCCESS);
+ 	 
+ 	     // 清理各种资源，析构有时序要求
+ 	     // 主线功能有bug，暂时不能主动释放stream
+ 	     // constexpr uint32_t fakeThreadNum = 1;
+ 	     // EXPECT_EQ(HcommThreadFree(&fakeThreadHandle, fakeThreadNum), HcclResult::HCCL_SUCCESS);
+ 	     MockChannelDestory(handlePair);
+ 	     ccuRet = HcommCcuInsDestroy(insHandle);
+ 	     EXPECT_EQ(ccuRet, CcuResult::CCU_SUCCESS);
+ 	 }
+
+HcclResult SetOpParamFastLaunchTag(OpParam &param)
+{
+    // 1. 数据类型
+    const char* dataTypeStr = nullptr;
+    if(param.opType == HcclCMDType::HCCL_CMD_ALLTOALL || param.opType == HcclCMDType::HCCL_CMD_ALLTOALLV ||
+        param.opType == HcclCMDType::HCCL_CMD_ALLTOALLVC) {
+        dataTypeStr = GetHcclDataTypeStr(param.all2AllVDataDes.sendType);
+    } else {
+        dataTypeStr = GetHcclDataTypeStr(param.DataDes.dataType);
+    }
+    CHK_PRT_RET((!dataTypeStr), HCCL_ERROR("unsupported data type"), HcclResult::HCCL_E_INTERNAL);
+    // 2. reduce op
+    const char* reduceOpStr = nullptr;
+    if (param.opType == HcclCMDType::HCCL_CMD_ALLREDUCE || param.opType == HcclCMDType::HCCL_CMD_REDUCE_SCATTER ||
+        param.opType == HcclCMDType::HCCL_CMD_REDUCE    || param.opType == HcclCMDType::HCCL_CMD_REDUCE_SCATTER_V) {
+        reduceOpStr = GetHcclReduceOpStr(param.reduceType);
+        CHK_PRT_RET((!reduceOpStr), HCCL_ERROR("unsupported reduce op"), HcclResult::HCCL_E_INTERNAL);
+    }
+    // 3. count
+    char countBuf[32];
+    const char* countStr = nullptr;
+    if (param.opType != HcclCMDType::HCCL_CMD_ALLTOALLV) {
+        int countLen = snprintf_s(countBuf, sizeof(countBuf), sizeof(countBuf) - 1, "%llu", static_cast<uint64_t>(param.DataDes.count));
+        CHK_PRT_RET((countLen <= 0), HCCL_ERROR("failed to format count"), HcclResult::HCCL_E_INTERNAL);
+        countStr = countBuf;
+    }
+    // 4. root
+    char rootBuf[10];
+    const char* rootStr = nullptr;
+    if (param.opType == HcclCMDType::HCCL_CMD_REDUCE || param.opType == HcclCMDType::HCCL_CMD_SCATTER ||
+        param.opType == HcclCMDType::HCCL_CMD_BROADCAST) {
+        int rootLen = snprintf_s(rootBuf, sizeof(rootBuf), sizeof(rootBuf) - 1, "%llu", static_cast<uint64_t>(param.root));
+        CHK_PRT_RET((rootLen <= 0), HCCL_ERROR("failed to format root"), HcclResult::HCCL_E_INTERNAL);
+        rootStr = rootBuf;
+    }
+    // 5 一次性拼接
+    char* dst = param.fastLaunchTag;
+    size_t remain = sizeof(param.fastLaunchTag);
+
+    auto append_str = [&](const char* s) -> bool {
+        if (!s) return true;
+        size_t len = strlen(s);
+        if (len >= remain) return false;
+        memcpy(dst, s, len);
+        dst += len;
+        remain -= len;
+        return true;
+    };
+
+    if (!append_str(param.tag) || !append_str("_") || !append_str(dataTypeStr)) {
+        goto fail;
+    }
+    if (reduceOpStr && (!append_str("_")) || !append_str(reduceOpStr)) {
+        goto fail;
+    }
+    if (countStr && (!append_str("_")) || !append_str(countStr)) {
+        goto fail;
+    }
+    if (rootStr && (!append_str("_r")) || !append_str(rootStr)) {
+        goto fail;
+    }
+    *dst = '\0';
+    HCCL_DEBUG("[SetOpParamFastLaunchTag] fastLaunchTag: [%s]", param.fastLaunchTag);
+    return HcclResult::HCCL_SUCCESS;
+
+fail:
+    HCCL_ERROR("failed to fill fastLaunchTag");
+    return HcclResult::HCCL_E_INTERNAL;
+}
 }
