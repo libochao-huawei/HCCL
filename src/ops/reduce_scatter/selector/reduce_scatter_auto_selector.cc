aac535b39fff4424d4267bb1059ae3f2ee54b522
yjzz1007 <zhaojiayu7@huawei.com>
2026-04-29 14:59:16 +0800

    int64 selector

--- src/ops/reduce_scatter/selector/reduce_scatter_auto_selector.cc
+++ src/ops/reduce_scatter/selector/reduce_scatter_auto_selector.cc
@@ -338,12 +338,10 @@ SelectorStatus ReduceScatterAutoSelector::SelectMeshAlgoAicpuForMesh1DClos(const
         // PCIE机型算法选择
         if (IsLayerAllConnetedWithTopo(topoInfo, 0, CommTopo::COMM_TOPO_1DMESH)) {
             selectAlgName = "InsReduceScatterMesh1D";
+        } else if (Is64BitDataType(opParam.DataDes.dataType) || opParam.reduceType == HcclReduceOp::HCCL_REDUCE_PROD) {
+            selectAlgName = "InsReduceScatterAicpuReduceNHR";
         } else {
-            if (Is64BitDataType(opParam.DataDes.dataType) || opParam.reduceType == HcclReduceOp::HCCL_REDUCE_PROD) {
-                selectAlgName = "InsReduceScatterMesh1D";
-            } else {
-                selectAlgName = "InsReduceScatterParallelMesh1DNHRPcie";
-            }
+            selectAlgName = "InsReduceScatterParallelMesh1DNHRPcie";
         }
     } else if (IsLayerAllConnetedWithTopo(topoInfo, 0, CommTopo::COMM_TOPO_1DMESH)) {
         // MESH_1D 即可链接所有卡， 使用 MESH_1D 算法
