ded231aeafe2cbaab131394cab0b53979d8a01ea
ethWeek1 <penghaohang@huawei.com>
2026-04-30 16:35:36 +0800

    fix

--- src/ops/all_to_all_v/template/ccu/ccu_temp_all_to_all_mesh1d_2Die.cc
+++ src/ops/all_to_all_v/template/ccu/ccu_temp_all_to_all_mesh1d_2Die.cc
@@ -297,8 +297,9 @@ HcclResult CcuTempAllToAllMesh1D2Die::KernelRun(const OpParam &param, const Temp
     uint64_t token;
     CHK_RET(GetToken(buffInfo_, token));
     uint64_t sliceSize        = templateDataParams.sliceSize;
-    uint64_t inputSliceStride = templateDataParams.sdispls[1] * DATATYPE_SIZE_TABLE[param.all2AllDataDes.recvType] -  buffInfo_.inBuffBaseOff;
+    // uint64_t inputSliceStride = templateDataParams.sdispls[1] * DATATYPE_SIZE_TABLE[param.all2AllDataDes.recvType] -  buffInfo_.inBuffBaseOff;
     uint64_t outputSliceStride = templateDataParams.sdispls[1] * DATATYPE_SIZE_TABLE[param.all2AllDataDes.recvType] -  buffInfo_.inBuffBaseOff;
+    uint64_t inputSliceStride = outputSliceStride;
     uint64_t outBuffBaseOff =  buffInfo_.outBuffBaseOff;
     HCCL_INFO("[CcuTempAllToAllMesh1D2Die][KernelRun] begin. Rank[%d], input[%#llx/%#llx], output[%#llx/%#llx], "
         "sendType[%d], recvType[%d]", myRank_, inputAddr, param.inputPtr, outputAddr, param.outputPtr,
