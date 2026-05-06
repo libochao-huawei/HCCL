94f8eb9aa890d49a0d1f8db8653ce5573bfdb90a
xiaocui235 <cuiruxing@huawei.com>
2026-05-06 14:33:50 +0800

    checker ok

--- src/ops/reduce_scatter/template/aicpu/ins_temp_reduce_scatter_mesh_1D_Z_axis_detour.cc
+++ src/ops/reduce_scatter/template/aicpu/ins_temp_reduce_scatter_mesh_1D_Z_axis_detour.cc
@@ -29,39 +29,33 @@ HcclResult InsTempReduceScatterMesh1DZAxisDetour::CalcRes(
 {
     CHK_PRT_RET(topoInfo == nullptr,
         HCCL_ERROR("[InsTempReduceScatterMesh1DZAxisDetour][CalcRes] topoInfo is nullptr"), HCCL_E_PARA);
-
     std::vector<HcclChannelDesc> level0Channels;
-    // 校验topoInfo是否为空
-    CHK_PRT_RET(topoInfo == nullptr,
-        HCCL_ERROR("[InsTempReduceScatterMesh1D][CalcRes] topoInfo is nullptr"), HCCL_E_PARA);
-    CHK_RET(CalcChannelRequestMesh1D(comm, param, topoInfo, subCommRanks_, level0Channels));
-    // resourceRequest.channels.push_back(level0Channels);
-    // std::vector<HcclChannelDesc> level0Channels;
-    // CHK_RET(CalcChannelRequestMesh1DLevel0(comm, param, topoInfo, subCommRanks_, level0Channels));
+    CHK_RET(CalcChannelRequestMesh1DLevel0(comm, param, topoInfo, subCommRanks_, level0Channels));
     std::vector<HcclChannelDesc> level1Channels;
     CHK_RET(CalcChannelRequestMesh1DLevel1(comm, param, topoInfo, subCommRanks_, level1Channels));
-    // std::vector<HcclChannelDesc> mergedChannels;
-    // mergedChannels.insert(mergedChannels.end(), level0Channels.begin(), level0Channels.end());
-    // mergedChannels.insert(mergedChannels.end(), level1Channels.begin(), level1Channels.end());
-    // resourceRequest.channels.push_back(mergedChannels);
-    // channelsPerRank_ = CalcChannelsPerRank(mergedChannels);
+    std::vector<HcclChannelDesc> mergedChannels;
+    mergedChannels.insert(mergedChannels.end(), level0Channels.begin(), level0Channels.end());
+    mergedChannels.insert(mergedChannels.end(), level1Channels.begin(), level1Channels.end());
+    resourceRequest.channels.push_back(mergedChannels);
     level0ChannelNumPerRank_ = CalcChannelsPerRank(level0Channels);
-    // level1ChannelNumPerRank_ = CalcChannelsPerRank(level1Channels);
+    level1ChannelNumPerRank_ = CalcChannelsPerRank(level1Channels);
+    channelsPerRank_ = level0ChannelNumPerRank_ + level1ChannelNumPerRank_;
     CHK_RET(GetRes(resourceRequest));
-    // HCCL_DEBUG("[InsTempReduceScatterMesh1DZAxisDetour][CalcRes] myRank[%u], channelsPerRank_[%u], "
-    //            "level0ChannelNum[%zu], level1ChannelNum[%zu], notifyNumOnMainThread[%u], slaveThreadNum[%u]",
-    //            myRank_, channelsPerRank_, level0Channels.size(), level1Channels.size(),
-    //            resourceRequest.notifyNumOnMainThread, resourceRequest.slaveThreadNum);
+    HCCL_DEBUG("[InsTempReduceScatterMesh1DZAxisDetour][CalcRes] myRank[%u], channelsPerRank_[%u], "
+               "level0ChannelNum[%zu], level1ChannelNum[%zu], notifyNumOnMainThread[%u], slaveThreadNum[%u]",
+               myRank_, channelsPerRank_, level0Channels.size(), level1Channels.size(),
+               resourceRequest.notifyNumOnMainThread, resourceRequest.slaveThreadNum);
+    HCCL_DEBUG("[InsTempReduceScatterMesh1DZAxisDetour][CalcRes]myRank[%u], channelsPerRank_[%u], "
+               "level0ChannelNumPerRank_[%zu], level1ChannelNumPerRank_[%zu], level0DataRatio_[%zu]",
+               myRank_, channelsPerRank_, level0ChannelNumPerRank_, level1ChannelNumPerRank_, level0DataRatio_);
     return HCCL_SUCCESS;
 }
 
 u64 InsTempReduceScatterMesh1DZAxisDetour::GetThreadNum() const
 {
-    // if (templateRankSize_ <= 1) {
-    //     return 1;
-    // }
-    // return (templateRankSize_ - 1) * channelsPerRank_ + 1;
-    u32 threadNum = templateRankSize_ > 1 ? templateRankSize_ : 1;
+
+    u32 threadNum = templateRankSize_ > 1 ? ((templateRankSize_ - 1) * channelsPerRank_ + 1) : 1;
+    HCCL_INFO("[InsTempReduceScatterMesh1DZAxisDetour][GetThreadNum] templateRankSize_[%u] channelsPerRank_[%u] threadNum[%u]", templateRankSize_, channelsPerRank_, threadNum);
     return threadNum;
 }
 
@@ -71,9 +65,23 @@ HcclResult InsTempReduceScatterMesh1DZAxisDetour::CalcDataSplitByPortGroup(
     std::vector<u64> &elemCountOut, std::vector<u64> &sizeOut,
     std::vector<u64> &elemOffset)
 {
+    HCCL_INFO("[InsTempReduceScatterMesh1DZAxisDetour][CalcDataSplitByPortGroup] Run Start");
     return CalcDataSplitByPortGroupZAxisDetour(totalDataCount, dataTypeSize, channels,
         elemCountOut, sizeOut, elemOffset,
         level0ChannelNumPerRank_, level1ChannelNumPerRank_, level0DataRatio_);
+}
+
+
+HcclResult InsTempReduceScatterMesh1DZAxisDetour::SetchannelsPerRank(
+    const std::map<u32, std::vector<ChannelInfo>> &channels)
+{
+    CHK_PRT_RET(channels.empty(), HCCL_ERROR("[SetchannelsPerRank] channels is empty."), HCCL_E_INTERNAL);
+    channelsPerRank_ = CalcChannelsPerRank(channels);
+    level0ChannelNumPerRank_ = 1;
+    level1ChannelNumPerRank_ = channelsPerRank_ - level0ChannelNumPerRank_;
+    HCCL_INFO("[InsTempReduceScatterMesh1DZAxisDetour][SetchannelsPerRank] skip, channelsPerRank_[%u] "
+              "already set in CalcRes", channelsPerRank_);
+    return HCCL_SUCCESS;
 }
 
 } // namespace ops_hccl
