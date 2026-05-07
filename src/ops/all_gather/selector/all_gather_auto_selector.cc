4266967f20db8b9094d855a04fb825bb6bed7f06
NANYI00 <hanyan36@huawei.com>
2026-05-07 11:41:32 +0800

    050701

--- src/ops/all_gather/selector/all_gather_auto_selector.cc
+++ src/ops/all_gather/selector/all_gather_auto_selector.cc
@@ -14,6 +14,8 @@
 namespace ops_hccl {
 constexpr u64 AG_2D_SMALL_DATA_SIZE = 1024 * 1024;
 constexpr u32 MAX_RANK_NUM_FOR_CONCURRENT_ALGO = 4;
+constexpr u32 ISFLATTENDATA = 8 * 1024 * 1024;
+
 
 SelectorStatus AllGatherAutoSelector::SelectCcuMsAlgo(
     const TopoInfoWithNetLayerDetails *topoInfo, const OpParam &opParam, const std::map<HcclCMDType, std::vector<HcclAlgoType>> &configAlgMap,
@@ -176,6 +178,8 @@ SelectorStatus AllGatherAutoSelector::SelectCcuScheduleAlgo(
                 return SelectorStatus::NOT_MATCH;
             } else if (topoInfo->netLayerDetails.localNetInsSizeOfLayer[0] == 1) {
                 selectAlgName = "CcuAllGatherNHR1DMem2Mem";
+            } else if ((dataSize * topoInfo->userRankSize) <= ISFLATTENDATA && topoInfo->userRankSize > 8) {
+                selectAlgName = "CcuAllGatherNHR1DFlatten";
             } else {
                 selectAlgName = "CcuAllGatherParallelMesh1DNHR";
             }
