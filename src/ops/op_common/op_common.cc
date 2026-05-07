f3a8b81a6b2ccb1cbc1b5b9265a684fcf9ed4994
renyumin <renyumin1@huawei.com>
2026-05-07 10:55:24 +0800

    compile

--- src/ops/op_common/op_common.cc
+++ src/ops/op_common/op_common.cc
@@ -824,6 +824,7 @@ HcclResult HcclGetAlgRes(HcclComm comm, OpParam& param, std::unique_ptr<InsCollA
 
     CHK_RET(GetAlgResWithEngine(comm, param, resRequest, resCtxHost, topoInfo, algHierarchyInfo, resCtxSequence, size,
         increCreateChannelFlag));
+    param.ctxSize = size;
 
     // 参数一致性校验
     if (!isChecked) {
@@ -900,7 +901,6 @@ HcclResult GetAlgResWithEngine(HcclComm comm, const OpParam &param, AlgResourceR
         HCCL_ERROR("fail to get engine, invalid engine type[%d].", param.engine);
         return HCCL_E_PARA;
     }
-    param.ctxSize = size;
     return HCCL_SUCCESS;
 }
 
