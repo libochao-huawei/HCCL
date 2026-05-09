f4d51b0dae8c976e436be3b8bc0794ae80a7070b
renyumin <renyumin1@huawei.com>
2026-05-09 18:47:09 +0800

    include

--- src/common/hcomm_dlsym/hccl_res_expt_dl.cc
+++ src/common/hcomm_dlsym/hccl_res_expt_dl.cc
@@ -15,7 +15,7 @@
 
 // ---------- 桩函数定义（签名与真实API完全一致）----------
 DEFINE_WEAK_FUNC(HcclResult, HcclCommAddExchangeInfo, HcclComm comm, void *data, uint32_t length);
-DEFINE_WEAK_FUNC(HcclResult, HcclCommGetExchangeInfo, HcclComm comm, uint32_t remoteRank, void *data, uint32_t &length);
+DEFINE_WEAK_FUNC(HcclResult, HcclCommGetExchangeInfo, HcclComm comm, uint32_t remoteRank, void **data, uint32_t *length);
 DEFINE_WEAK_FUNC(HcclResult, HcclCommResetExchangeInfo, HcclComm comm);
 
 // 初始化
