cbfbb24f42910bf054d7a0f8549f45ca268d4aa8
littlemons <yaowenjie7@huawei.com>
2026-05-11 22:24:13 +0800

    fixbug

--- src/ops/all_to_all_v/executor/ins_v2_all_to_all_concurrent_executor.cc
+++ src/ops/all_to_all_v/executor/ins_v2_all_to_all_concurrent_executor.cc
@@ -128,6 +128,7 @@ HcclResult InsV2AllToAllConcurrentExecutor<AlgTopoMatch, InsAlgTemplate0, InsAlg
                                               resReq1.ccuKernelInfos.end());
     } else if (param.engine == CommEngine::COMM_ENGINE_AICPU || param.engine == CommEngine::COMM_ENGINE_AICPU_TS) {
         printf("[ywj]%s:%s:%u", __FILE__, __FUNCTION__, __LINE__);
+        resourceRequest.channels.resize(1);
         resourceRequest.channels[0].insert(resourceRequest.channels[0].end(),
                                               channelDescs0.begin(),
                                               channelDescs0.end());
