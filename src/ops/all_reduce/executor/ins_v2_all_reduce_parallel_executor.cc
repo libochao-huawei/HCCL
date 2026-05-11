bf2fb31072093271914ba011c70b2c1961cd9033
yjzz1007 <zhaojiayu7@huawei.com>
2026-05-11 22:14:30 +0800

    ar

--- src/ops/all_reduce/executor/ins_v2_all_reduce_parallel_executor.cc
+++ src/ops/all_reduce/executor/ins_v2_all_reduce_parallel_executor.cc
@@ -22,8 +22,6 @@
 #include "ccu_temp_all_gather_mesh_1D_mem2mem.h"
 #include "ccu_temp_reduce_scatter_mesh_1D_mem2mem.h"
 #include "ccu_temp_reduce_scatter_nhr_1D_mem2mem.h"
-#include "ccu_temp_reduce_scatter_nhr_1D_multi_jetty_mem2mem.h"
-#include "ccu_temp_all_gather_nhr_1D_multi_jetty_mem2mem.h"
 #endif /* !HCCL_CANN_COMPAT_850 */
 #endif
 
