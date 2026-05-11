7748756f7a4cc5c67f10eb36aa7512e6fd0dda5c
w00947004 <wangtianzhu3@huawei.com>
2026-05-11 09:44:06 +0800

    Title:hccl-Allgather-Z-axis-bypass

---
+++ src/ops/all_gather/template/aicpu/ins_temp_all_gather_mesh_1D_Z_axis_detour.h
@@ -0,0 +1,72 @@
+/**
+ * Copyright (c) 2025 Huawei Technologies Co., Ltd.
+ * This program is free software, you can redistribute it and/or modify it under the terms and conditions of
+ * CANN Open Software License Agreement Version 2.0 (the "License").
+ * Please refer to the License for details. You may not use this file except in compliance with the License.
+ * THIS SOFTWARE IS PROVIDED ON AN "AS IS" BASIS, WITHOUT WARRANTIES OF ANY KIND, EITHER EXPRESS OR IMPLIED,
+ * INCLUDING BUT NOT LIMITED TO NON-INFRINGEMENT, MERCHANTABILITY, OR FITNESS FOR A PARTICULAR PURPOSE.
+ * See LICENSE in the root of the software repository for the full text of the License.
+ */
+
+#ifndef INS_TEMP_ALL_GATHER_MESH_1D_Z_AXIS_DETOUR_H
+#define INS_TEMP_ALL_GATHER_MESH_1D_Z_AXIS_DETOUR_H
+
+#include "alg_v2_template_base.h"
+#include "executor_base.h"
+
+namespace ops_hccl {
+
+class InsTempAllGatherMesh1D1DZAxisDetour : public InsAlgTemplateBase {
+public:
+    InsTempAllGatherMesh1D1DZAxisDetour() = default;
+    explicit InsTempAllGatherMesh1D1DZAxisDetour(const OpParam &param, const u32 rankId,  // 传通信域的rankId，userRank
+                                    const std::vector<std::vector<u32>> &subCommRanks);
+    // Host侧调用
+    ~InsTempAllGatherMesh1D1DZAxisDetour() override;
+
+    std::string Describe() const override
+    {
+        std::string info = "Template of all gather Mesh with tempRankSize ";
+        info += std::to_string(templateRankSize_);
+        return info;
+    }
+    HcclResult KernelRun(const OpParam &param, const TemplateDataParams &tempAlgParams,
+                         TemplateResource &templateResource) override;
+    HcclResult CalcRes(HcclComm comm, const OpParam &param, const TopoInfoWithNetLayerDetails *topoInfo,
+                       AlgResourceRequest &resourceRequest) override;
+    HcclResult GetRes(AlgResourceRequest &resourceRequest) const override;
+
+    u64 CalcScratchMultiple(BufferType inBuffType, BufferType outBuffType) override;
+    u64 GetThreadNum() const override;
+    void GetNotifyIdxMainToSub(std::vector<u32> &notifyIdxMianToSub) override;
+    void GetNotifyIdxSubToMain(std::vector<u32> &notifyIdxSubToMain) override;
+
+    HcclResult CalcDataSplitByPortGroup(const u64 totalDataCount, const u64 dataTypeSize,
+                                        const std::vector<ChannelInfo> &channels,
+                                        std::vector<u64> &elemCountOut, std::vector<u64> &sizeOut,
+                                        std::vector<u64> &elemOffset) override;
+    HcclResult SetchannelsPerRank(const std::map<u32, std::vector<ChannelInfo>> &channels) override;
+    void SetIsNewTemp(bool type)
+    {
+        isNew = type;
+    }
+    bool GetIsNewTemp(voild) const
+    {
+        return isNew;
+    }
+    u32 channelsSize;
+protected:
+    HcclResult RunAllGatherMesh(const std::vector<ThreadHandle> &threads,
+                                                        const std::map<u32, std::vector<ChannelInfo>> &channels);
+    HcclResult LocalDataCopy(const std::vector<ThreadHandle> &threads);
+    HcclResult PostLocalCopy(const std::vector<ThreadHandle> &threads);
+    TemplateDataParams tempAlgParams_;
+private:
+    u32 level0ChannelNumPerRank_{1};
+    u32 level1ChannelNumPerRank_{0};
+    float level0DataRatio_{1.0f};
+};
+
+}  // namespace ops_hccl
+
+#endif  // INS_TEMP_ALL_GATHER_MESH_1D_H
