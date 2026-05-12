a0e48f3e62e6f2f6b3faeca6f7ab844f27f85f14
yjzz1007 <zhaojiayu7@huawei.com>
2026-05-12 22:33:40 +0800

    bug

--- src/ops/reduce_scatter/template/aicpu/ins_temp_reduce_scatter_nhr.cc
+++ src/ops/reduce_scatter/template/aicpu/ins_temp_reduce_scatter_nhr.cc
@@ -291,6 +291,8 @@ HcclResult InsTempReduceScatterNHR::RunNHR(const std::vector<ThreadHandle> &thre
                     txSliceSize, txSliceSize / DATATYPE_SIZE_TABLE[dataType_]); // 发送源
                 DataSlice txDstSlice = DataSlice(sendRemoteCclBuffAddr, txScOff,
                     txSliceSize, txSliceSize / DATATYPE_SIZE_TABLE[dataType_]);  // 发送目标
+                HCCL_INFO("zjy tempAlgParams_.buffInfo.hcclBuff[%u] addr=%p, txScOff=%llu, txSliceSize=%llu",
+                    txIdx, tempAlgParams_.buffInfo.hcclBuff.addr, txScOff, txSliceSize);
                 txSrcSlices.push_back(txSrcSlice);
                 txDstSlices.push_back(txDstSlice);
                 DataSlice rxSrcSlice = DataSlice(recvRemoteCclBuffAddr, rxScOff,
