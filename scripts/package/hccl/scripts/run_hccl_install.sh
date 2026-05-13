19bd9ebf445dc62d584fe1376d42113e3fb8cef6
XuanYang11 <yangxuan77@h-partners.com>
2026-05-13 16:13:15 +0800

    完成将py脚本使用cmake仓对应脚本,修改scripts/hcomm/scripts的sh文件

--- scripts/package/hccl/scripts/run_hccl_install.sh
+++ scripts/package/hccl/scripts/run_hccl_install.sh
@@ -9,7 +9,6 @@ pylocal=n
 in_install_for_all=n
 setenv_flag=n
 docker_root=""
-sourcedir="$PWD/hccl"
 curpath=$(dirname $(readlink -f "$0"))
 common_func_path="${curpath}/common_func.inc"
 pkg_version_path="${curpath}/../version.info"
@@ -123,10 +122,6 @@ if [ ! -d "$common_parse_dir" ]; then
 fi
 
 new_install() {
-    if [ ! -d "${sourcedir}" ]; then
-        log "INFO" "no need to install hccl files."
-        return 0
-    fi
     output_progress 10
 
     local setenv_option=""
