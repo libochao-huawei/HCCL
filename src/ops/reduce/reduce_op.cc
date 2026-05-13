600f75578febcc15680a1e82bbbc595618555b51
x30067372 <xujiahui27@h-partners.com>
2026-05-13 09:47:15 +0800

    reduce精度问题修复

--- src/ops/reduce/reduce_op.cc
+++ src/ops/reduce/reduce_op.cc
@@ -222,10 +222,10 @@ HcclResult ReduceOutPlaceCommon(void *sendBuf, void *recvBuf, uint64_t count, Hc
     
     CHK_RET(HcclGetOpExpansionMode(comm, param));
 
-    CcuFastLaunchCtx *ccuFastLaunchCtx = nullptr;
-    if ((opMode == OpMode::OPBASE) && ShouldGoCcuFastLaunch(comm, param, &ccuFastLaunchCtx)) {
-        return HcclExecOpCcuFastLaunch(comm, param, ccuFastLaunchCtx);
-    }
+    // CcuFastLaunchCtx *ccuFastLaunchCtx = nullptr;
+    // if ((opMode == OpMode::OPBASE) && ShouldGoCcuFastLaunch(comm, param, &ccuFastLaunchCtx)) {
+    //     return HcclExecOpCcuFastLaunch(comm, param, ccuFastLaunchCtx);
+    // }
 
     if (userRankSize == 1) {
         HCCL_WARNING("[%s] ranksize == 1, enter SingleRankProc", __func__);
