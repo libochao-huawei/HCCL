d0808c69e4078b53feea40e01b1efa52c970beb7
shenjiahui_ <shenjiahui5@huawei.com>
2026-05-14 19:47:34 +0800

    fix ag selector

--- src/ops/all_gather/selector/all_gather_auto_selector.cc
+++ src/ops/all_gather/selector/all_gather_auto_selector.cc
@@ -232,7 +232,7 @@ SelectorStatus AllGatherAutoSelector::SelectAicpuAlgo(
         }
     } else {
         if (topoInfo->level0Topo == Level0Shape::MESH_1D) {
-            if (dataSize * topoInfo->userRankSize > AG_AICPU_1D_TWO_LEVER_DATA_SIZE_THRESHOLD) {
+            if (IsTwoLevelNetLayer(topoInfo) && dataSize * topoInfo->userRankSize > AG_AICPU_1D_TWO_LEVER_DATA_SIZE_THRESHOLD) {
                 selectAlgName = "InsAllGatherMesh1D1DZAxisDetour";
             } else {
                 selectAlgName = "InsAllGatherMesh1D";
