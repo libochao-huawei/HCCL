05cb1b5c84433a858004a8eda36a7697d58bec49
shenjiahui_ <shenjiahui5@huawei.com>
2026-05-14 10:20:31 +0800

    add st cmake

--- src/ops/all_reduce/selector/all_reduce_auto_selector.cc
+++ src/ops/all_reduce/selector/all_reduce_auto_selector.cc
@@ -367,15 +367,11 @@ SelectorStatus AllReduceAutoSelector::SelectMeshAlgoAicpu(const TopoInfoWithNetL
         ratio = DEFAULT_RANK_SIZE / topoInfo->userRankSize / topoInfo->userRankSize;
     }
     bool isTwoLevelFlag = IsTwoLevelNetLayer(topoInfo);
-    bool overZDetourThreshold = (dataSize * ratio > AR_AICPU_1D_MAX_DATA_SIZE);
     if (topoInfo->level0Topo == Level0Shape::MESH_1D) {
         if (isDataTypeOrReduceTypeSpecial) {
-            if (dataSize <= AR_AICPU_1D_64DATATYPE_DATA_SIZE) {
-                selectAlgName = "InsAllReduceMesh1DOneShot";
-            } else {
-                selectAlgName = (isTwoLevelFlag && overZDetourThreshold) ?
-                    "InsAllReduceMesh1DTwoShotZAxisDetour" : "InsAllReduceMesh1DTwoShot";
-            }
+            selectAlgName = dataSize <= AR_AICPU_1D_64DATATYPE_DATA_SIZE ?
+                            "InsAllReduceMesh1DOneShot" :
+                            "InsAllReduceMesh1DTwoShot";
         } else if (dataSize <= AR_AICPU_1D_SMALL_DATA_SIZE) {
             selectAlgName = "InsAllReduceMesh1DOneShot";
         } else if (dataSize * ratio > AR_AICPU_1D_MAX_DATA_SIZE) {
