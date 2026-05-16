ddc52b594c6a34824645d8e9b3d5c4dde2c52bf8
zdy1205 <zhaodeyang@hisilicon.com>
2026-05-16 18:22:28 +0800

    Add order preservation protocol for all_reduce

---
+++ src/ops/reduce_scatter/template/aicpu/ins_temp_reduce_scatter_order_preserved_level1.h
@@ -0,0 +1,73 @@
+/**
+ * Copyright (c) 2025 Huawei Technologies Co., Ltd.
+ * This program is free software, you can redistribute it and/or modify it under the terms and conditions of
+ * CANN Open Software License Agreement Version 2.0 (the "License").
+ * Please refer to the License for details. You may not use this file except in compliance with the License.
+ * THIS SOFTWARE IS PROVIDED ON AN "AS IS" BASIS, WITHOUT WARRANTIES OF ANY KIND, EITHER EXPRESS OR IMPLIED,
+ * INCLUDING BUT NOT LIMITED TO NON-INFRINGEMENT, MERCHANTABILITY, OR FITNESS FOR A PARTICULAR PURPOSE.
+ * See LICENSE in the root of the software repository for the full text of the License.
+ */
+
+#ifndef INS_TEMP_REDUCE_SCATTER_ORDER_PRESERVED_LEVEL1_H
+#define INS_TEMP_REDUCE_SCATTER_ORDER_PRESERVED_LEVEL1_H
+
+#include <cstring>
+#include <cmath>
+#include <algorithm>
+#include "alg_v2_template_base.h"
+#include "executor_base.h"
+#include "alg_data_trans_wrapper.h"
+
+namespace ops_hccl {
+
+constexpr u32 REDUCE_SCATTER_MAX_STREAM_NUM_A5 = 8;
+
+class InsTempReduceScatterOrderPreservedLevel1 : public InsAlgTemplateBase {
+public:
+    InsTempReduceScatterOrderPreservedLevel1() = default;
+    explicit InsTempReduceScatterOrderPreservedLevel1(const OpParam& param, const u32 rankId,
+        const std::vector<std::vector<u32>> &subCommRanks);
+    ~InsTempReduceScatterOrderPreservedLevel1() override;
+
+    std::string Describe() const override
+    {
+        std::string info = "Template of Order Preserved ReduceScatter Level1 with tempRankSize ";
+        info += std::to_string(templateRankSize_);
+        return info;
+    }
+
+    HcclResult KernelRun(const OpParam& param, const TemplateDataParams& tempAlgParams,
+        TemplateResource& templateResource) override;
+    HcclResult CalcRes(HcclComm comm, const OpParam& param, const TopoInfoWithNetLayerDetails* topoInfo,
+        AlgResourceRequest& resourceRequest) override;
+    HcclResult GetRes(AlgResourceRequest& resourceRequest) const override;
+    u64 CalcScratchMultiple(BufferType inBuffType, BufferType outBuffType) override;
+    u64 GetThreadNum() const override;
+
+    void GetNotifyIdxMainToSub(std::vector<u32> &notifyIdxMainToSub) override;
+    void GetNotifyIdxSubToMain(std::vector<u32> &notifyIdxSubToMain) override;
+
+private:
+    HcclResult RunAllToAll(const std::map<u32, std::vector<ChannelInfo>> &channels,
+        const std::vector<ThreadHandle> &threads, const TemplateDataParams &tempAlgParams);
+    HcclResult RunLocalReduce(const std::vector<ThreadHandle> &threads,
+        const TemplateDataParams &tempAlgParams);
+    HcclResult LocalCopy(const TemplateDataParams &tempAlgParams, const std::vector<ThreadHandle> &threads);
+    HcclResult PostCopy(const TemplateDataParams &tempAlgParams, const std::vector<ThreadHandle> &threads);
+    
+    HcclResult PreSyncThreads(const std::vector<ThreadHandle> &threads);
+    HcclResult PostSyncThreads(const std::vector<ThreadHandle> &threads);
+    
+    u32 CalcOutputIndex(const u32 round);
+    bool IsLastBlockData(const u32 outputIndex);
+    bool IsLastRank(const u32 rankId);
+
+    u64 count_{0};
+    u32 dataTypeSize_{0};
+    u32 all2allOffset_{0};
+    bool deterministicStrict_{false};
+};
+
+} // namespace ops_hccl
+
+#endif
