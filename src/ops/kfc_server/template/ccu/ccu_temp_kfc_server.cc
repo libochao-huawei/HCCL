def5ebfe61a7675226f81bd9a3586b281edd22df
w00951525 <wangfuhao7@huawei.com>
2026-05-16 14:13:26 +0800

    新增kfc server算子

---
+++ src/ops/kfc_server/template/ccu/ccu_temp_kfc_server.cc
@@ -0,0 +1,60 @@
+/**
+ * Copyright (c) 2025 Huawei Technologies Co., Ltd.
+ * This program is free software, you can redistribute it and/or modify it under the terms and conditions of
+ * CANN Open Software License Agreement Version 2.0 (the "License").
+ * Please refer to the License for details. You may not use this file except in compliance with the License.
+ * THIS SOFTWARE IS PROVIDED ON AN "AS IS" BASIS, WITHOUT WARRANTIES OF ANY KIND, EITHER EXPRESS OR IMPLIED,
+ * INCLUDING BUT NOT LIMITED TO NON-INFRINGEMENT, MERCHANTABILITY, OR FITNESS FOR A PARTICULAR PURPOSE.
+ * See LICENSE in the root of the software repository for the full text of the License.
+ */
+
+#include "channel.h"
+#include "hccl_ccu_res.h"
+#include "ccu_assist_pub.h"
+#include "alg_data_trans_wrapper.h"
+#include "template_utils.h"
+#include "kernel/ccu_kernel_kfc_server.h"
+#include "ccu_temp_kfc_server.h"
+
+namespace ops_hccl {
+
+CcuTempKfcServer::CcuTempKfcServer(const OpParam& param, const u32 rankId,
+                                   const std::vector<std::vector<u32>> &subCommRanks)
+: CcuAlgTemplateBase(param, rankId, subCommRanks)
+{
+    tempRankSize_ = subCommRanks[0].size();
+    auto it = std::find(subCommRanks[0].begin(), subCommRanks[0].end(), rankId);
+    if (it != subCommRanks[0].end()) {
+        mySubCommRank_ = std::distance(subCommRanks[0].begin(), it);
+    }
+}
+
+CcuTempKfcServer::~CcuTempKfcServer()
+{
+}
+
+HcclResult CcuTempKfcServer::CalcRes(HcclComm comm, const OpParam& param, const TopoInfoWithNetLayerDetails* topoInfo,
+                                     AlgResourceRequest& resourceRequest)
+{
+    HCCL_INFO("[CcuTempKfcServer::CalcRes]");
+    return HcclResult::HCCL_SUCCESS;
+}
+
+HcclResult CcuTempKfcServer::KernelRun(const OpParam& param,
+                                       const TemplateDataParams& templateDataParams,
+                                       TemplateResource& templateResource)
+{
+    (void)templateDataParams;
+    (void)templateResource;
+    HCCL_INFO("[CcuTempKfcServer] KernelRun End.");
+    return HcclResult::HCCL_SUCCESS;
+}
+
+u64 CcuTempKfcServer::CalcScratchMultiple(BufferType inBuffType, BufferType outBuffType)
+{
+    (void)inBuffType;
+    (void)outBuffType;
+    return 0;
+}
+
+}
