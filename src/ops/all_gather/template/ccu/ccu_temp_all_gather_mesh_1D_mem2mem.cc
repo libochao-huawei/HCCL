5895c588542cd7921285400d86e2b115bef92a93
sqw <shiqiwen@huawei.com>
2026-05-18 16:32:13 +0800

    bugfix

--- src/ops/all_gather/template/ccu/ccu_temp_all_gather_mesh_1D_mem2mem.cc
+++ src/ops/all_gather/template/ccu/ccu_temp_all_gather_mesh_1D_mem2mem.cc
@@ -120,9 +120,8 @@ HcclResult CcuTempAllGatherMesh1DMem2Mem::KernelRun(const OpParam& param,
     uint64_t lastSliceSize      = templateDataParams.tailSize;
     // bool inputOutputEqual = (inputAddr + inputSliceStride * mySubCommRank_ == outputAddr + outputSliceStride * mySubCommRank_);
     // uint64_t isInputOutputEqual = static_cast<uint64_t>(inputOutputEqual);
-    uint64_t inputOutputEqual = (inputAddr == outputAddr)? 1 : 0;
+    uint64_t isInputOutputEqual = (inputAddr == outputAddr)? 1 : 0;
     // HCCL_RUN_INFO("[CcuTempAllGatherMesh1DMem2Mem][KernelRun] isInputOutputEqual [%llu] and inputOutputEqual_original [%llu]", isInputOutputEqual, inputOutputEqual_original);
-    // isInputOutputEqual = inputOutputEqual_original;
 
     if (templateDataParams.tailSize != 0 && mySubCommRank_ == templateRankSize_ - 1) {
         normalSliceSize = templateDataParams.tailSize;
