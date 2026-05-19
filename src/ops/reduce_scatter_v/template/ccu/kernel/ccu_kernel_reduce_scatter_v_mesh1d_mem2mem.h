8eacafc078404da9258c8164c61b471dc8a43d66
gcw_dFpssWuk <wanghaixu5@h-partners.com>
2026-05-19 17:06:40 +0800

    5.19

--- src/ops/reduce_scatter_v/template/ccu/kernel/ccu_kernel_reduce_scatter_v_mesh1d_mem2mem.h
+++ src/ops/reduce_scatter_v/template/ccu/kernel/ccu_kernel_reduce_scatter_v_mesh1d_mem2mem.h
@@ -41,7 +41,7 @@ struct ReduceScatterVMesh1DMem2MemContext: CcuKernelCtxBase {
     ccu::Variable scratch;
     ccu::Variable sliceSize;
     GroupOpSizeVars reduceGosize;
-    ccu::Event event; //
+    ccu::Event event; // gai
     ccu::LocalAddr scr;
     std::vector<ccu::RemoteAddr> dst;
     std::vector<ccu::RemoteAddr> reduceScatterVSrc;
