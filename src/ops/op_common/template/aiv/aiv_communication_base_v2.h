010d4939e074bf4624e679df4fd9d3b8c068a506
chenyia <chenyi163@huawei.com>
2026-05-19 22:17:16 +0800

    aiv graph mode stream

--- src/ops/op_common/template/aiv/aiv_communication_base_v2.h
+++ src/ops/op_common/template/aiv/aiv_communication_base_v2.h
@@ -497,6 +497,15 @@ __aicore__ inline bool AivCommBase::IsFirstOP(int32_t sliceId)
 
 __aicore__ inline void AivCommBase::BarrierForFirstOP()
 {
+    // 清零
+    uint32_t clearSize = BASE_FLAG_OFFSET;
+    uint32_t blockClearSize = clearSize / numBlocks_;
+    uint32_t blockClearOffset = blockClearSize * block_idx;
+    GlobalTensor<uint8_t> flagGT;
+    flagGT.SetGlobalBuffer(GM_OUT[rank_] + blockClearOffset, blockClearSize);
+    AscendC::Fill(flagGT, blockClearSize, 0);
+    SyncAll<true>();
+
     // 每个核分配多个rank
     uint32_t perCoreRankNum = rankSize_ / numBlocks_;
     uint32_t remainRankNum = rankSize_ % numBlocks_;
