5da7037598ceb653d74ae24a4693ae43eb6bf438
XuanYang11 <yangxuan77@h-partners.com>
2026-05-19 09:37:29 +0800

    +

--- scripts/package/hccl/scripts/hccl_custom_install.sh
+++ scripts/package/hccl/scripts/hccl_custom_install.sh
@@ -3,7 +3,7 @@
 # Copyright (c) Huawei Technologies Co., Ltd. 2022. All rights reserved.
 
 arch=$(uname -m 2>&1 | awk '{print tolower($0)}')
-sourcedir="$PWD/${arch}/hccl"
+sourcedir="$PWD/${arch}-linux"
 curpath=$(dirname $(readlink -f "$0"))
 common_func_path="${curpath}/common_func.inc"
 unset PYTHONPATH
