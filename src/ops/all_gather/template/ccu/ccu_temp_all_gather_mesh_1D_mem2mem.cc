c493bb3028d4d899f07ba51b42beb7b8ee4114c4
sqw <shiqiwen@huawei.com>
2026-05-20 21:19:52 +0800

    bugfix

--- src/ops/all_gather/template/ccu/ccu_temp_all_gather_mesh_1D_mem2mem.cc
+++ src/ops/all_gather/template/ccu/ccu_temp_all_gather_mesh_1D_mem2mem.cc
@@ -9,8 +9,6 @@
  */
 
 #include "channel.h"
-//#include "hccl_ccu_res.h"
-#include "ccu_assist_pub.h"
 #include "ccu_kernel_all_gather_mesh1d_mem2mem.h"
 #include "ccu_temp_all_gather_mesh_1D_mem2mem.h"
 #include "ccu_control_api.h"
