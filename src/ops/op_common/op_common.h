51cb4fb82d3259b717a7ab4eaf08148483b52940
shirokawa_ <jinliuyi@huawei.com>
2026-05-20 11:17:38 +0800

    new

--- src/ops/op_common/op_common.h
+++ src/ops/op_common/op_common.h
@@ -177,19 +177,4 @@ HcclResult SetExecTimeout(OpParam &param);
 bool IsHostDpu(HcclComm comm);
 }  // namespace ops_hccl
 
-#ifdef __cplusplus
-extern "C" {
-#endif
-
-HcclResult HcclSetAivCoreLimitGraphMode(const char *group, u32 aivCoreLimit);
-HcclResult HcclSelectAlgGraphMode(const char *group, u64 count, HcclDataType dataType, HcclReduceOp op, HcclCMDType opType,
-                           u32 aivCoreLimit, bool *ifAiv, char **algName);
-HcclResult HcclCalcAivCoreNumGraphMode(u32 aivCoreLimit, u32 *numBlocks);
-HcclResult HcclGetAlgExecParamGraphMode(const char *tag, const char *group, u64 count, void *inputPtr, void *outputPtr,
-                                 HcclCMDType opType, bool clearEnable, HcclDataType dataType, HcclReduceOp op,
-                                 void **commContext, u64 *len, u32 aivCoreLimit);
-#ifdef __cplusplus
-}
-#endif
-
 #endif
