6e7781177e2c23107799c968ef4a40fa0e3c96e9
weixin_43960572 <yuantangzhi@huawei.com>
2026-05-21 16:22:33 +0800

    allgather nhr decrease localcopy in nhr

--- src/ops/all_gather/template/aicpu/ins_temp_all_gather_nhr.h
+++ src/ops/all_gather/template/aicpu/ins_temp_all_gather_nhr.h
@@ -47,11 +47,30 @@ protected:
     TemplateDataParams tempAlgParams_;
     bool isDmaRead_{false};
 private:
+    bool CanReadLastStepToOutput() const;
+    bool IsLastStepReadSlice(u32 algRank) const;
     HcclResult PreprareDataSplitForMultiChannel(const TemplateResource &templateResource);
     HcclResult LocalDataCopy(const std::vector<ThreadHandle> &threads, const u32 &channelIdx);
-    HcclResult PostLocalCopy(const std::vector<ThreadHandle> &threads, const u32 &channelIdx);
+    HcclResult PostLocalCopy(const ThreadHandle &thread, const u32 &channelIdx);
+    HcclResult BuildNormalStepSlices(const ChannelInfo &channelSend, const ChannelInfo &channelRecv,
+        const AicpuNHRStepInfo &stepInfo, const u32 &channelIdx,
+        std::vector<DataSlice> &txSrcSlices, std::vector<DataSlice> &txDstSlices,
+        std::vector<DataSlice> &rxSrcSlices, std::vector<DataSlice> &rxDstSlices);
+    HcclResult RunStepNHR(const std::vector<ThreadHandle> &threads,
+        const std::map<u32, std::vector<ChannelInfo>> &channels, const u32 &channelIdx,
+        u32 step, u32 nSteps, bool &postLocalCopyLaunched);
     HcclResult RunAllGatherNHR(const std::vector<ThreadHandle> &threads,
-                               const std::map<u32, std::vector<ChannelInfo>> &channels, const u32 &channelIdx);
+                               const std::map<u32, std::vector<ChannelInfo>> &channels, const u32 &channelIdx,
+                               bool &postLocalCopyLaunched);
+    HcclResult BuildLastStepWriteThenReadSlices(const ChannelInfo &channelSend, const ChannelInfo &channelRecv,
+        const AicpuNHRStepInfo &stepInfo, const u32 &channelIdx,
+        std::vector<DataSlice> &txSrcSlices, std::vector<DataSlice> &txDstSlices,
+        std::vector<DataSlice> &rxSrcSlices, std::vector<DataSlice> &rxDstSlices);
+    HcclResult RunLastStepWriteThenRead(const std::vector<ThreadHandle> &threads, const ChannelInfo &channelSend,
+                                        const ChannelInfo &channelRecv, const AicpuNHRStepInfo &stepInfo,
+                                        const u32 &channelIdx, u32 step, bool &postLocalCopyLaunched);
+    bool readLastStepToOutput_{false};
+    std::vector<u32> lastStepReadSliceIdxs_;
     u64 dataTypeSize_{0};
     std::vector<u64> dataSplit_;
     std::vector<u64> dataOffset_;
