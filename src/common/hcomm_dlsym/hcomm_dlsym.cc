7267192963401ef5c3d2dcd00949dffb641fe1eb
Leewis <liwei945@huawei.com>
2026-05-21 14:54:24 +0800

    modify C++ RULE
    Co-authored-by: Leewis<liwei945@huawei.com>
    
    !912 merge master into master
    
    modify C++ RULE
    
    Created-by: Leewis
    Commit-by: Leewis
    Merged-by: cann-robot
    Description: ## 描述
    <!--在这里详细描述你的改动，包括改动的原因和所采取的方法。-->
    按照C++代码规范，更改头文件包含，使用新的标准C++文件
    <!-- 如果这个PR是为了解决特定的Issue，请在这里提供Issue链接。-->
    <!-- 如果这个PR不涉及Issue，可填写"NA"。-->
    https://gitcode.com/cann/hccl/issues/117
    https://gitcode.com/cann/hccl/issues/116
    
    <!--描述进行了哪些测试来验证你的改动。包括但不限于构造对应xx测试用例、二级冒烟、算子泛化等。-->
    冒烟测试
    
    <!--如果这个PR包含文档的更新，请在这里指出。例如：更新了README.md文件。-->
    
    <!--  [x] 表示选中 -->
    - [] Bug修复
    - [ ] 新特性
    - [ ] 性能优化
    - [] 文档更新
    - [x] 其他，请描述：代码规范
    
    See merge request: cann/hccl!912

--- src/common/hcomm_dlsym/hcomm_dlsym.cc
+++ src/common/hcomm_dlsym/hcomm_dlsym.cc
@@ -18,8 +18,8 @@
 #include "hccl_res_expt_dl.h"
 #include <pthread.h>
 #include <dlfcn.h>
-#include <stdio.h>
-#include <stdlib.h>
+#include <cstdio>
+#include <cstdlib>
 #include <acl/acl.h>
 
 static void* gLibHandle = nullptr;
