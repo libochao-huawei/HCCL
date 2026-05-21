f6d4c1e5976af960684462abc014964e0b3ac5d9
yjzz1007 <zhaojiayu7@huawei.com>
2026-05-21 20:09:15 +0800

    test

--- src/ops/reduce_scatter/template/ccu/ccu_temp_reduce_scatter_mesh_1D_mem2mem.cc
+++ src/ops/reduce_scatter/template/ccu/ccu_temp_reduce_scatter_mesh_1D_mem2mem.cc
@@ -51,9 +51,9 @@ HcclResult CcuTempReduceScatterMesh1DMem2Mem::CalcRes(HcclComm comm, const OpPar
                              return std::make_unique<CcuKernelReduceScatterMesh1DMem2Mem>(arg);
                          };
     std::vector<HcclChannelDesc> channelDescs;
-    if(topoInfo->level0Topo != Level0Shape::MESH_1D_CLOS) {
-        CHK_RET(CalcChannelRequestMesh1DFullMesh(comm, param, topoInfo, subCommRanks_, channelDescs));
-    } else {
+    // if(topoInfo->level0Topo != Level0Shape::MESH_1D_CLOS) {
+    //     CHK_RET(CalcChannelRequestMesh1DFullMesh(comm, param, topoInfo, subCommRanks_, channelDescs));
+    // } else {
         std::vector<HcclChannelDesc> myChannelDescs;
         CHK_RET(CalcChannelRequestMesh1DWithPriorityTopo(comm, param, topoInfo, subCommRanks_, myChannelDescs, CommTopo::COMM_TOPO_1DMESH));
         for(auto channel : myChannelDescs) {
@@ -62,7 +62,7 @@ HcclResult CcuTempReduceScatterMesh1DMem2Mem::CalcRes(HcclComm comm, const OpPar
             }
         }
         HCCL_DEBUG("[CcuTempReduceScatterMesh1DMem2Mem::CalcRes] Get Mesh Channel Success!");
-    }
+    //}
     
     kernelInfo.kernelArg = std::make_shared<CcuKernelArgReduceScatterMesh1DMem2Mem>(subCommRanks_[0].size(),
                                                                                     mySubCommRank_,
