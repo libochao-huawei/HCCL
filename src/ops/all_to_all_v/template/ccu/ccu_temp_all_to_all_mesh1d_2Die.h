cf8a8e1ce9bc919be521583e2180e711f1cec2db
NANYI00 <hanyan36@huawei.com>
2026-05-21 17:21:26 +0800

    052107

--- src/ops/all_to_all_v/template/ccu/ccu_temp_all_to_all_mesh1d_2Die.h
+++ src/ops/all_to_all_v/template/ccu/ccu_temp_all_to_all_mesh1d_2Die.h
@@ -39,7 +39,7 @@ private:
     HcclResult PartitionChannels(HcclComm comm, const std::vector<HcclChannelDesc> &channelDescs, uint32_t &meshDieId,
                                 std::map<u32, std::vector<HcclChannelDesc>>& rankIdToChannelDesc);
     HcclResult CalcChannelRequest(HcclComm comm, const OpParam& param, const TopoInfoWithNetLayerDetails* topoInfo,
-        const std::vector<std::vector<u32>>& subcommInfo, std::vector<HcclChannelDesc> &channels);
+        const std::vector<std::vector<u32>>& subcommInfo, std::vector<std::vector<HcclChannelDesc>> &channels);
     HcclResult ProcessLinkForProtocol(HcclComm comm, const std::vector<CommProtocol>& expectedProtocols,
         const std::vector<CommLink>& linkList, u32 myRank, u32 remoteRank, uint32_t netLayer,
         std::vector<HcclChannelDesc>& channels, bool& protocolFound, const std::string& funcName);
