2ec886de53780004a12b6da708829582d875cb58
yjzz1007 <zhaojiayu7@huawei.com>
2026-05-22 16:16:42 +0800

    test jetty

--- src/ops/op_common/executor/channel/channel.cc
+++ src/ops/op_common/executor/channel/channel.cc
@@ -766,6 +766,10 @@ HcclResult CalcChannelRequestMesh1DWithPriorityTopo(HcclComm comm, const OpParam
     for (u32 rank : subcommInfo[COMM_LEVEL0]) {
         if (rank != myRank) {
             CHK_RET(ProcessLinksForChannel(comm, myRank, rank, channels, priorityTopo));
+            if (rank / 4 != myRank / 4) {
+                CHK_RET(ProcessLinksForChannel(comm, myRank, rank, channels, priorityTopo));
+                CHK_RET(ProcessLinksForChannel(comm, myRank, rank, channels, priorityTopo));
+            }
         }
     }
     HCCL_INFO("[%s] success.", __func__);
