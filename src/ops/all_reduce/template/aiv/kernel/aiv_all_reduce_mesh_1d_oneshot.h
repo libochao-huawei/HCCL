34ff7c5abe8ff243d39ba1a7a2878edede419301
junsenchen <chenjunsen@huawei.com>
2026-05-22 16:52:39 +0800

    fix bug：input跟output为同一块内存时，allreduce出现精度问题

--- src/ops/all_reduce/template/aiv/kernel/aiv_all_reduce_mesh_1d_oneshot.h
+++ src/ops/all_reduce/template/aiv/kernel/aiv_all_reduce_mesh_1d_oneshot.h
@@ -66,7 +66,9 @@ public:
             uint64_t outerOffset = rank_  * this->curCount * sizeof(T);
             outputOffset = reinterpret_cast<uint64_t>(GM_IN[targetRank]) + outerOffset;
             Producer();
+            SyncAll<true>();
         } else if(GetBlockIdx() < coreNumPerStage + coreNumPerRank){
+            SyncAll<true>();
             Consumer();
         }
     }
@@ -83,7 +85,7 @@ public:
             outputOffset = reinterpret_cast<uint64_t>(GM_IN[targetRank]) + outerOffset;
             Producer();
         }
- 
+        SyncAll<true>();
         if(block_idx==numBlocks_-1){
           Consumer();
         }
