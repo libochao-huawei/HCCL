511424dd01319dacf7fdfb79e90272ecbc396779
shenjiahui_ <shenjiahui5@huawei.com>
2026-05-25 20:42:53 +0800

    fix bug

--- src/ops/reduce/executor/reduce_parallel_executor.cc
+++ src/ops/reduce/executor/reduce_parallel_executor.cc
@@ -455,7 +455,7 @@ HcclResult
     const u64 scratchMemBlockSize = maxTmpMemSize_ / totalScratchMultiple;
     CHK_PRT_RET(dataTypeSize_ == 0, "[ReduceParallelExecutor][OrchestrateImpl] dataTypeSize_ is 0", HCCL_E_INTERNAL);
     u64 maxCountPerLoop = scratchMemBlockSize / dataTypeSize_;
-    if (param.engine != CommEngine::COMM_ENGINE_AICPU_TS) {
+    if (param_.engine != CommEngine::COMM_ENGINE_AICPU_TS) {
         maxCountPerLoop = std::min<u64>(scratchMemBlockSize, UB_MAX_DATA_SIZE) / dataTypeSize_;
     }
     CHK_PRT_RET(maxCountPerLoop == 0, "[ReduceParallelExecutor][OrchestrateImpl] maxCountPerLoop is 0", HCCL_E_INTERNAL);
