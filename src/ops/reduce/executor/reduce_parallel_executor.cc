393da4cf60e3a6651c40cafd3bf72d10170a1d47
shenjiahui_ <shenjiahui5@huawei.com>
2026-05-25 19:05:43 +0800

    other engine use old ub data size

--- src/ops/reduce/executor/reduce_parallel_executor.cc
+++ src/ops/reduce/executor/reduce_parallel_executor.cc
@@ -452,9 +452,10 @@ HcclResult
 
     const u64 scratchMemBlockSize = maxTmpMemSize_ / totalScratchMultiple;
     CHK_PRT_RET(dataTypeSize_ == 0, "[ReduceParallelExecutor][OrchestrateImpl] dataTypeSize_ is 0", HCCL_E_INTERNAL);
-    u64 ubMaxDataCount = std::min(UB_MAX_DATA_SIZE * intraLocalRankSize_ / dataSplitSize0_,
-        UB_MAX_DATA_SIZE * interLocalRankSize_ / (1 - dataSplitSize0_));
-    const u64 maxCountPerLoop = std::min<u64>(scratchMemBlockSize, ubMaxDataCount) / dataTypeSize_;
+    u64 maxCountPerLoop = scratchMemBlockSize / dataTypeSize_;
+    if (param.engine != CommEngine::COMM_ENGINE_AICPU_TS) {
+        maxCountPerLoop = std::min<u64>(scratchMemBlockSize, UB_MAX_DATA_SIZE) / dataTypeSize_;
+    }
     CHK_PRT_RET(maxCountPerLoop == 0, "[ReduceParallelExecutor][OrchestrateImpl] maxCountPerLoop is 0", HCCL_E_INTERNAL);
     const u32 loopTimes = dataCount_ / maxCountPerLoop + ((dataCount_ % maxCountPerLoop == 0) ? 0 : 1);
 
