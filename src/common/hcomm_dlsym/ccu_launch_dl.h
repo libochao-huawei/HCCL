cfd6bc26042c3ca37ebbe7768210882ad2a848cd
x30067372 <xujiahui27@h-partners.com>
2026-05-26 11:01:02 +0800

    c接口兼容性修改

---
+++ src/common/hcomm_dlsym/ccu_launch_dl.h
@@ -0,0 +1,32 @@
+/**
+ * Copyright (c) 2026 Huawei Technologies Co., Ltd.
+  * This program is free software, you can redistribute it and/or modify it under the terms and conditions of
+  * CANN Open Software License Agreement Version 2.0 (the "License").
+  * Please refer to the License for details. You may not use this file except in compliance with the License.
+  * THIS SOFTWARE IS PROVIDED ON AN "AS IS" BASIS, WITHOUT WARRANTIES OF ANY KIND, EITHER EXPRESS OR IMPLIED,
+  * INCLUDING BUT NOT LIMITED TO NON-INFRINGEMENT, MERCHANTABILITY, OR FITNESS FOR A PARTICULAR PURPOSE.
+  * See LICENSE in the root of the software repository for the full text of the License.
+  */
+
+#ifndef CCU_LAUNCH_DL_H
+#define CCU_LAUNCH_DL_H
+
+#include "dlsym_common.h"
+#include "ccu_launch.h"   // 原始头文件，包含所有类型和声明
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+DECL_WEAK_FUNC(CcuResult, HcommCcuKernelRegisterStart, CcuInsHandle insHandle);
+DECL_WEAK_FUNC(CcuResult, HcommCcuKernelRegister, CcuInsHandle insHandle, const char *kernelFuncName, const void *kernelFunc, const void *kernelArg, CcuKernelHandle *kernelHandle);
+DECL_WEAK_FUNC(CcuResult, HcommCcuKernelRegisterEnd, CcuInsHandle insHandle);
+DECL_WEAK_FUNC(CcuResult, HcommCcuKernelLaunch, ThreadHandle threadHandle, CcuKernelHandle kernelHandle, const void *taskArgs, uint32_t argSize);
+
+void CcuLaunchDlInit(void* libHcommHandle);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif // CCU_LAUNCH_DL_H
