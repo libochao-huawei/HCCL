228d808575b89484541a4e2d175b98cb4fd6a0bb
x30067372 <xujiahui27@h-partners.com>
2026-05-26 11:04:05 +0800

    c接口兼容性修改

--- src/common/hcomm_dlsym/ccu_res_dl.cc
+++ src/common/hcomm_dlsym/ccu_res_dl.cc
@@ -8,7 +8,7 @@
  * See LICENSE in the root of the software repository for the full text of the License.
  */
 
-#include "hcomm_ccu_dl.h"
+#include "ccu_res_dl.h"
 #include <dlfcn.h>
 #include <stdio.h>
 #include <stdlib.h>
