890839e90a5a9ab2a78458f38ddf7bfa7bc3d288
XuanYang11 <yangxuan77@h-partners.com>
2026-05-26 09:17:29 +0800

    整改完src下全部代码

--- src/common/hcomm_dlsym/hccl_compat.cmake
+++ src/common/hcomm_dlsym/hccl_compat.cmake
@@ -1,5 +1,15 @@
+# ----------------------------------------------------------------------------
+# Copyright (c) 2026 Huawei Technologies Co., Ltd.
+# This program is free software, you can redistribute it and/or modify it under the terms and conditions of
+# CANN Open Software License Agreement Version 2.0 (the "License").
+# Please refer to the License for details. You may not use this file except in compliance with the License.
+# THIS SOFTWARE IS PROVIDED ON AN "AS IS" BASIS, WITHOUT WARRANTIES OF ANY KIND, EITHER EXPRESS OR IMPLIED,
+# INCLUDING BUT NOT LIMITED TO NON-INFRINGEMENT, MERCHANTABILITY, OR FITNESS FOR A PARTICULAR PURPOSE.
+# See LICENSE in the root of the software repository for the full text of the License.
+# ----------------------------------------------------------------------------
+
 if(STATIC_MODE)
-    target_sources(${HCCT_TARGET} PRIVATE
+    target_sources(hccl PRIVATE
         ${CMAKE_CURRENT_SOURCE_DIR}/hccl_rank_graph_dl.cc
         ${CMAKE_CURRENT_SOURCE_DIR}/hccl_res_dl.cc
         ${CMAKE_CURRENT_SOURCE_DIR}/hcomm_primitives_dl.cc
