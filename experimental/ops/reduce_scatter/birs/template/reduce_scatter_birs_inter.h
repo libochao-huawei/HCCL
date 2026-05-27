0ab6fb15ce5d22f68f7f539da9e54573c827c9d5
Davydov_Danil <davydov.danil1@huawei.com>
2026-05-28 02:09:25 +0800

    Add ReduceScatter BIRS support

---
+++ experimental/ops/reduce_scatter/birs/template/reduce_scatter_birs_inter.h
@@ -0,0 +1,53 @@
+/**
+ * Copyright (c) 2026 Huawei Technologies Co., Ltd.
+ * This program is free software, you can redistribute it and/or modify it under the terms and conditions of
+ * CANN Open Software License Agreement Version 2.0 (the "License").
+ * Please refer to the License for details. You may not use this file except in compliance with the License.
+ * THIS SOFTWARE IS PROVIDED ON AN "AS IS" BASIS, WITHOUT WARRANTIES OF ANY KIND, EITHER EXPRESS OR IMPLIED,
+ * INCLUDING BUT NOT LIMITED TO NON-INFRINGEMENT, MERCHANTABILITY, OR FITNESS FOR A PARTICULAR PURPOSE.
+ * See LICENSE in the root of the software repository for the full text of the License.
+ */
+
+#ifndef REDUCE_SCATTER_BIRS_INTER_H
+#define REDUCE_SCATTER_BIRS_INTER_H
+
+#include "reduce_scatter_birs.h"
+
+namespace ops_hccl_experimental {
+using ops_hccl::AlgTemplateBase;
+using ops_hccl::Slice;
+using ops_hccl::ChannelInfo;
+
+class ReduceScatterBIRSInter : public ReduceScatterBIRS {
+public:
+    explicit ReduceScatterBIRSInter();
+
+    ~ReduceScatterBIRSInter() override;
+
+    HcclResult Prepare(u32 serverNum, u32 interRankSize) override;
+
+    HcclResult RunAsync(const u32 rank, const u32 rankSize, std::vector<ChannelInfo> &channels) override;
+
+protected:
+    HcclResult LocalCopyPreproc(ThreadHandle &stream, const u32 rank, u64 sliceSize, u64 localStrideSize);
+
+    HcclResult Preprocess(const u32 rank, const u32 rankSize, std::vector<ChannelInfo> &channels) override;
+    HcclResult HCCSInterStep(u32 round, const u32 rank, const u32 rankSize, u32 rankSizeX_,
+                            u64 sliceSize, u64 localStrideSize);
+    HcclResult SIOInterStep(u32 round, const u32 rank, const u32 rankSize, u32 rankSizeX_,
+                            u64 sliceSize, u64 localStrideSize);
+    HcclResult LocalCopyInterStep(u32 round, const u32 rank, const u32 rankSize, u32 rankSizeX_,
+                            u64 sliceSize, u64 localStrideSize);
+    HcclResult PreprocIntra(const u32 rank, const u32 rankSize, u32 rankSizeX_, u64 sliceSize,
+                            u64 localStrideSize, std::vector<ChannelInfo> &channels);
+    HcclResult IntraLoop(const u32 rank, const u32 rankSize, u32 rankSizeX_, u64 sliceSize,
+                        u64 localStrideSize, std::vector<ChannelInfo> &channels);
+
+private:
+    u32 serverNum_;
+    u32 intraRankSize_;
+    std::vector<u32> vec_offsets;
+};
+}
+
+#endif /* * REDUCE_SCATTER_BIRS_INTER_H */
