e0030efd8810513df2ece98c5d42b200ad0352a9
Davydov_Danil <davydov.danil1@huawei.com>
2026-05-28 02:09:25 +0800

    Add ReduceScatter BIRS support

--- src/ops/scatter/scatter_op.h
+++ src/ops/scatter/scatter_op.h
@@ -12,6 +12,7 @@
 #define OPS_HCCL_SRC_OPS_SCATTER_OP
 
 #include <array>
+#include <map>
 #include <string>
 #include "hccl.h"
 
@@ -33,6 +34,8 @@ HcclResult HcclScatter(void *sendBuf, void *recvBuf, uint64_t recvCount, HcclDat
 
 namespace ops_hccl {
 using NotifyArray = std::array<aclrtNotify, AICPU_CONTROL_NOTIFY_NUM>;
+extern thread_local aclrtNotify g_notifies[AICPU_CONTROL_NOTIFY_NUM];
+extern thread_local std::map<HcclComm, NotifyArray> g_notifiesMap;
 
 HcclResult ScatterOutPlace(OpParam &param, void *sendBuf, void *recvBuf, uint64_t recvCount, HcclDataType dataType, uint32_t root,
     HcclComm comm, aclrtStream stream, u32 userRankSize);
