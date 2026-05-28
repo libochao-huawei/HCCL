a0b4f53cca39f23c8524b38f8752d8a93b0ff40b
zhongyu1 <zhongyu37@huawei.com>
2026-05-28 14:38:00 +0800

    ag + rs mesh2die mem2mem

--- src/ops/reduce_scatter/executor/ins_v2_reduce_scatter_sole_executor.cc
+++ src/ops/reduce_scatter/executor/ins_v2_reduce_scatter_sole_executor.cc
@@ -20,7 +20,7 @@
 #include "ccu_temp_reduce_scatter_mesh_1D_mem2mem.h"
 #include "ccu_temp_reduce_scatter_mesh_1D.h"
 #include "ccu_temp_reduce_scatter_nhr_1D_mem2mem.h"
-// #include "ccu_temp_reduce_scatter_mesh_1D_2die_mem2mem.h"
+#include "ccu_temp_reduce_scatter_mesh_1D_2die_mem2mem.h"
 // #include "ccu_temp_reduce_scatter_mesh2die.h"
 // #include "ccu_temp_reduce_scatter_nhr_1D_multi_jetty_mem2mem.h"
 #endif /* !HCCL_CANN_COMPAT_850 */
@@ -273,10 +273,10 @@ REGISTER_EXEC_V2(HcclCMDType::HCCL_CMD_REDUCE_SCATTER, CcuReduceScatterMesh1D, I
 REGISTER_EXEC_V2(HcclCMDType::HCCL_CMD_REDUCE_SCATTER, CcuReduceScatterNHR1DMem2Mem, InsV2ReduceScatterSoleExecutor, TopoMatch1D,
     CcuTempReduceScatterNHR1DMem2Mem);
 #endif /* !HCCL_CANN_COMPAT_850 */
-// #if !defined(HCCL_CANN_COMPAT_850)
-// REGISTER_EXEC_V2(HcclCMDType::HCCL_CMD_REDUCE_SCATTER, CcuReduceScatterMeshMem2Mem1D2Die, InsV2ReduceScatterSoleExecutor, TopoMatch1D,
-//     CcuTempReduceScatterMeshMem2Mem1D2Die);
-// #endif /* !HCCL_CANN_COMPAT_850 */
+#if !defined(HCCL_CANN_COMPAT_850)
+REGISTER_EXEC_V2(HcclCMDType::HCCL_CMD_REDUCE_SCATTER, CcuReduceScatterMeshMem2Mem1D2Die, InsV2ReduceScatterSoleExecutor, TopoMatch1D,
+    CcuTempReduceScatterMeshMem2Mem1D2Die);
+#endif /* !HCCL_CANN_COMPAT_850 */
 // #if !defined(HCCL_CANN_COMPAT_850)
 // REGISTER_EXEC_V2(HcclCMDType::HCCL_CMD_REDUCE_SCATTER, CcuReduceScatterMesh2Die, InsV2ReduceScatterSoleExecutor, TopoMatch1D,
 //     CcuTempReduceScatterMesh2Die);
