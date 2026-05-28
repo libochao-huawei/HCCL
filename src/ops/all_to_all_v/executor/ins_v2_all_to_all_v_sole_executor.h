893f84465a14fd0c2e36a31942a599e92fb55423
zghre <huangjingzhi5@h-partners.com>
2026-05-28 19:50:55 +0800

    code check

--- src/ops/all_to_all_v/executor/ins_v2_all_to_all_v_sole_executor.h
+++ src/ops/all_to_all_v/executor/ins_v2_all_to_all_v_sole_executor.h
@@ -28,8 +28,8 @@ public:
     /* *************** 资源计算 *************** */
 
     HcclResult CalcRes(HcclComm comm, const OpParam& param,
-                       const TopoInfoWithNetLayerDetails* topoInfo, const AlgHierarchyInfoForAllLevel& algHierarchyInfo,
-                       AlgResourceRequest& resourceRequest) override;
+                const TopoInfoWithNetLayerDetails* topoInfo, const AlgHierarchyInfoForAllLevel& algHierarchyInfo,
+                AlgResourceRequest& resourceRequest) override;
 
     HcclResult CalcAlgHierarchyInfo(HcclComm comm, TopoInfoWithNetLayerDetails* topoInfo,
                                     AlgHierarchyInfoForAllLevel& algHierarchyInfo) override;
