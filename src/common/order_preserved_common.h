94996ae2276909cd9d3debc98e64ce618e1a9f9e
zghre <huangjingzhi5@h-partners.com>
2026-05-28 14:35:16 +0800

    feat(ops): ReduceScatter和AllReduce算子支持batch一致性
    Co-authored-by: zghre<huangjingzhi5@h-partners.com>
    
    !1066 merge master into master
    
    feat(ops): ReduceScatter和AllReduce算子支持batch一致性
    
    Created-by: zghre
    Commit-by: zghre
    Merged-by: cann-robot
    Description: ## 描述
    ReduceScatter和AllReduce算子新增order_preserved executor，支持batch一致性功能
    新增order_preserved_common.h公共头文件
    新增相关的executor和template实现
    
    NA
    
    - [ ] Bug修复
    - [x] 新特性
    - [ ] 性能优化
    - [ ] 文档更新
    - [ ] 其他，请描述：
    
    See merge request: cann/hccl!1066

---
+++ src/common/order_preserved_common.h
@@ -0,0 +1,40 @@
+/**
+ * Copyright (c) 2025 Huawei Technologies Co., Ltd.
+ * This program is free software, you can redistribute it and/or modify it under the terms and conditions of
+ * CANN Open Software License Agreement Version 2.0 (the "License").
+ * Please refer to the License for details. You may not use this file except in compliance with the License.
+ * THIS SOFTWARE IS PROVIDED ON AN "AS IS" BASIS, WITHOUT WARRANTIES OF ANY KIND, EITHER EXPRESS OR IMPLIED,
+ * INCLUDING BUT NOT LIMITED TO NON-INFRINGEMENT, MERCHANTABILITY, OR FITNESS FOR A PARTICULAR PURPOSE.
+ * See LICENSE in the root of the software repository for the full text of the License.
+ */
+
+#ifndef HCCL_ORDER_PRESERVED_COMMON_H
+#define HCCL_ORDER_PRESERVED_COMMON_H
+
+#include "alg_param.h"
+#include "alg_env_config.h"
+
+namespace ops_hccl {
+
+constexpr u32 MIN_STRICT_RANK_NUM_ORDER_PRESERVED = 2;
+constexpr u32 MAX_RANK_NUM_FOR_ORDER_PRESERVED = 32;
+
+inline bool IsNeedStrictModeForOrderPreserved(const OpParam& opParam, u32 rankSize)
+{
+    u8 deterministicLevel = GetExternalInputHcclDeterministic();
+    HcclDataType dataType = opParam.DataDes.dataType;
+    HcclReduceOp reduceType = opParam.reduceType;
+    
+    return (deterministicLevel == static_cast<u8>(DeterministicEnableLevel::DETERMINISTIC_STRICT))
+        && (dataType == HcclDataType::HCCL_DATA_TYPE_FP16 ||
+            dataType == HcclDataType::HCCL_DATA_TYPE_FP32 ||
+            dataType == HcclDataType::HCCL_DATA_TYPE_BFP16 ||
+            dataType == HcclDataType::HCCL_DATA_TYPE_FP64)
+        && (reduceType == HcclReduceOp::HCCL_REDUCE_SUM ||
+            reduceType == HcclReduceOp::HCCL_REDUCE_PROD)
+        && rankSize > MIN_STRICT_RANK_NUM_ORDER_PRESERVED;
+}
+
+} // namespace ops_hccl
+
+#endif // HCCL_ORDER_PRESERVED_COMMON_H
