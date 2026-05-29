c5779222c635d6ba35470965c94439e3f97f7d84
DengLLy <lideng2@huawei.com>
2026-05-29 14:46:18 +0800

    [refactor] 用TopoMatch1D替代TopoMatchMultilevelMesh1D: 扁平化拓扑复用现有1D匹配器
    - 删除TopoMatchMultilevelMesh1D类(.h/.cc)，改用TopoMatch1D
    - TopoMatch1D topoLevelNums校验从1-2层放宽至1-3层，使用COMM_LAYER_SIZE_3常量
    - 设备类型不匹配从仅日志改为CHK_PRT_RET返回HCCL_E_PARA错误
    - Describe()更新为反映1-3层拓扑支持
    - 移除CMakeLists中topo_match_multilevel_mesh1d.cc编译项
    - executor注册中TopoMatchMultilevelMesh1D改为TopoMatch1D，移除多余include

--- src/ops/all_gather_v/executor/ins_v2_all_gather_v_sole_executor.cc
+++ src/ops/all_gather_v/executor/ins_v2_all_gather_v_sole_executor.cc
@@ -10,7 +10,6 @@
 
 #include "ins_v2_all_gather_v_sole_executor.h"
 #include "topo_match_1d.h"
-#include "topo_match_multilevel_mesh1d.h"
 #include "ins_temp_all_gather_v_mesh_1D.h"
 
 #ifndef AICPU_COMPILE
@@ -197,7 +196,7 @@ REGISTER_EXEC_V2(HcclCMDType::HCCL_CMD_ALLGATHER_V, InsAllGatherVMesh1D, InsV2Al
     InsTempAllGatherVMesh1D);
 #if !defined(HCCL_CANN_COMPAT_850)
 REGISTER_EXEC_V2(HcclCMDType::HCCL_CMD_ALLGATHER_V, InsAllGatherVMultilevelMesh1D, InsV2AllGatherVSoleExecutor,
-    TopoMatchMultilevelMesh1D, InsTempAllGatherVMesh1D);
+    TopoMatch1D, InsTempAllGatherVMesh1D);
 #endif /* !HCCL_CANN_COMPAT_850 */
 #ifndef AICPU_COMPILE
 #if !defined(HCCL_CANN_COMPAT_850)
