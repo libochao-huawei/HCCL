0492dfe128fcabb6c4cd25c1c4e7aece0ad73c94
HwH258 <huangweihao6@huawei.com>
2026-05-30 18:00:49 +0800

    rs seq

---
+++ test/st/algorithm/testcase/reduce_scatter_ccu_testcase.cc
@@ -0,0 +1,118 @@
+/**
+ * Copyright (c) 2025 Huawei Technologies Co., Ltd.
+ * This program is free software, you can redistribute it and/or modify it under the terms and conditions of
+ * CANN Open Software License Agreement Version 2.0 (the "License").
+ * Please refer to the License for details. You may not use this file except in compliance with the License.
+ * THIS SOFTWARE IS PROVIDED ON AN "AS IS" BASIS, WITHOUT WARRANTIES OF ANY KIND, EITHER EXPRESS OR IMPLIED,
+ * INCLUDING BUT NOT LIMITED TO NON-INFRINGEMENT, MERCHANTABILITY, OR FITNESS FOR A PARTICULAR PURPOSE.
+ * See LICENSE in the root of the software repository for the full text of the License.
+ */
+ 
+#include "gtest/gtest.h"
+#include "sim_world.h"
+#include "hccl.h"
+#include "hccl/hccl_types.h"
+#include "acl/acl_rt.h"
+#include "hccl_verifier.h"
+#include "check_utils.h"
+#include <thread>
+#include "alg_env_config.h"
+ 
+using namespace HcclSim;
+using namespace ops_hccl;
+
+constexpr uint32_t DATATYPE_SIZE_TABLE_REDUCE_SCATTER[HCCL_DATA_TYPE_RESERVED] = {sizeof(int8_t), sizeof(int16_t), sizeof(int32_t),
+    2, sizeof(float), sizeof(int64_t), sizeof(uint64_t), sizeof(uint8_t), sizeof(uint16_t), sizeof(uint32_t),
+    8, 2, 16, 2, 1, 1, 1, 1};
+
+ 
+class ST_REDUCE_SCATTER_CCU_TEST : public ::testing::Test {
+protected:
+    void SetUp() override
+    {
+        ResetAlgEnvConfigInitState();
+    }
+    void TearDown() override
+    {
+        unsetenv("HCCL_OP_EXPANSION_MODE");
+        unsetenv("HCCL_ENABLE_OPEN_AICPU");
+    }
+    static void SetUpTestCase()
+    {}
+    static void TearDownTestCase()
+    {}
+};
+ 
+void RunReduceScatterCcuA5(const TopoMeta &topoMeta, const u64 &recvCount, const HcclDataType &dataType,
+    const HcclReduceOp &reduceOp)
+{
+    // 仿真模型初始化
+    SimWorld::Global()->Init(topoMeta, DevType::DEV_TYPE_950);
+ 
+    // 设置展开模式为AI_CPU
+    setenv("HCCL_INDEPENDENT_OP", "1", 1);
+    
+    // 算子执行参数设置
+    u32 rankSize = 0;  // 参与集合通信的卡数(同topoMeta卡数一致)
+    for (const auto &superPod : topoMeta) {
+        for (const auto &podIdx : superPod) {
+            rankSize += podIdx.size();
+        }
+    }
+
+    const u32 dataTypeSize = DATATYPE_SIZE_TABLE_REDUCE_SCATTER[dataType];
+    // 多线程运行SCATTER算子
+    std::vector<std::thread> threads;
+    for (auto rankId = 0; rankId < rankSize; ++rankId) {
+        threads.emplace_back([=]() {
+            // 1.SetDevice
+            aclrtSetDevice(rankId);
+ 
+            // 2.创建流
+            aclrtStream stream = nullptr;
+            aclrtCreateStream(&stream);
+ 
+            // 3.初始化通信域
+            HcclComm comm = nullptr;
+            CHK_RET(HcclCommInitClusterInfo("./ranktable.json", rankId, &comm));
+ 
+            void *sendBuf = nullptr;
+            void *recvBuf = nullptr;
+            u64 sendBufSize = recvCount * dataTypeSize * rankSize;  // 数据量转化为字节数
+            u64 recvBufSize = recvCount * dataTypeSize;
+            // 打桩实现，仿真运行需标记内存是INPUT和OUTPUT
+            aclrtMalloc(&sendBuf, sendBufSize, static_cast<aclrtMemMallocPolicy>(BUFFER_INPUT_MARK));
+            aclrtMalloc(&recvBuf, recvBufSize, static_cast<aclrtMemMallocPolicy>(BUFFER_OUTPUT_MARK));
+ 
+            // 4.算子下发
+            CHK_RET(HcclReduceScatter(sendBuf, recvBuf, recvCount, dataType, reduceOp, comm, stream));
+ 
+            // 5.销毁通信域
+            CHK_RET(HcclCommDestroy(comm));
+            return HCCL_SUCCESS;
+        });
+    }
+ 
+    // 等待多线程执行完成
+    for (auto& thread : threads) {
+        thread.join();
+    }
+ 
+    // 结果成图校验
+    auto taskQueues = SimTaskQueue::Global()->GetAllRankTaskQueues();
+    HcclResult res = CheckReduceScatter(taskQueues, rankSize, dataType, recvCount, reduceOp);
+    EXPECT_TRUE(res == HCCL_SUCCESS);
+ 
+    // 资源清理
+    SimWorld::Global()->Deinit();
+}
+
+TEST_F(ST_REDUCE_SCATTER_CCU_TEST, st_reduce_scatter_a5_aicpu_mesh_3rank_int32_max_test)
+{
+    // 仿真模型初始化
+    TopoMeta topoMeta {{{0, 1}, {0, 1}}}; // 三维数组指定超节点-Server-Device信息
+    auto recvCount = 100;                                // 单卡数据量
+    auto dataType = HcclDataType::HCCL_DATA_TYPE_INT32;  // 数据类型
+    auto reduceOp = HcclReduceOp::HCCL_REDUCE_MIN;
+    RunReduceScatterCcuA5(topoMeta, recvCount, dataType, reduceOp);
+}
