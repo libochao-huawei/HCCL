1d67f80cd6c28db9c8a7b7eb5dd66f8a9d2e804a
chengym <chengyueming@huawei.com>
2026-05-30 15:43:52 +0800

    delete useless codes

--- src/ops/broadcast/executor/ins_v2_broadcast_sequence_executor.cc
+++ src/ops/broadcast/executor/ins_v2_broadcast_sequence_executor.cc
@@ -138,9 +138,6 @@ HcclResult InsV2BroadcastSequenceExecutor<AlgTopoMatch, InsAlgTemplate0, InsAlgT
     rankSizeLevel0_ = algHierarchyInfo_.infos[0][0].size();
     rankSizeLevel1_ = algHierarchyInfo_.infos[1][0].size();
 
-    // 计算框内的root同号卡
-    intraLocalRoot_ = root_ % rankSizeLevel0_ + rankIdxLevel1_ * rankIdxLevel0_;
-
     dataCount_ = param.DataDes.count;
     dataTypeSize_ = SIZE_TABLE[param.DataDes.dataType];
     dataSize_ = dataCount_ * dataTypeSize_;
