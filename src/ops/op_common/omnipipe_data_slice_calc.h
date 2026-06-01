0f26da6f2ffbecde7c712f31124df90ec7383371
zghre <huangjingzhi5@h-partners.com>
2026-06-01 12:07:41 +0800

    code check

--- src/ops/op_common/omnipipe_data_slice_calc.h
+++ src/ops/op_common/omnipipe_data_slice_calc.h
@@ -206,7 +206,7 @@ double CalcBandwidth2D(double xB, double yB, u64 xRankSize, u64 yRankSize, int m
 void CalAllgather2DOffset(u64* xAGOffset, u64* yAGOffset, u64 stepNum, u64 xRankSize, u64 yRankSize, u64* xAGDataSize,
                           u64* yAGDataSize);
 u64 CalAllgatherDataSizeRatio2D(double* xStepP2pDataSize, double* yStepP2pDataSize, double xB, double yB, u64 xRankSize,
-                                u64 yRankSize, double dataSize, u64 maxStep)
+                                u64 yRankSize, double dataSize, u64 maxStep);
 u64 CalAllgatherDataSize2D(u64* xStepP2pDataSize, u64* yStepP2pDataSize, double xB, double yB, u64 xRankSize,
                            u64 yRankSize, u64 dataSizeEachRank, u64 maxStep);
 OmniPipeSliceInfo CalcAGOmniPipeSliceInfo(OmniPipeSliceParam& omniPipeSliceParam);
