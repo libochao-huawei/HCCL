0878342fb30abe298760db62c73bc16be5cfee77
liujt_ <liujintao28@huawei.com>
2026-06-01 11:37:36 +0800

    add 3d alg

--- src/ops/all_gather/executor/ins_v2_all_gather_omnipipe_executor.h
+++ src/ops/all_gather/executor/ins_v2_all_gather_omnipipe_executor.h
@@ -53,6 +53,16 @@ protected:
         std::shared_ptr<InsAlgTemplateBase> tempAlg, AlgResourceRequest &resourceRequest);
 
 private:
+    enum class TopoType { UBX_2LEVEL, THREE_LEVEL };
+    TopoType topoType_ = TopoType::UBX_2LEVEL;
+
+    HcclResult BuildSubCommAndTempMap(
+        const OpParam& param,
+        const AlgHierarchyInfoForAllLevel& algHierarchyInfo,
+        std::vector<std::vector<u32>>& subCommRanks0,
+        std::vector<std::vector<u32>>& subCommRanks1,
+        std::vector<std::vector<u32>>& subCommRanks2,
+        std::map<u32, std::shared_ptr<InsAlgTemplateBase>>& tempMap);
 
     std::vector<uint64_t> rankSizeLevel_;
     std::vector<uint64_t> rankIdxLevel_;
