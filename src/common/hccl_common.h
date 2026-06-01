74c63406771361bbec8228494263e348f55a9b77
zghre <huangjingzhi5@h-partners.com>
2026-06-01 17:08:41 +0800

    hccl_common.h
    Signed-off-by: zghre <huangjingzhi5@h-partners.com>

--- src/common/hccl_common.h
+++ src/common/hccl_common.h
@@ -150,6 +150,15 @@ inline std::string GetDataTypeEnumStr(u32 dataType)
     return GetDataTypeEnumStr(hcclDataType);
 }
 
+inline std::string GetDataArrayStr(const u64* arr, u32 size)
+{
+    std::string result;
+    for (u32 i =0;i < size;++i){
+        result += (i == 0 ? "[" : ",") + std::to_string(arr[i]);
+    }
+    return result + "]";
+}
+
 // server内link类型
 enum class LinkTypeInServer {
     HCCS_TYPE = 0,
