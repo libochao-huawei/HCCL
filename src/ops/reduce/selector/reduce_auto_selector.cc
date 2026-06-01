80cbfd7a73c0c8f43b6550f20b35b8352015541a
NANYI00 <hanyan36@huawei.com>
2026-06-01 17:10:05 +0800

    060102

--- src/ops/reduce/selector/reduce_auto_selector.cc
+++ src/ops/reduce/selector/reduce_auto_selector.cc
@@ -42,11 +42,6 @@ SelectorStatus ReduceAutoSelector::SelectCcuMsAlgo(const TopoInfoWithNetLayerDet
         return SelectorStatus::NOT_MATCH;
     }
 
-    if(IsInputOutputOverlap(opParam) == true){
-        HCCL_WARNING("[ReduceAutoSelector] ccu_ms mode not support inplace.");
-        return SelectorStatus::NOT_MATCH;
-    }
-
     if (topoInfo->topoLevelNums > 1) {
         HCCL_WARNING("[ReduceAutoSelector] levelNum > 1 is not supported yet for ccu_ms mode.");
         return SelectorStatus::NOT_MATCH;
@@ -126,10 +121,6 @@ SelectorStatus ReduceAutoSelector::SelectCcuScheduleAlgo(const TopoInfoWithNetLa
         return SelectorStatus::NOT_MATCH;
     }
     
-    if(IsInputOutputOverlap(opParam) == true){
-        HCCL_WARNING("[ReduceAutoSelector] ccu_sched mode not support inplace.");
-        return SelectorStatus::NOT_MATCH;
-    }
     if (topoInfo->topoLevelNums > 1) {
         if (topoInfo->level0Topo == Level0Shape::MESH_1D) {
             if (topoInfo->netLayerDetails.localNetInsSizeOfLayer.at(0) == 1) {
