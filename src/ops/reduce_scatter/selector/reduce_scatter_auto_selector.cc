1e1e0b7347e6bbdf6626f44615dbb1df3006ac64
NANYI00 <hanyan36@huawei.com>
2026-06-02 15:02:12 +0800

    060201

--- src/ops/reduce_scatter/selector/reduce_scatter_auto_selector.cc
+++ src/ops/reduce_scatter/selector/reduce_scatter_auto_selector.cc
@@ -66,6 +66,10 @@ SelectorStatus ReduceScatterAutoSelector::SelectMeshAlgoCcums(const TopoInfoWith
     u64 perDataSize = DATATYPE_SIZE_TABLE[opParam.DataDes.dataType];
     u64 dataSize = opParam.DataDes.count * perDataSize;
     if (topoInfo->level0Topo == Level0Shape::MESH_1D) {
+        if (IsInputOutputOverlap(opParam) == true) { // 不支持 inplace 场景
+            HCCL_WARNING("[ReduceScatterAutoSelector] ccu_ms mode not support inplace.");
+            return SelectorStatus::NOT_MATCH;
+        }
         if (topoInfo->level0MeshType == Level0MeshType::TWO_DIE_REGULAR) {
             selectAlgName = "CcuReduceScatterMesh2Die";
         } else if (topoInfo->level0MeshType == Level0MeshType::TWO_DIE_NOT_REGULAR) {
@@ -145,7 +149,7 @@ SelectorStatus ReduceScatterAutoSelector::SelectCcuScheduleAlgo(const TopoInfoWi
                 CHK_PRT_RET(opParam.DataDes.dataType == HcclDataType::HCCL_DATA_TYPE_INT8,
                 HCCL_WARNING("[ReduceScatterAutoSelector] dataType[%d] is not supported yet for ccu schedule mode.",
                     opParam.DataDes.dataType), SelectorStatus::NOT_MATCH);
-                if ((dataSize * topoInfo->userRankSize) <= RS_FLATTEN_MAX_DATA_SIZE && topoInfo->userRankSize <= ccuSize) {
+                if ((dataSize * topoInfo->userRankSize) <= RS_FLATTEN_MAX_DATA_SIZE && topoInfo->userRankSize <= ccuSize && (!IsInputOutputOverlap(opParam))) {
                     selectAlgName = "CcuReduceScatterMesh1DMem2Mem";
                     return SelectorStatus::MATCH;
                 } else if (dataSize * topoInfo->userRankSize <= RS_CCU_64P_MIN_DATA_SIZE && topoInfo->userRankSize == ccuSize){
@@ -191,6 +195,10 @@ SelectorStatus ReduceScatterAutoSelector::SelectMeshAlgoCcuScheduleMesh1D(const 
     if (dataSize * ratio >= RS_M2M_1D_MAX_DATA_SIZE) {
         HCCL_DEBUG("[ReduceScatterAutoSelector] dataSize[%lu] * ratio[%f] >= MAX_DATA_SIZE[%lu].",
                    dataSize, ratio, RS_M2M_1D_MAX_DATA_SIZE);
+        return SelectorStatus::NOT_MATCH;
+    }
+    if (IsInputOutputOverlap(opParam) == true) { // 不支持 inplace 场景
+        HCCL_WARNING("[ReduceScatterAutoSelector] ccu_ms mode not support inplace.");
         return SelectorStatus::NOT_MATCH;
     }
     if (topoInfo->level0MeshType == Level0MeshType::TWO_DIE_REGULAR) {
