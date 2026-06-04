ab01c9ac51bca8624df04499e49b40a5d49f60a1
xuzewei28 <xuzewei2@h-partners.com>
2026-06-04 14:00:48 +0800

    fix

--- src/ops/all_gather/executor/ins_v2_all_gather_parallel_opt_executor.cc
+++ src/ops/all_gather/executor/ins_v2_all_gather_parallel_opt_executor.cc
@@ -293,7 +293,7 @@ void InsV2AllGatherParallelOptExecutor<AlgTopoMatch, InsAlgTemplate0, InsAlgTemp
     std::vector<float> &splitDataSize) const
 {
     // double splitData = multipleDimensionSplitRatio_;
-    double splitData = 0.47;
+    double splitData = 0.465; // 经验值，先发45.5%，后发剩余的55.5%，其中后发的55.5%中再先发45.5%，最后发剩余的30%
     splitDataSize.push_back(splitData);
     splitDataSize.push_back(splitData);
     splitDataSize.push_back(1 - splitData - splitData);
@@ -424,7 +424,7 @@ HcclResult InsV2AllGatherParallelOptExecutor<AlgTopoMatch, InsAlgTemplate0, InsA
         tempAlgParamsAll0.buffInfo.inBuffBaseOff = dataSizeAxis0 + dataSizeAxis1;
         tempAlgParamsAll0.inputSliceStride = dataSize_;
 
-        tempAlgParamsAll0.repeatNum = 4; // 先发4份
+        tempAlgParamsAll0.repeatNum = 5; // 先发4份
 
         tempAlgParamsAll0.buffInfo.hcclBuff = resCtx.cclMem;
         tempAlgParamsAll0.buffInfo.hcclBuffType = BufferType::HCCL_BUFFER;
@@ -520,7 +520,7 @@ HcclResult InsV2AllGatherParallelOptExecutor<AlgTopoMatch, InsAlgTemplate0, InsA
         tempAlgParamsAll1.buffInfo.inBuffBaseOff = dataSizeAxis0 + dataSizeAxis1;
         tempAlgParamsAll1.inputSliceStride = dataSize_;
 
-        tempAlgParamsAll1.repeatNum = rankSizeLevel1_ - 4; // 先发4份
+        tempAlgParamsAll1.repeatNum = rankSizeLevel1_ - 5; // 先发4份
 
         tempAlgParamsAll1.buffInfo.hcclBuff = resCtx.cclMem;
         tempAlgParamsAll1.buffInfo.hcclBuffType = BufferType::HCCL_BUFFER;
