1d84c3174419903cfef6b57d2fd4fdd870363577
GoodTimeLZ <linzhen9@huawei.com>
2026-06-05 17:28:35 +0800

    1

--- src/ops/op_common/template/aiv/hccl_aiv_utils.h
+++ src/ops/op_common/template/aiv/hccl_aiv_utils.h
@@ -73,6 +73,7 @@ struct OpCounterInfo {
 struct AivOpArgs {
     HcclCMDType cmdType = HcclCMDType::HCCL_CMD_MAX;
     std::string comm = {};
+    HcclComm hcclComm = nullptr;
     u32 numBlocks = MAX_NUM_BLOCKS;
     rtStream_t stream = nullptr;
     uint64_t beginTime = 0;
@@ -145,6 +146,8 @@ extern thread_local std::shared_ptr<InsQueue> g_recordingQueue;
 extern thread_local bool g_recordOnlyMode;
 extern thread_local u64 g_baseInputAddr;
 extern thread_local u64 g_baseOutputAddr;
+extern thread_local HcclComm g_aivCurrentComm;
+extern thread_local std::string g_aivCurrentCommName;
 
 using AivSuperKernelArgs = struct AivSuperKernelArgsDef {
     const void* buffersIn = nullptr; // 注册的CCLIN地址，所有卡可访问
@@ -178,6 +181,8 @@ using AivSuperKernelArgs = struct AivSuperKernelArgsDef {
     }
     AivSuperKernelArgsDef() {}
 };
+
+void ProcessAivExceptionCallBack(aclrtExceptionInfo *exceptionInfo);
 
 HcclResult RegisterKernel();
 
