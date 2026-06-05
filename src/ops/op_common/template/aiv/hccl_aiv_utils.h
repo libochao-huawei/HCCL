fd7530722fa1dc0500ce216628be933c5ebefb6f
silencesirius <caixin21@huawei.com>
2026-06-05 17:19:27 +0800

    alltoall perf opt in small data size scenario

--- src/ops/op_common/template/aiv/hccl_aiv_utils.h
+++ src/ops/op_common/template/aiv/hccl_aiv_utils.h
@@ -28,7 +28,7 @@ constexpr u32 AIV_TOPO_ADDR_OFFSET = 32 * 1024;
 constexpr u32 AIV_TOPO_BUFF_LEN = 8 * 1024;
 constexpr u32 AIV_FLAG_ADDR_OFFSET = 40 * 1024;
 constexpr u32 AIV_FLAG_AREA_SIZE = 1000 * 1024;
-constexpr u32 AIV_TAG_BUFF_LEN = 33 * 1024 * 1024;
+constexpr u32 AIV_TAG_BUFF_LEN = 65 * 1024 * 1024;
 
 constexpr u32 AIV_MAX_CCL_LOOP_NUM = 16;
 
@@ -104,6 +104,7 @@ struct AivOpArgs {
     u64 repeatNum = 0;
     u64 inputRepeatStride = 0;
     u64 outputRepeatStride = 0;
+    u64 hcclBuffSize = 0;
     bool isOpBase = false;
     ExtraArgs extraArgs = {}; 
     uint64_t topo_[TOPO_LEN] = {0}; 
@@ -166,6 +167,7 @@ extern thread_local std::shared_ptr<InsQueue> g_recordingQueue;
 extern thread_local bool g_recordOnlyMode;
 extern thread_local u64 g_baseInputAddr;
 extern thread_local u64 g_baseOutputAddr;
+extern thread_local u64 g_aivCurrentCclBufferSize;
 extern thread_local HcclComm g_aivCurrentComm;
 extern thread_local std::string g_aivCurrentCommName;
 
